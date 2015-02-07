5ecd3d77a4e7689ab90fce4eb603fa0d314e4c6f
Philip Martzok <philip@bitstars.com>
2015-02-07 11:48:39 +0100

    - finished main menu component

--- app/components/header/mainMenu.jsx
+++ app/components/header/mainMenu.jsx
@@ -1,14 +1,34 @@
 /** @jsx React.DOM */
 
 var React = require('react'),
-	Col = require('react-bootstrap/Col');
+	Col = require('react-bootstrap/Col'),
+	Container = require('react-bootstrap/Grid'),
+	Navbar = require('react-bootstrap/Navbar'),
+	Nav = require('react-bootstrap/Nav'),
+	DropdownButton = require('react-bootstrap/Dropdownbutton'),
+	NavItem = require('react-bootstrap/NavItem');
 
 module.exports = MainMenu = React.createClass({
 	render: function () {
 		return (
 			<div id="main-menu">
+				<Container>
+					<Navbar fluid={true} toggleNavKey="mainMenu" bsStyle="link">
+						<Nav eventKey="mainMenu">
+							<DropdownButton title="Home">
 
+							</DropdownButton>
+							<NavItem href="#">Offers</NavItem>
+							<NavItem href="#">Requests</NavItem>
+							<NavItem href="#">Map</NavItem>
+							<NavItem href="#">More</NavItem>
+						</Nav>
+					</Navbar>
+				</Container>
 			</div>
 		);
+	},
+	onToggle: function () {
+		console.log("Hello");
 	}
 });
