f2a660597fdc87287182c58c44dfb09a1fa39e79
Philip Martzok <philip@bitstars.com>
2015-06-26 11:26:13 +0200

    - added mixpanel logo to footer

--- app/shared/components/footer/footer.jsx
+++ app/shared/components/footer/footer.jsx
@@ -4,41 +4,47 @@ import {Grid, Col} from 'react-bootstrap';
 import Icon from '../helper/icon.jsx';
 
 export default React.createClass({
-    displayName: "Footer",
+	displayName: "Footer",
 
-    render() {
-        return (
-            <footer>
-                <Grid>
-                    <Col xs={12} md={6} className="foo-left">
-                        <p>
-                            <span>Shoutit &copy; 2015</span>
-                            &nbsp;|&nbsp;
-                            <Link to="rules">Marketplace Rules</Link>
-                            &nbsp;|&nbsp;
-                            <Link to="tos">Terms of Service</Link>
-                            &nbsp;|&nbsp;
-                            <Link to="policy">Privacy</Link>
-                        </p>
-                    </Col>
+	render() {
+		return (
+			<footer>
+				<Grid>
+					<Col xs={12} md={6} className="foo-left">
+						<p>
+							<span>Shoutit &copy; 2015</span>
+							&nbsp;|&nbsp;
+							<Link to="rules">Marketplace Rules</Link>
+							&nbsp;|&nbsp;
+							<Link to="tos">Terms of Service</Link>
+							&nbsp;|&nbsp;
+							<Link to="policy">Privacy</Link>
+						</p>
+					</Col>
 
-                    <Col xs={12} md={6} className="foo-center">
-                        <ul>
-                            <li>
-                                <a href="https://play.google.com/store/apps/details?id=com.shoutit.app.android">
-                                    <Icon name="playstore"/>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://itunes.apple.com/us/app/shoutit-app/id947017118?mt=8&uo=6&at=&ct="
-                                   target="itunes_store">
-                                    <Icon name="appstore"/>
-                                </a>
-                            </li>
-                        </ul>
-                    </Col>
-                </Grid>
-            </footer>
-        );
-    }
+					<Col xs={12} md={6} className="foo-center">
+						<ul>
+							<li>
+								<a href="https://play.google.com/store/apps/details?id=com.shoutit.app.android">
+									<Icon name="playstore"/>
+								</a>
+							</li>
+							<li>
+								<a href="https://itunes.apple.com/us/app/shoutit-app/id947017118?mt=8&uo=6&at=&ct="
+								   target="itunes_store">
+									<Icon name="appstore"/>
+								</a>
+							</li>
+							<li>
+								<a href="https://mixpanel.com/f/partner">
+									<img src="https://cdn.mxpnl.com/site_media/images/partner/badge_light.png"
+										 alt="Mobile Analytics"/>
+								</a>
+							</li>
+						</ul>
+					</Col>
+				</Grid>
+			</footer>
+		);
+	}
 });
