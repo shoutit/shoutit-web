ecf345c9accc4be8638384ab8c3114033bea66da
Philip Martzok <philip@bitstars.com>
2015-07-06 14:46:55 +0200

    - update pubished

--- newrelic.js
+++ newrelic.js
@@ -8,20 +8,20 @@
 var name = process.env.NODE_ENV === "development" ? '[Dev] Shoutit Webserver' : 'Shoutit Webserver';
 
 exports.config = {
-  /**
-   * Array of application names.
-   */
-  app_name: [name],
-  /**
-   * Your New Relic license key.
-   */
-  license_key: '20539b4798f35e738173b849d6be9b6d874a0e7f',
-  logging: {
-    /**
-     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
-     * issues with the agent, 'info' and higher will impose the least overhead on
-     * production applications.
-     */
-    level: 'info'
-  }
+	/**
+	 * Array of application names.
+	 */
+	app_name: [name],
+	/**
+	 * Your New Relic license key.
+	 */
+	license_key: '20539b4798f35e738173b849d6be9b6d874a0e7f',
+	logging: {
+		/**
+		 * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+		 * issues with the agent, 'info' and higher will impose the least overhead on
+		 * production applications.
+		 */
+		level: 'info'
+	}
 };
