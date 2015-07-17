b61a6aa721550a99c7c8784c40f52280af3e4ad5
Philip Martzok <philip@bitstars.com>
2015-07-17 13:18:39 +0200

    - fixed footer padding

--- app/server/web.js
+++ app/server/web.js
@@ -95,27 +95,25 @@ ShoutitClient.misc().sortTypes()
 function fetchData(userSession, routes, params, query) {
 	var data = {};
 	//return Promise.resolve(data);
-	//return Promise.all(routes.filter(function (route) {
-	//	return route.handler.fetchData;
-	//}).map(function (route) {
-	//	return new Promise(function (resolve) {
-	//		route.handler.fetchData(ShoutitClient, userSession, params, route.name, query)
-	//			.on('complete', function (result, resp) {
-	//				if (result instanceof Error || resp.statusCode !== 200) {
-	//					resolve({});
-	//				} else {
-	//					resolve(result);
-	//				}
-	//			});
-	//	}).then(function (fetched) {
-	//			console.log("Fetched data for", route.name);
-	//			data[route.name] = fetched;
-	//		});
-	//})).then(function () {
-	//	return data;
-	//});
-
-	return Promise.resolve(data);
+	return Promise.all(routes.filter(function (route) {
+		return route.handler.fetchData;
+	}).map(function (route) {
+		return new Promise(function (resolve) {
+			route.handler.fetchData(ShoutitClient, userSession, params, route.name, query)
+				.on('complete', function (result, resp) {
+					if (result instanceof Error || resp.statusCode !== 200) {
+						resolve({});
+					} else {
+						resolve(result);
+					}
+				});
+		}).then(function (fetched) {
+				console.log("Fetched data for", route.name);
+				data[route.name] = fetched;
+			});
+	})).then(function () {
+		return data;
+	});
 }
 
 
