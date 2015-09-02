7b76745117616bff96d05716fcc29e9ce62b3e63
Mo Chawich <mo.chawich@gmail.com>
2015-09-03 01:17:16 +0200

    - Added utils file - Dynamic id_rsa path using the home directory - deployed to dev.www.shoutit.com :)

---
+++ app/shared/utils.js
@@ -0,0 +1,14 @@
+/**
+ * Created by mo on 03.09.2015.
+ */
+
+
+/**
+ * Return home directory for current user.
+ *
+ * @return {String}
+ */
+
+exports.getUserHome = function getUserHome() {
+    return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+};
--- flights/targets.js
+++ flights/targets.js
@@ -2,20 +2,23 @@
  * Created by Philip on 10.04.2015.
  */
 
-var plan = require('flightplan');
+var plan = require('flightplan'),
+    utils = require('../app/shared/utils'),
+    path = require('path');
+
 
 plan.target('dev-old', {
     host: 'web.shoutit.com',
     username: 'root',
     agent: process.env.SSH_AUTH_SOCK,
-    privateKey: "E:\\shoutit\\private_key_shoutit.pem"
+    privateKey: path.join(utils.getUserHome(), ".ssh", "id_rsa")
 });
 
 plan.target('dev', {
     host: 'dev.www.shoutit.com',
     username: 'root',
     agent: process.env.SSH_AUTH_SOCK,
-    privateKey: "E:\\shoutit\\private_key_shoutit.pem"
+    privateKey: path.join(utils.getUserHome(), ".ssh", "id_rsa")
 }, {
     stopPM2: true
 });
@@ -25,7 +28,7 @@ plan.target('production', [
         host: 'node-01.www.shoutit.com',
         username: 'root',
         agent: process.env.SSH_AUTH_SOCK,
-        privateKey: "E:\\shoutit\\private_key_shoutit.pem"
+        privateKey: path.join(utils.getUserHome(), ".ssh", "id_rsa")
     }
 ]);
 
