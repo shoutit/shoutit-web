7e5e5f8f041cb0510b354c15a794386ce8546130
Pooria Attarzadeh <pooria_az@ymail.com>
2015-09-10 02:17:33 +0430

    removing console.log

--- app/server/resources/auth/changePassword.js
+++ app/server/resources/auth/changePassword.js
@@ -2,7 +2,6 @@
 
 module.exports = function (client, path) {
 	return function (session, data) {
-		console.log(data);
 		return client.post(path + '/change_password' , {
 			accessToken: session && session.accessToken ? session.accessToken : null,
 			data: JSON.stringify(data)
