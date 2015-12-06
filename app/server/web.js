51c4f754bb1d3a558157fadc526c243d27f12bb0
Mo Chawich <mo.chawich@gmail.com>
2015-12-06 13:36:56 +0000

    Revert "- Updated SERVER_ROOT for local testing of Facebook publish_actions"
    This reverts commit 412c98232d1a30c31ccc2243b8b0f58dce3b6024.

--- app/server/web.js
+++ app/server/web.js
@@ -36,10 +36,10 @@ var morgan = require('morgan'),
 	cors = require('cors');
 
 // Runtime Data:
-var SERVER_ROOT = process.env.SERVER_ROOT || "https://shoutit-web.ngrok.io/";
+var SERVER_ROOT = process.env.SERVER_ROOT || "localhost:8080";
 var graphData = require('./resources/consts/graphData');
 var currencies, categories, sortTypes;
-var whitelist = ['https://shoutit.com', 'https://www.shoutit.com', 'http://dev.www.shoutit.com', 'https://shoutit-web.ngrok.io'];
+var whitelist = ['https://shoutit.com', 'https://www.shoutit.com', 'http://dev.www.shoutit.com'];
 if (["developmentLocal"].indexOf(process.env.NODE_ENV) >= 0) {
 	whitelist.push('http://localhost');
 }
