ed4413e4355b313cd92a7c63ab3ef67eeeaa055c
Giampaolo Bellavite <io@gpbl.org>
2015-12-16 00:17:07 +0100

    Update chat route handler for React 0.14

--- app/shared/components/chat/chat.jsx
+++ app/shared/components/chat/chat.jsx
@@ -1,41 +1,22 @@
 import React from 'react';
 
-import {Col} from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 import DocumentTitle from 'react-document-title';
 
-import ConversationList from './conversations/list.jsx';
+import Conversations from './conversations/Conversations.jsx';
 
-export default function (envData) {
+export default function({ children, params }) {
+  return (
+    <DocumentTitle title="Chats - Shoutit">
+      <div className="chat">
+        <Col xs={12} md={4} className="chat-left-padding">
+          <Conversations chatId={ params.chatId } />
+        </Col>
+        <Col xs={12} md={8} className="chat-left-padding">
+          { children }
+        </Col>
+      </div>
+    </DocumentTitle>
+  );
 
-	return React.createClass({
-		displayName: "Chat",
-
-		statics: {
-			fetchData(client, session) {
-				return client.conversations().load(session);
-			},
-
-			willTransitionTo(transition) {
-				if(envData && !envData.user) {
-					transition.redirect("login");
-				}
-			}
-		},
-
-		render() {
-			return (
-				<DocumentTitle title={"Chats - Shoutit"}>
-					<div className="chat">
-						<Col xs={12} md={4} className="chat-left-padding">
-							<ConversationList {...this.props}/>
-						</Col>
-						<Col xs={12} md={8} className="chat-left-padding">
-							{ this.props.children }
-						</Col>
-					</div>
-				</DocumentTitle>
-			);
-		}
-
-	});
 }
