cac550f0f8cdbd311542f0678ca0c128b7d15259
Giampaolo Bellavite <io@gpbl.org>
2015-12-16 22:44:54 +0100

    Refactor messageItem → MessageGroup

---
+++ app/shared/components/chat/message/MessageGroup.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import classNames from 'classnames';
+import moment from 'moment';
+
+import {Image, Icon} from '../../helper';
+
+/**
+ * This component shows messages sent by the same user, e.g. the passed `messages `
+ * are assumed to have always the same `user`.
+ * @param {Array} props.messages
+ * @param {Boolean} props.isSentByMe
+ */
+export default function MessageGroup({ messages, isSentByMe }) {
+
+  const user = messages[0].user;
+
+  const blockClasses = classNames({'from': isSentByMe, 'to': !isSentByMe })
+  const imageClasses = classNames({'from-img': isSentByMe, 'to-img': !isSentByMe })
+  const msgClasses = classNames({'from-msg': isSentByMe, 'to-msg': !isSentByMe })
+
+  const createdAt = messages[messages.length-1].created_at;
+
+  return (
+    <div className={blockClasses}>
+      <div className={imageClasses}>
+        <Image infix="user" size="small" src={user.image}/>
+      </div>
+      <div className={msgClasses}>
+
+        { messages.map((message, i) => <p key={ i }>{message.text}</p>) }
+
+        { isSentByMe
+          ? <Icon className="muichat-to" name="chat-mui1"/>
+          : <Icon className="muichat" name="mui-chat"/>
+        }
+
+        <span className="timestamp" title={ createdAt }>
+          { moment.unix(createdAt).fromNow() }
+        </span>
+
+      </div>
+    </div>
+
+  );
+
+}
