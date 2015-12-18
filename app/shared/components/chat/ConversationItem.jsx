868f971f78a893b951d933d62e28003733cbeab7
Giampaolo Bellavite <io@gpbl.org>
2015-12-18 21:20:12 +0100

    Fix layout in conversation item

--- app/shared/components/chat/ConversationItem.jsx
+++ app/shared/components/chat/ConversationItem.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router';
 import moment from 'moment';
+
+import { Column, Grid } from '../helper';
+
 import ManyUsersImage from '../user/ManyUsersImage.jsx';
 
 export default function ConversationItem({ conversation, me, selected, onClick }) {
@@ -12,33 +15,43 @@ export default function ConversationItem({ conversation, me, selected, onClick }
     .join(', ');
 
   return (
-    <div
+    <Grid
+      fluid
       className={ `ConversationItem${selected ? ' isSelected' : ''} `}
       onClick={ onClick.bind(this, conversation.id) }>
 
-      <ManyUsersImage users={ users.filter(user => user.username !== me) } />
+      <Column fluid size={3} clear >
+        <ManyUsersImage users={ users.filter(user => user.username !== me) } />
+      </Column>
 
-      { conversation.type === 'about_shout' &&
-        <p className="ConversationItem-about">
-          <Link to={ `/shout/${about.id}/${about.location.city}/${about.title}` }
-            onClick={ e => e.stopPropagation() }>
-            {about.title}
+      <Column fluid size={8} >
+
+        { conversation.type === 'about_shout' &&
+          <p className="ConversationItem-about">
+            <Link to={ `/shout/${about.id}/${about.location.city}/${about.title}` }
+              onClick={ e => e.stopPropagation() }>
+              {about.title}
+            </Link>
+          </p>
+        }
+
+        <div className="ConversationItem-partecipants">
+          { partecipants }
+        </div>
+
+        <p className="ConversationItem-lastMessage">
+          <Link tabIndex={ -1 } to={ `/chat/${conversation.id}` } onClick={ e => e.stopPropagation() } >
+            { lastMessage.text }
           </Link>
         </p>
-      }
 
-      <div className="ConversationItem-partecipants">{ partecipants }</div>
+      </Column>
+      <Column fluid size={ 4 }>
+        <p className="ConversationItem-createdAt">
+          { moment.unix(lastMessage.created_at).calendar() }
+        </p>
+      </Column>
 
-      <p className="ConversationItem-lastMessage">
-        <Link tabIndex={ -1 } to={ `/chat/${conversation.id}` } onClick={ e => e.stopPropagation() } >
-          { lastMessage.text }
-        </Link>
-      </p>
-
-      <p className="ConversationItem-createdAt">
-        { moment.unix(lastMessage.created_at).calendar() }
-      </p>
-
-    </div>
+    </Grid>
   );
 }
