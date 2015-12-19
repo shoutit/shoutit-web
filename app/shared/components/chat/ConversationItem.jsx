259e0eaefe8ae6e8af5a1a4a90c6d237988493ab
Giampaolo Bellavite <io@gpbl.org>
2015-12-19 17:48:09 +0100

    Use flexbox for conversation item

--- app/shared/components/chat/ConversationItem.jsx
+++ app/shared/components/chat/ConversationItem.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import {Link} from 'react-router';
 import moment from 'moment';
 
-import { Column, Grid } from '../helper';
-
 import ManyUsersImage from '../user/ManyUsersImage.jsx';
 
 export default function ConversationItem({ conversation, me, selected }) {
@@ -15,40 +13,38 @@ export default function ConversationItem({ conversation, me, selected }) {
     .join(', ');
 
   return (
-    <Grid
-      fluid
-      className={ `ConversationItem${selected ? ' isSelected' : ''} `}>
-        <Column fluid size={3} clear >
-          <ManyUsersImage users={ users.filter(user => user.username !== me) } />
-        </Column>
+    <div className={ `ConversationItem${selected ? ' isSelected' : ''} `}>
 
-        <Column fluid size={8} >
+        <div className="ConversationItem-usersImage">
+          <ManyUsersImage users={ users.filter(user => user.username !== me) } />
+        </div>
+
+        <div className="ConversationItem-body">
 
           { conversation.type === 'about_shout' &&
-            <p className="ConversationItem-about">
+            <div>
               <Link to={ `/shout/${about.id}/${about.location.city}/${about.title}` }
                 onClick={ e => e.stopPropagation() }>
                 {about.title}
               </Link>
-            </p>
+            </div>
           }
 
           <div className="ConversationItem-partecipants">
             { partecipants }
           </div>
 
-          <p className="ConversationItem-lastMessage">
-            <Link tabIndex={ -1 } to={ `/chat/${conversation.id}` } onClick={ e => e.stopPropagation() } >
+          <div className="ConversationItem-lastMessage">
+            <Link to={ `/chat/${conversation.id}` } onClick={ e => e.stopPropagation() } >
               { lastMessage.text }
             </Link>
-          </p>
+          </div>
 
-        </Column>
-        <Column fluid size={ 4 }>
-          <p className="ConversationItem-createdAt">
-            { moment.unix(lastMessage.created_at).calendar() }
-          </p>
-        </Column>
-    </Grid>
+        </div>
+
+        <div className="ConversationItem-createdAt">
+          { moment.unix(lastMessage.created_at).calendar() }
+        </div>
+    </div>
   );
 }
