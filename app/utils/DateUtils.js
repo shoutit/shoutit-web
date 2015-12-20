555a9084a45372682f591a6bfa55ec0f18232fea
Giampaolo Bellavite <io@gpbl.org>
2015-12-20 10:26:58 +0100

    New DateUtils 🙌

---
+++ app/utils/DateUtils.js
@@ -0,0 +1,22 @@
+import moment from 'moment';
+
+export function formatCreatedAt(m, now=moment()) {
+
+  if (typeof m !== 'object') {
+    m = moment.unix(m);
+  }
+
+  if (m.isSame(now, 'day')) {
+    return m.format('LT');
+  }
+
+  if (m.isSame(now, 'week')) {
+    return m.format('ddd');
+  }
+
+  if (m.isSame(now, 'year')) {
+    return m.format('ll').replace(m.format('YYYY'), '').replace(/[^\w\d ]*/ig, '').trim();
+  }
+
+  return m.format('l')
+}
