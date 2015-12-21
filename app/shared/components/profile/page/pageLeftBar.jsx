0d85ff5db999bd7254fb428fbdc2575bfe022f44
Pooria Attarzadeh <pooria_az@ymail.com>
2015-12-21 22:11:16 +0330

    creating pageprofile components

---
+++ app/shared/components/profile/page/pageLeftBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Grid} from '../../helper';
+import ProfilePictureCard from '../cards/profilePictureCard.jsx';
+import ProfileButtonsCard from '../cards/profileButtonsCard.jsx';
+import PageAdminsCard from '../cards/pageAdminsCard.jsx';
+
+export default React.createClass({
+    displayName: "ProfileLeftBar",
+
+    render() {
+        let user = this.props.user;
+        return (
+            <Grid fluid={true}>
+                <ProfilePictureCard user={user} />
+                <ProfileButtonsCard user={user} />
+                <PageAdminsCard user={user} />
+            </Grid>
+        );
+    }
+});
