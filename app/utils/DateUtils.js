2befa3d503269c4c7bfb9f2a44c68cc8fe1b1dff
Giampaolo Bellavite <io@gpbl.org>
2016-01-16 10:10:58 +0100

    Add getUnixTime util

--- app/utils/DateUtils.js
+++ app/utils/DateUtils.js
@@ -1,25 +1,29 @@
-import moment from 'moment';
+import moment from "moment";
 
 /**
  * Properly format a `created_date` date.
  */
 export function formatCreatedAt(m, now=moment()) {
 
-  if (typeof m !== 'object') {
+  if (typeof m !== "object") {
     m = moment.unix(m);
   }
 
-  if (m.isSame(now, 'day')) {
-    return m.format('LT');
+  if (m.isSame(now, "day")) {
+    return m.format("LT");
   }
 
-  if (m.isSame(now, 'week')) {
-    return m.format('ddd');
+  if (m.isSame(now, "week")) {
+    return m.format("ddd");
   }
 
-  if (m.isSame(now, 'year')) {
-    return m.format('ll').replace(m.format('YYYY'), '').replace(/[^\w\d ]*/ig, '').trim();
+  if (m.isSame(now, "year")) {
+    return m.format("ll").replace(m.format("YYYY"), "").replace(/[^\w\d ]*/ig, "").trim();
   }
 
-  return m.format('l')
+  return m.format("l");
+}
+
+export function getUnixTime() {
+  return Math.floor(Date.now() / 1000);
 }
