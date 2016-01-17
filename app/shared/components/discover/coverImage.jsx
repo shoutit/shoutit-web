82a3776dfdc30934e592b43282e0c98fc960359a
p0o <pooria_az@ymail.com>
2016-01-17 13:58:03 +0330

    design works for cover image of discover page #19

--- app/shared/components/discover/coverImage.jsx
+++ app/shared/components/discover/coverImage.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import {Grid, Column} from '../helper';
 
+// TODO: Change it to shoutit default cover (for discover page)
+var defaultDiscoverCoverImage = 'http://luminous-landscape.com/articleImages/KWR_Home_Page_Images/Whats_New_Images/V10-DSF1668-Flat.jpg'
+
 var CoverImage = (props) => {
-    // TODO: Remove the temporary image placeholder
-    let image = props.image || 'http://luminous-landscape.com/articleImages/KWR_Home_Page_Images/Whats_New_Images/V10-DSF1668-Flat.jpg';
+    const image = props.image || defaultDiscoverCoverImage;
     const style = {
-        backgroundImage: `url(${image})`,
-        backgroundSize: 'cover',
-        height: '120px'
+        backgroundImage: `url(${image})`
     };
     return (
-        <Grid fluid={true} style={style}>
-            {props.title}
+        <Grid fluid={true} style={style} className="discover-cover">
+            <div className={image? "title with-image": "title"}>
+                {props.title}
+            </div>
         </Grid>
     );
 }
