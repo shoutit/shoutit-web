ded3c14d801c92cf500b2d6afd01902a40c980e8
p0o <pooria_az@ymail.com>
2016-01-27 02:57:06 +0330

    adding general components to main page #33

--- app/shared/components/main/footer.jsx
+++ app/shared/components/main/footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Icon, Grid, Column} from '../helper';
 import {Link} from 'react-router';
 import {Link as ScrollLink} from 'react-scroll';
 import {ANDROID_LINK, IOS_LINK} from '../../consts/defaults';
@@ -8,47 +9,53 @@ export default React.createClass({
 
     render() {
         return (
-            <div className="mainpage-footer si-container">
-                <div className="logo-section clear grid-4">
-                    <div className="icon res1x-footer_logo"></div>
+            <Grid className="mainpage-footer">
+                <Column clear={true} size="4" className="logo-section">
+                    <Icon name="footer_logo" />
                     <span className="copyright">Copyright © 2015 Shoutit All rights reserved.</span>
-                </div>
-                <div className="links-section grid-2">
+                </Column>
+                <Column size="2" className="links-section">
                     <h3>Explore</h3>
                     <Link to="policy">Shouts</Link>
                     <Link to="policy">Tags</Link>
                     <Link to="policy">Users</Link>
-                </div>
-                <div className="links-section grid-2">
+                </Column>
+                <Column size="2" className="links-section">
                     <h3>About</h3>
                     <Link to="policy">The Company</Link>
                     <Link to="policy">History</Link>
                     <Link to="policy">Vision</Link>
-                </div>
-                <div className="links-section grid-2">
+                </Column>
+                <Column size="2" className="links-section">
                     <h3>Follow Us</h3>
                     <Link to="policy">Facebook</Link>
                     <Link to="policy">Google+</Link>
                     <Link to="policy">Twitter</Link>
-                </div>
-                <div className="links-section grid-2">
+                </Column>
+                <Column size="2" className="links-section grid-2">
                     <h3>Contact</h3>
                     <Link to="policy">Basic Info</Link>
                     <Link to="policy">Map</Link>
                     <Link to="policy">Contact Form</Link>
-                    <ScrollLink to="nav" className="btn-explore" spy={true} smooth={true}
-                                offset={-100} duration={500} >Go to top</ScrollLink>
-                </div>
-                <div className="links-section grid-3">
+                    <ScrollLink className="btn-explore"
+                                spy={true}
+                                to="nav"
+                                smooth={true}
+                                offset={-100}
+                                duration={500}>
+                                    Go to top
+                    </ScrollLink>
+                </Column>
+                <Column size="2" className="links-section">
                     <h3>Mobile</h3>
                     <a href={ANDROID_LINK} target="_blank">
-                        <div className="icon res1x-app-store-small"></div>
+                        <Icon name="app-store-small" />
                     </a>
                     <a href={IOS_LINK} target="_blank">
-                        <div className="icon res1x-google-play-small"></div>
+                        <Icon name="google-play-small" />
                     </a>
-                </div>
-            </div>
+                </Column>
+            </Grid>
         );
     }
 })
--- app/shared/components/main/mainPage.jsx
+++ app/shared/components/main/mainPage.jsx
@@ -3,6 +3,7 @@ import {Link, History} from 'react-router';
 import {StoreWatchMixin} from 'fluxxor';
 import {Link as ScrollLink, Element, Button} from 'react-scroll';
 import SearchBar from '../header/searchBar.jsx';
+import {Icon, Grid, Column} from '../helper';
 import Explore from './explore.jsx';
 import Footer from './footer.jsx';
 import {ANDROID_LINK, IOS_LINK} from '../../consts/defaults';
@@ -30,20 +31,22 @@ export default React.createClass({
     render() {
         return (
             <div className="mainpage">
-                <Element name="nav" className="mainpage-nav si-container">
-                    <div className="nav-left clear grid-8">
-                        <Link to="/"><div className="res1x-logo"></div></Link>
-                    </div>
-                    <div className="nav-right grid-7">
-                        <Link to="/login"><span className="btn-login">Login</span></Link>
-                        <Link to="/signup"><span className="btn-d">Signup</span></Link>
-                        <span className="btn-d">About</span>
-                    </div>
-                </Element>
-                <div className="mainpage-cover si-container">
-                    <div className="mainpage-cover-opacity si-container">
-                        <div className="hero si-container">
-                            <div className="grid-7 offset-2">
+                <Grid>
+                    <Element name="nav" className="mainpage-nav">
+                        <Column clear={true} size="8" className="nav-left">
+                            <Link to="/"><Icon name="logo" /></Link>
+                        </Column>
+                        <Column size="7" className="nav-right">
+                            <Link to="/login"><span className="btn-login">Login</span></Link>
+                            <Link to="/signup"><span className="btn-d">Signup</span></Link>
+                            <span className="btn-d">About</span>
+                        </Column>
+                    </Element>
+                </Grid>
+                <Grid className="mainpage-cover">
+                    <Grid fluid={true} className="mainpage-cover-opacity">
+                        <Grid fluid={true} className="hero">
+                            <Column size="7" offset="2">
                                 <h1>Everything Much Cheaper!</h1>
                                 <p>
                                     Lorem ipsum dolor sit amet, quo ad adhuc debet munere. Harum congue ne his, suas viris constituam ne cum, sonet mandamus at his.
@@ -53,31 +56,35 @@ export default React.createClass({
 
                                 <ScrollLink to="how" className="btn-how" smooth={true}
                                         offset={-20} duration={1000} >How It Works</ScrollLink>
-                            </div>
-                            <div className="grid-3 offset-1">
-                                <a href={IOS_LINK} target="_blank"><div className="icon res1x-app_store" ></div></a>
-                                <a href={ANDROID_LINK} target="_blank"><div className="icon res1x-google_play" ></div></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="mainpage-search si-container">
+                            </Column>
+                            <Column size="3" offset="1">
+                                <a href={IOS_LINK} target="_blank"><Icon name="app_store" /></a>
+                                <a href={ANDROID_LINK} target="_blank"><Icon name="google_play" /></a>
+                            </Column>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid className="mainpage-search">
                     <div className="grid-9 offset-3">
                         <SearchBar height="45" flux={this.props.flux}/>
                     </div>
-                </div>
+                </Grid>
 
-                <Element name="explore" className="mainpage-explore si-container">
-                    <Explore categories={this.state.categories} />
-                </Element>
+                <Grid>
+                    <Element name="explore" className="mainpage-explore">
+                        <Explore categories={this.state.categories} />
+                    </Element>
+                </Grid>
 
-                <Element name="how" className="mainpage-how si-container">
-                    <h2>How It Works</h2>
-                </Element>
-                
+                <Grid>
+                    <Element name="how" className="mainpage-how">
+                        <h2>How It Works</h2>
+                    </Element>
+                </Grid>
+
                 <Footer />
                 {this.props.children}
-               
+
             </div>
         );
     }
