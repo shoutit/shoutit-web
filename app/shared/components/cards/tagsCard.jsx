8666719f4f746ad9e2e649a3897819bb8a9766ba
p0o <pooria_az@ymail.com>
2016-02-03 02:08:15 +0330

    refactoring tags card as a stateless component #31

--- app/shared/components/cards/tagsCard.jsx
+++ app/shared/components/cards/tagsCard.jsx
@@ -1,54 +1,34 @@
 import React from 'react';
 import {Icon, Column, Grid} from '../helper';
 
-export default React.createClass({
-    displayName: "tagsCard",
+export default function TagsCard(props) {
+    const {suggestions} = props;
+    const tags = suggestions? suggestions.tags: null;
 
-    render() {
-        return (
-            <section className="si-card">
-                <div className="card-header">
-                    <h3>suggested tags</h3>
-                </div>
-                <Grid fluid={true}>
-                    <Column fluid={true} clear={true} size="3" className="card-list-img">
-                        <Icon name="tag" className="tag-card-icon"/>
-                    </Column>
-                    <Column fluid={true} size="9" className="card-list-item">
-                        <span>Cars</span>
-                    </Column>
-                    <Column fluid={true} size="3" >
-                        <Icon name="listen" className="card-listen-btn"/>
-                    </Column>
-                    <Column fluid={true} clear={true} size="3" className="card-list-img">
-                        <Icon name="tag" className="tag-card-icon" />
-                    </Column>
-                    <Column fluid={true} size="9" className="card-list-item">
-                        <span>Gaming</span>
-                    </Column>
-                    <Column fluid={true} size="3" >
-                        <Icon name="listen" className="card-listen-btn"/>
-                    </Column>
-                    <Column fluid={true} clear={true} size="3" className="card-list-img">
-                        <Icon name="tag" className="tag-card-icon" />
-                    </Column>
-                    <Column fluid={true} size="9" className="card-list-item">
-                        <span>Houses</span>
-                    </Column>
-                    <Column fluid={true} size="3" >
-                        <Icon name="listen" className="card-listen-btn"/>
-                    </Column>
-                    <Column fluid={true} clear={true} size="3" className="card-list-img">
-                        <Icon name="tag" className="tag-card-icon" />
-                    </Column>
-                    <Column fluid={true} size="9" className="card-list-item">
-                        <span>Gadgets</span>
-                    </Column>
-                    <Column fluid={true} size="3" >
-                        <Icon name="listen" className="card-listen-btn"/>
-                    </Column>
-                </Grid>
-            </section>
-        );
-    }
-});
+    return (
+        <section className="si-card">
+            <div className="card-header">
+                <h3>suggested tags</h3>
+            </div>
+            {tags.loading?
+                <Progress />
+                :
+                tags.list.map((tag, idx) => {
+                    return (
+                        <Grid fluid={true} key={`card-tag-${idx}`}>
+                            <Column fluid={true} clear={true} size="3" className="card-list-img">
+                                <Icon name="tag" className="tag-card-icon"/>
+                            </Column>
+                            <Column fluid={true} size="9" className="card-list-item">
+                                <span>{tag.name}</span>
+                            </Column>
+                            <Column fluid={true} size="3" >
+                                <Icon name="listen" className="card-listen-btn"/>
+                            </Column>
+                        </Grid>
+                    );
+                })
+            }
+        </section>
+    );
+}
