de5ea9b5b0b4a580fdab6dff948bc5ea1fa544ee
p0o <pooria_az@ymail.com>
2016-02-02 22:11:32 +0330

    adding suggestion store to the board component #31

--- app/shared/components/feed/board.jsx
+++ app/shared/components/feed/board.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Cards from '../cards';
+import {StoreWatchMixin} from 'fluxxor';
 
 export default React.createClass({
     displayName: "Board",
+    mixins: [new StoreWatchMixin("suggestions")],
 
     statics: {
         fetchId: 'suggestions',
@@ -19,19 +21,30 @@ export default React.createClass({
         flux: React.PropTypes.object
     },
 
+    getStateFromFlux() {
+        const {flux} = this.context;
+        const suggestions = flux.store('suggestions').getState();
+
+        return {
+            suggestions
+        }
+    },
+
     componentDidMount() {
         this.context.flux.actions.getSuggestions();
     },
 
     render() {
         const {items} = this.props;
+        const {suggestions} = this.state;
 
-        return React.createElement.apply(undefined, [
+        return React.createElement.apply(this, [
                     'div',
                     null
                     ].concat(
                         items.map((component) => {
-                            return React.createElement(Cards[component], {key: component}, null)
+                            return React.createElement(Cards[component],
+                                   {key: component, suggestions}, null)
                         })
                     )
                 );
