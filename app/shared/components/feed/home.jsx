bfe9c594e27b663364f071e314a66850ab86bbde
p0o <pooria_az@ymail.com>
2016-02-05 15:52:14 +0330

    deleting the old implementation of card layout #83

--- app/shared/components/feed/home.jsx
+++ app/shared/components/feed/home.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {match} from "react-router";
-import routes from "../../routes";
 import {Grid, Column} from "../helper";
-import Board from "./board.jsx";
-import findLast from "lodash/collection/findLast";
 
 export default React.createClass({
   displayName: "Home",
@@ -14,13 +10,6 @@ export default React.createClass({
     location: React.PropTypes.object
   },
 
-  statics: {
-    layoutConfig: {
-      leftBoard: ["profileCard", "listeningCard", "pagesCard"],
-      rightBoard: ["tagsCard", "listenToCard", "suggestShoutCard"]
-    }
-  },
-
   getChildContext() {
     return {
       flux: this.props.flux,
@@ -29,49 +18,19 @@ export default React.createClass({
     };
   },
 
-    /**
-     * Getting developer's required layout for the path in the tree
-     *
-     * Component should be defined under /home tree in react router and uses a static object called layoutConfig
-     * @returns {OBJECT} latest configured layout object in the routes tree, if any
-     */
-  getLayoutConfig() {
-    let lastConfiguredLayoutRoute;
-    match({routes, location: this.props.location.pathname + this.props.location.search},
-            (error, redirectLocation, renderProps) => {
-              lastConfiguredLayoutRoute = findLast(renderProps.routes, (item) => {
-                return item.component.layoutConfig;
-              });
-            });
-
-        // There is no layoutConfig defined in the whole route tree - NOT GOOD!
-    if(!lastConfiguredLayoutRoute) { return null; }
-
-    return lastConfiguredLayoutRoute.component? lastConfiguredLayoutRoute.component.layoutConfig: null;
-  },
-
   render() {
-    const {leftBoard, rightBoard} = this.getLayoutConfig();
 
-        // Calculating middle column size (15 is 100%)
-    const middleColumnSize = leftBoard && rightBoard? "9": leftBoard || rightBoard? "12" : "15";
     return (
       <Grid className="homepage-holder">
-          {leftBoard &&
-              <Column size="3" clear={true}>
-                  <Board items={leftBoard}/>
-              </Column>
-          }
+          <Column size="3" clear={true}>
 
-          <Column size={middleColumnSize} clear={!leftBoard}>
+          </Column>
+          <Column size="9">
               {React.cloneElement(this.props.children, {flux: this.props.flux})}
           </Column>
+          <Column size="3">
 
-          {rightBoard &&
-              <Column size="3">
-                  <Board items={rightBoard}/>
-              </Column>
-          }
+          </Column>
       </Grid>
     );
   }
