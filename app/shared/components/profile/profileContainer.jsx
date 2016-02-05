56e6f8a4028654c453e295f0789f50a3fb0943f7
p0o <pooria_az@ymail.com>
2016-02-05 23:37:09 +0330

    fixing indentions #83

--- app/shared/components/profile/profileContainer.jsx
+++ app/shared/components/profile/profileContainer.jsx
@@ -3,41 +3,41 @@ import {Grid, Column} from '../helper';
 import Profile from './profile.jsx';
 
 export default React.createClass({
-    displayName: "Home",
+  displayName: "Home",
 
-    childContextTypes: {
-        flux: React.PropTypes.object,
-        params: React.PropTypes.object,
-        location: React.PropTypes.object
-    },
+  childContextTypes: {
+    flux: React.PropTypes.object,
+    params: React.PropTypes.object,
+    location: React.PropTypes.object
+  },
 
-    statics: {
-        fetchId: 'user',
-        fetchData(client, session, params) {
-            return client.users().get(session, params.username);
-        }
-    },
-
-    getChildContext() {
-        return {
-            flux: this.props.flux,
-            params: this.props.params,
-            location: this.props.location
-        }
-    },
-
-    render() {
-        return (
-            <div className="profile-holder">
-                <Grid >
-                    <Column size="12" clear={true}>
-                        {this.props.children}
-                    </Column>
-                    <Column size="3">
-
-                    </Column>
-                </Grid>
-            </div>
-        );
+  statics: {
+    fetchId: 'user',
+    fetchData(client, session, params) {
+      return client.users().get(session, params.username);
     }
+  },
+
+  getChildContext() {
+    return {
+      flux: this.props.flux,
+      params: this.props.params,
+      location: this.props.location
+    };
+  },
+
+  render() {
+    return (
+      <div className="profile-holder">
+        <Grid >
+          <Column size="12" clear={true}>
+            {this.props.children}
+          </Column>
+          <Column size="3">
+
+          </Column>
+        </Grid>
+      </div>
+    );
+  }
 });
