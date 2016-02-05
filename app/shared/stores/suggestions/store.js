8576a1015914bb8dd4ac11e8f2bb6b938e0790e3
p0o <pooria_az@ymail.com>
2016-02-05 12:57:51 +0330

    adding only the identifiers to lists not the whole data #31

--- app/shared/stores/suggestions/store.js
+++ app/shared/stores/suggestions/store.js
@@ -29,6 +29,30 @@ var SuggestionsStore = Fluxxor.createStore({
     };
   },
 
+  addPagesList(data) {
+    this.state.pages.list = data.map((item) => item.username);
+    this.state.pages.loading = false;
+    this.emit("change");
+  },
+
+  addShoutsList(data) {
+    this.state.shouts.list = data.map((item) => item.id);
+    this.state.shouts.loading = false;
+    this.emit("change");
+  },
+
+  addUsersList(data) {
+    this.state.users.list = data.map((item) => item.username);
+    this.state.users.loading = false;
+    this.emit("change");
+  },
+
+  addTagsList(data) {
+    this.state.tags.list = data.map((item) => item.name);
+    this.state.tags.loading = false;
+    this.emit("change");
+  },
+
   onGetSuggestions(payload) {
     // TODO: should get location for logged user from their profile for server rendering
     // It is happening now but we need it before onComponentDidMount
@@ -44,6 +68,10 @@ var SuggestionsStore = Fluxxor.createStore({
         console.error(err);
       } else {
         const {pages, shouts, tags, users} = res.body;
+        this.addPagesList(pages);
+        this.addShoutsList(shouts);
+        this.addTagsList(tags);
+        this.addUsersList(users);
 
         this.state.loading = false;
 
