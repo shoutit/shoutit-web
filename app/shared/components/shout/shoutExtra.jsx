0279586965add62626ed14a779b4fa5b644a058c
p0o <pooria_az@ymail.com>
2016-02-09 01:10:09 +0330

    fix indention in shoutExtra #83

--- app/shared/components/shout/shoutExtra.jsx
+++ app/shared/components/shout/shoutExtra.jsx
@@ -7,67 +7,67 @@ import {Column, Grid, ReactVisible} from '../helper';
 import GridShout from '../feed/feed/gridShout/gridShoutItem.jsx';
 
 export default React.createClass({
-    displayName: "ShoutExtra",
+  displayName: "ShoutExtra",
 
-    renderUserShouts() {
-        const {extra} = this.props;
+  renderUserShouts() {
+    const {extra} = this.props;
 
-        if(extra.more && extra.more.offers) {
-            const moreShouts = extra.more.offers;
+    if (extra.more && extra.more.offers) {
+      const moreShouts = extra.more.offers;
 
-            if(extra.more && extra.more.loading) {
-              return (<Progress />);
-            } else if(!moreShouts.length) {
-              return null;
-            } else {
-              return (
-                <div>
-                  <h3 className="extra-title">more shouts by this user</h3>
-                  {moreShouts.map((shout, idx) => (
-                    <GridShout shout={shout}
-                               creator={this.props.creator}
-                               index={idx}
-                               key={'grid-' + idx}/>
-                  ))}
-                </div>
-              );
-            }
-        }
-    },
-
-    renderRelatedShouts() {
-        const {extra} = this.props;
-
-        if(extra.related && extra.related.res) {
-          const relatedShouts = extra.related.res;
-
-          if(extra.more && extra.more.loading) {
-            return (<Progress />);
-          } else if(!relatedShouts.length) {
-            return null;
-          } else {
-            return (
-              <div>
-                <h3 className="extra-title">related shouts</h3>
-                {relatedShouts.map((shout, idx) => (
-                  <GridShout shout={shout}
-                             creator={shout.user}
-                             index={idx}
-                             key={'grid-' + idx}/>
-                ))}
-              </div>
-            );
-          }
-        }
-    },
-
-    render() {
+      if (extra.more && extra.more.loading) {
+        return (<Progress />);
+      } else if (!moreShouts.length) {
+        return null;
+      } else {
         return (
-            <div className="shout-extra">
-                {this.renderUserShouts()}
-                {this.renderRelatedShouts()}
-            </div>
+          <div>
+            <h3 className="extra-title">more shouts by this user</h3>
+            {moreShouts.map((shout, idx) => (
+              <GridShout shout={shout}
+                         creator={this.props.creator}
+                         index={idx}
+                         key={'grid-' + idx}/>
+            ))}
+          </div>
         );
-
+      }
     }
+  },
+
+  renderRelatedShouts() {
+    const {extra} = this.props;
+
+    if (extra.related && extra.related.res) {
+      const relatedShouts = extra.related.res;
+
+      if (extra.more && extra.more.loading) {
+        return (<Progress />);
+      } else if (!relatedShouts.length) {
+        return null;
+      } else {
+        return (
+          <div>
+            <h3 className="extra-title">related shouts</h3>
+            {relatedShouts.map((shout, idx) => (
+              <GridShout shout={shout}
+                         creator={shout.user}
+                         index={idx}
+                         key={'grid-' + idx}/>
+            ))}
+          </div>
+        );
+      }
+    }
+  },
+
+  render() {
+    return (
+      <div className="shout-extra">
+        {this.renderUserShouts()}
+        {this.renderRelatedShouts()}
+      </div>
+    );
+
+  }
 });
