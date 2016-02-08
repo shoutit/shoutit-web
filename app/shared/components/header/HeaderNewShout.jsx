7a5d1a4de7c27f2993e5e86f086c3226f52546bf
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-02-08 15:26:32 +0100

    NewShout dialog overhaul

---
+++ app/shared/components/header/HeaderNewShout.jsx
@@ -0,0 +1,41 @@
+/*
+* This component is created only for popup shout
+* For embedded shout use embeddedShout.jsx
+* github/p0o
+*/
+import React from "react";
+import { StoreWatchMixin } from "fluxxor";
+
+import ShoutForm from "../shouting/form/shoutForm.jsx";
+
+export default React.createClass({
+
+  displayName: "HeaderNewShout",
+
+  mixins: [new StoreWatchMixin("shouts")],
+
+  getStateFromFlux() {
+    const { currencies, categories, draft, status, waiting }
+      = this.props.flux.store("shouts").getState();
+    return {  currencies, categories, draft, status, waiting };
+  },
+
+  render() {
+    const { loggedUser, currentLocation, onShoutSent, flux } = this.props;
+    const { currencies, categories, draft, status, waiting } = this.state;
+    return (
+      <ShoutForm
+        currencies={ currencies }
+        categories={ categories }
+        draft={ draft }
+        status={ status }
+        waiting={ waiting }
+        user={ loggedUser }
+        current={ currentLocation }
+        collapsed={ false }
+        onShoutSent={ onShoutSent }
+        flux={ flux }
+      />
+    );
+  }
+});
--- app/shared/components/header/HeaderProfile.jsx
+++ app/shared/components/header/HeaderProfile.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router";
 import SVGIcon from "../helper/SVGIcon";
-import NewShoutButton from "../shouting/newShoutButton.jsx";
 
 if (process.env.BROWSER) {
   require("styles/components/HeaderProfile.scss");
 }
 
-export default function HeaderProfile({ unreadConversations, onMessagesClick, onProfileClick, onNotificationsClick, loggedUser, flux }) {
+export default function HeaderProfile({
+  unreadConversations,
+  loggedUser,
+  onMessagesClick,
+  onProfileClick,
+  onNotificationsClick,
+  onNewShoutClick
+}) {
   return (
     <div className="HeaderProfile">
       <div>
@@ -19,7 +25,7 @@ export default function HeaderProfile({ unreadConversations, onMessagesClick, on
         <SVGIcon name="bell" badge={ 4 } onClick={ onNotificationsClick } />
       </div>
       <div>
-        <span className="HeaderProfile-newShout">
+        <span className="HeaderProfile-newShout" onClick={ onNewShoutClick }>
           <span><SVGIcon name="sparkle" fill /></span>
           <span>Create Shout</span>
         </span>
