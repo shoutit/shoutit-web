b0396be2093eb6ef9705251cdcb54f5249dff648
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-02-09 12:45:17 +0100

    Remove unused script

--- scripts/ci-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# from https://github.com/sandcastle/circleci-docker/blob/master/script/ci-test
-
-set -e
-
-cd "$(dirname "$0")/.."
-echo "Running tests"
-date "+%H:%M:%S"
-MOCHA_FILE=$CIRCLE_TEST_REPORTS/test-results.xml npm run test:cover
