18c970b437f87b996a56d320a7ec066c6ff2a0d3
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-02-09 11:52:10 +0100

    Add script test to output the test results

---
+++ scripts/ci-test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# from https://github.com/sandcastle/circleci-docker/blob/master/script/ci-test
+
+set -e
+
+cd "$(dirname "$0")/.."
+echo "Running tests"
+date "+%H:%M:%S"
+MOCHA_FILE=$CIRCLE_TEST_REPORTS/test-results.xml npm test
