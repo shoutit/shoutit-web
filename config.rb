ba855e03dce930e88deb4534fa8683576cc3b6cb
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-02-10 09:45:15 +0100

    Remove unused bower and compass files

--- config.rb
+++
@@ -1,8 +0,0 @@
-require 'compass/import-once/activate'
-# Require any additional compass plugins here.
-
-# Set this to the root of your project when deployed:
-http_path = "/"
-images_dir = "app/public/img"
-images_path = "app/res/img"
-http_generated_images_path = "/img"
