457c93cbf5b1116a7a844793a170ed156ae08002
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-02-10 15:33:34 +0100

    Simplify config a lot

---
+++ config/index.js
@@ -0,0 +1,10 @@
+/* eslint no-var: 0 */
+
+var baseUrl = process.env.SHOUTIT_BASE_URL || "";
+
+module.exports = {
+  baseUrl: baseUrl,
+  googleMapsKey: "AIzaSyBTB6-OnMETp1wjS8ZnUugqrlW5UcdEkgc",
+  apiUrl: "http://dev.api.shoutit.com/v2/",
+  imagesPath: baseUrl + "/images"
+};
