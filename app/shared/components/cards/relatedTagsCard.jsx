6a9898cc66347a7b21f405e669c513c56ab3edfe
p0o <pooria_az@ymail.com>
2016-02-12 22:41:52 +0330

    refactor related tags card as a stateless function #31

--- app/shared/components/cards/relatedTagsCard.jsx
+++ app/shared/components/cards/relatedTagsCard.jsx
@@ -1,41 +1,31 @@
 import React from 'react';
-import {Icon, Grid, Column} from '../helper';
+import {Icon, Grid, Column, Progress} from '../helper';
 
-export default React.createClass({
-    displayName: "relatedTagsCard",
-
-    contextTypes: {
-        params: React.PropTypes.object
-    },
-
-    render() {
-        return (
-            <section className="si-card">
-                <div className="card-header">
-                    <h3>related tags</h3>
-                </div>
-                <Grid fluid={true}>
-                    <Column fluid={true} clear={true} size="3" className="card-list-img">
-                        <Icon name="tag" className="tag-card-icon"/>
-                    </Column>
-                    <Column fluid={true} size="9" className="card-list-item">
-                        <span>Lenovo Desktops</span>
-                    </Column>
-                    <Column fluid={true} size="3" >
-                        <Icon name="listen" className="card-listen-btn"/>
-                    </Column>
-                    <Column fluid={true} clear={true} size="3" className="card-list-img">
-                        <Icon name="tag" className="tag-card-icon" />
-                    </Column>
-                    <Column fluid={true} size="9" className="card-list-item">
-                        <span>Lenovo Tablets</span>
-                    </Column>
-                    <Column fluid={true} size="3" >
-                        <Icon name="listen" className="card-listen-btn"/>
-                    </Column>
-                </Grid>
-            </section>
+export default function RelatedTags(props) {
+  return (
+    <section className="si-card">
+      <div className="card-header">
+        <h3>related tags</h3>
+      </div>
+      { props.loading?
+          <Progress />
+        :
+          props.tags.map((item, idx) => {
+            return (
+              <Grid fluid={true} key={`related-tags-card-${idx}`}>
+                <Column fluid={true} clear={true} size="3" className="card-list-img">
+                  <Icon name="tag" className="tag-card-icon"/>
+                </Column>
+                <Column fluid={true} size="9" className="card-list-item">
+                  <span>{ item.name }</span>
+                </Column>
+                <Column fluid={true} size="3">
+                  <Icon name="listen" className="card-listen-btn"/>
+                </Column>
+              </Grid>
             );
-
-    }
-});
+          })
+      }
+    </section>
+  );
+}
