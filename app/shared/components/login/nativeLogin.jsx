3c76765a8ecc01a69bcd5d151f594e7ecba4f588
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-02-13 20:46:32 +0100

    Lint file

--- app/shared/components/login/nativeLogin.jsx
+++ app/shared/components/login/nativeLogin.jsx
@@ -1,34 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Link} from 'react-router';
-import {Input,Button} from 'react-bootstrap';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Input, Button } from "react-bootstrap";
 
 export default React.createClass({
+
   displayName: "NativeLogin",
+
+  onLoginSubmit(e) {
+    e.preventDefault();
+
+    const flux = this.props.flux;
+    const email = ReactDOM.findDOMNode(this.refs.email).children[0].value;
+    const pass = ReactDOM.findDOMNode(this.refs.pass).children[0].value;
+
+    if (email && pass) {
+      flux.actions.login("shoutit",{email:email, pass:pass});
+    }
+  },
 
   render() {
     return(
       <div>
-        <form onSubmit={this.onLoginSubmit}>
-          <Input ref='email' type='email' placeholder='Email or Username' className='input-email' />
-          <Input ref='pass' type='password' placeholder='Password' className='input-pass' />
-          <Button bsSize='large' type='submit' block
-              className={this.props.logingIn? 'btn-signin btn-signin-disabled':'btn-signin'}>
-          {this.props.logingIn? 'Logging in...': 'Log in'}</Button>
-          <Input type='checkbox' label='Keep me logged in' />
+        <form onSubmit={ this.onLoginSubmit }>
+          <Input ref="email" type="email" placeholder="Email or Username" className="input-email" />
+          <Input ref="pass" type="password" placeholder="Password" className="input-pass" />
+          <Button bsSize="large" type="submit" block
+              className={ this.props.logingIn ? "btn-signin btn-signin-disabled" : "btn-signin" }>
+          {this.props.logingIn? "Logging in...": "Log in"}</Button>
+          <Input type="checkbox" label="Keep me logged in" />
         </form>
       </div>
     );
-  },
-
-  onLoginSubmit(e) {
-    e.preventDefault();
-    let flux = this.props.flux;
-
-    let email = ReactDOM.findDOMNode(this.refs.email).children[0].value;
-    let pass = ReactDOM.findDOMNode(this.refs.pass).children[0].value;
-
-    if(email && pass)
-      flux.actions.login('shoutit',{email:email, pass:pass});
   }
+
 });
