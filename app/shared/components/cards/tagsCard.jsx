fef9cfbb98f00926e4b10f4d0b997cd68bb840b8
p0o <pooria_az@ymail.com>
2016-02-14 14:31:51 +0330

    fix indents in tags card #31

--- app/shared/components/cards/tagsCard.jsx
+++ app/shared/components/cards/tagsCard.jsx
@@ -4,39 +4,39 @@ import {Icon, Column, Grid, Progress} from '../helper';
 import TagListenButton from '../general/tagListenButton.jsx';
 
 const TagsCard = (props, context) => {
-    const {loading, tags} = props;
+  const {loading, tags} = props;
 
-    return (
-        <section className="si-card">
-            <div className="card-header">
-                <h3>suggested tags</h3>
-            </div>
-            {loading?
-                <Progress />
-                :
-                tags.map((tag, idx) => {
-                    return (
-                        <Grid fluid={true} key={`card-tag-${idx}`}>
-                            <Column fluid={true} clear={true} size="3" className="card-list-img">
-                                <Icon name="tag" className="tag-card-icon"/>
-                            </Column>
-                            <Column fluid={true} size="9" className="card-list-item">
-                                <Link to={`/tag/${tag.name}`}>{ tag.name }</Link>
-                            </Column>
-                            <Column fluid={true} size="3" >
-                                <TagListenButton hasTitle={false} tag={tag} flux={context.flux}/>
-                            </Column>
-                        </Grid>
-                    );
-                })
-            }
-        </section>
-    );
+  return (
+    <section className="si-card">
+      <div className="card-header">
+        <h3>suggested tags</h3>
+      </div>
+      {loading ?
+        <Progress />
+        :
+        tags.map((tag, idx) => {
+          return (
+            <Grid fluid={true} key={`card-tag-${idx}`}>
+              <Column fluid={true} clear={true} size="3" className="card-list-img">
+                <Icon name="tag" className="tag-card-icon"/>
+              </Column>
+              <Column fluid={true} size="9" className="card-list-item">
+                <Link to={`/tag/${tag.name}`}>{ tag.name }</Link>
+              </Column>
+              <Column fluid={true} size="3">
+                <TagListenButton hasTitle={false} tag={tag} flux={context.flux}/>
+              </Column>
+            </Grid>
+          );
+        })
+      }
+    </section>
+  );
 };
 
 // Getting flux from context
 TagsCard.contextTypes = {
-    flux: React.PropTypes.object
+  flux: React.PropTypes.object
 };
 
 export default TagsCard;
