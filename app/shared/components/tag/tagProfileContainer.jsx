0f6373e9ac67cd988d6ed670a8718a26dbd8a44f
p0o <pooria_az@ymail.com>
2016-02-16 01:08:58 +0330

    fix the problem with empty related tags results

--- app/shared/components/tag/tagProfileContainer.jsx
+++ app/shared/components/tag/tagProfileContainer.jsx
@@ -29,10 +29,13 @@ export default React.createClass({
       tagName = params.tagName,
       tagEntry = this.state.tags.tags[tagName];
 
+    const relatedTagIsNotLoaded = tagEntry && !tagEntry.related.loading &&
+      !tagEntry.related.err && !tagEntry.related.list.length;
+
     if (!this.state.tags.loading && !tagEntry && tagEntry !== null) {
       flux.actions.loadTag(tagName);
     }
-    if(tagEntry && !tagEntry.related.loading && !tagEntry.related.list.length) {
+    if(relatedTagIsNotLoaded) {
       flux.actions.loadTagRelated(tagName);
     }
   },
