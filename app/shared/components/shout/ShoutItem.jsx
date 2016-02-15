e41a07f624be95dfb5f77d7c6a1661fc22722ec9
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-02-15 19:31:30 +0100

    New ShoutItem component

--- app/shared/components/shout/ShoutItem.jsx
+++ app/shared/components/shout/ShoutItem.jsx
@@ -1,9 +1,57 @@
 import React from "react";
+import currencyFormatter from "currency-formatter";
 
-export default function ShoutItem({ title }) {
+if (process.env.BROWSER) {
+  require("styles/components/ShoutItem.scss");
+}
+
+export default function ShoutItem({
+  shout,
+  outline=false,            // show a border instead of a shadow
+  thumbnailRatio=4/3,       // 4/3, 16/9 etc.
+  alwaysShowThumbnail=true  // show thumbnail also when not available
+}) {
+  const { price, currency: code, title, user, thumbnail } = shout;
+
+  let className = "ShoutItem";
+  if (outline) {
+    className += ` ${outline}`;
+  }
+  if (!thumbnail) {
+    className += " no-thumbnail";
+  }
+
+  let thumbnailStyle, thumbWrapperStyle;
+  if (alwaysShowThumbnail || thumbnail) {
+    thumbnailStyle = {
+      backgroundImage: `url("${thumbnail}")`
+    };
+    thumbWrapperStyle = {
+      paddingBottom: `${100/thumbnailRatio}%`
+    };
+  }
+
   return (
-    <div style={{border: "1px solid red"}}>
-      { title }
+    <div className={ className }>
+
+      { (alwaysShowThumbnail || thumbnail) &&
+        <div className="ShoutItem-thumbwrapper" style={ thumbWrapperStyle }>
+          <div className="ShoutItem-thumb"
+            style={ thumbnailStyle } />
+        </div>
+      }
+
+      <div className="ShoutItem-detail">
+        <span className="ShoutItem-title">
+          { title }
+        </span>
+        <span className="ShoutItem-name">
+          { user.name }
+        </span>
+        <span className="ShoutItem-price">
+          { currencyFormatter.format(price, { code } )}
+        </span>
+      </div>
     </div>
   );
 }
