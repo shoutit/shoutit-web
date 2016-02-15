d724d31497bbc118342f42c89dcb8cbcf681c19e
p0o <pooria_az@ymail.com>
2016-02-15 14:12:49 +0330

    add suggest shout card to discover page

--- app/shared/components/discover/discoverHome.jsx
+++ app/shared/components/discover/discoverHome.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import {Grid, Column} from '../helper';
+import React from "react";
+import { Grid, Column } from "../helper";
+import { SuggestShoutCard } from "../cards";
 
 export default React.createClass({
   displayName: "DiscoverHome",
@@ -15,11 +16,13 @@ export default React.createClass({
       flux: this.props.flux,
       params: this.props.params,
       location: this.props.location
-    }
+    };
   },
 
   render() {
-    const {flux, params} = this.props;
+    const { suggestions, flux, params } = this.props;
+    const shoutsData = suggestions.data? suggestions.data.shouts.list[0]: null;
+
     return (
       <div className="discover-holder">
         <Grid >
@@ -27,7 +30,10 @@ export default React.createClass({
             { React.cloneElement(this.props.children, {flux, params}) }
           </Column>
           <Column size="3">
-
+            <SuggestShoutCard
+              shout={ shoutsData }
+              loading={ suggestions.data && suggestions.data.shouts.loading }
+            />
           </Column>
         </Grid>
       </div>
