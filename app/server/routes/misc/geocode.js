14701fc6593f0d28f59aa68748c27646ea99f0b1
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-02-15 22:24:35 +0100

    Add some logging for the geocode...

--- app/server/routes/misc/geocode.js
+++ app/server/routes/misc/geocode.js
@@ -1,5 +1,8 @@
 /* eslint no-console: 0 */
 
+import debug from "debug";
+const log = debug("shoutit:server:geocode");
+
 export default function(client) {
   return (req, res) => {
     const options = {
@@ -7,6 +10,7 @@ export default function(client) {
         "X-Forwarded-For": req.connection.remoteAddress
       }
     };
+    log("Sending X-Forwarded-For header: %s", req.connection.remoteAddress);
     client.geocode(req.session, req.query, options)
       .on("success", data => res.json(data))
       .on("fail", (data, response) => res.status(response.statusCode).json(data))
