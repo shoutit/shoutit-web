df06ea4fbd4fd2522b142836ff2df4b36a560da6
p0o <pooria_az@ymail.com>
2016-02-16 21:13:12 +0330

    refactor tooltip component to load the data

--- app/shared/components/general/profileInfoTooltip.jsx
+++ app/shared/components/general/profileInfoTooltip.jsx
@@ -4,57 +4,49 @@ import Separator from '../general/separator.jsx';
 import UserImage from '../user/userImage.jsx';
 import CountryFlag from '../helper/CountryFlag.jsx';
 
-export default React.createClass({
-  displayName: 'ProfileInfoTooltip',
+import { imagesPath } from "../../../../config";
 
-  propTypes: {
-    user: React.PropTypes.object
-  },
+export default function ProfileInfoTooltip(props) {
+  const { user, loading } = this.props;
+  const defaultCoverImg = imagesPath + "/pattern@2x.png";
 
-  render() {
-    let name = this.props.user.name,
-      username = this.props.user.username,
-      image = this.props.user.image,
-      country = this.props.user.location.country;
-
-    return (
-      <div className="profile-info-tooltip">
-        <div className="tooltip-box">
-          <Grid fluid={true} className="profile-cover">
-          </Grid>
-          <Grid fluid={true} className="profile-context">
-            <Column fluid={true} clear={true} size="13">
-              <UserImage image={image} size="64" type="rounded" className="profile-context-image"/>
-              <h3>{name}</h3>
-              <h4>{username}</h4>
-            </Column>
-            <Column fluid={true} size="2">
-              <Icon name="listen" className="profile-context-listen"/>
-            </Column>
-          </Grid>
-          <Separator />
-          <Grid fluid={true} className="profile-actions">
-            <Column fluid={true} clear={true} size="5">
-              <div className="si-country-button">
-                <CountryFlag code={ country }/>
-                {country}
-              </div>
-            </Column>
-            <Column fluid={true} size="5">
-              <div className="si-listening-button">
-                <Icon name="listening"/>
-                Listening
-              </div>
-            </Column>
-            <Column fluid={true} size="5">
-              <div className="si-message-button">
-                <Icon name="message"/>
-                Message
-              </div>
-            </Column>
-          </Grid>
-        </div>
+  return (
+    <div className="profile-info-tooltip">
+      <div className="tooltip-box">
+        {loading?
+          <Progress />
+        : user.location &&
+          <div>
+            <Grid fluid={true} className="profile-cover" backgroundImage={ `url(${user.cover || defaultCoverImg}` }>
+            </Grid>
+            <Grid fluid={true} className="profile-context">
+              <Column fluid={true} clear={true} size="13">
+                <UserImage image={ user.image } size="64" type="rounded" className="profile-context-image"/>
+                <h3>{ user.name }</h3>
+                <h4>{ user.username }</h4>
+              </Column>
+              <Column fluid={true} size="2">
+                <Icon name="listen" className="profile-context-listen"/>
+              </Column>
+            </Grid>
+            <Separator />
+            <Grid fluid={true} className="profile-actions">
+              <Column fluid={true} clear={true} size="7">
+                <div className="si-country-button">
+                  <CountryFlag code={ user.location.country }/>
+                  { user.location.country }
+                </div>
+              </Column>
+              <Column fluid={true} size="7">
+                <div className="si-listening-button">
+                  <Icon name="listening"/>
+                  Listening
+                </div>
+              </Column>
+            </Grid>
+          </div>
+        }
       </div>
-    );
-  }
-});
+    </div>
+  );
+}
