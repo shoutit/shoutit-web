4daaa9a8209b7a37d6f214ce1429f530b49a1786
p0o <pooria_az@ymail.com>
2016-02-16 20:22:15 +0330

    fix indentions

--- app/shared/components/general/profileInfoTooltip.jsx
+++ app/shared/components/general/profileInfoTooltip.jsx
@@ -5,56 +5,56 @@ import UserImage from '../user/userImage.jsx';
 import CountryFlag from '../helper/CountryFlag.jsx';
 
 export default React.createClass({
-    displayName: 'ProfileInfoTooltip',
+  displayName: 'ProfileInfoTooltip',
 
-    propTypes: {
-        user: React.PropTypes.object
-    },
+  propTypes: {
+    user: React.PropTypes.object
+  },
 
-    render() {
-        let name = this.props.user.name,
-            username = this.props.user.username,
-            image = this.props.user.image,
-            country = this.props.user.location.country;
+  render() {
+    let name = this.props.user.name,
+      username = this.props.user.username,
+      image = this.props.user.image,
+      country = this.props.user.location.country;
 
-        return (
-           <div className="profile-info-tooltip">
-            <div className="tooltip-box">
-                <Grid fluid={true} className="profile-cover">
-                </Grid>
-                <Grid fluid={true} className="profile-context">
-                    <Column fluid={true} clear={true} size="13">
-                        <UserImage image={image} size="64" type="rounded" className="profile-context-image"/>
-                        <h3>{name}</h3>
-                        <h4>{username}</h4>
-                    </Column>
-                    <Column fluid={true} size="2">
-                        <Icon name="listen" className="profile-context-listen" />
-                    </Column>
-                </Grid>
-                <Separator />
-                <Grid fluid={true} className="profile-actions">
-                    <Column fluid={true} clear={true} size="5">
-                        <div className="si-country-button">
-                            <CountryFlag code={ country } />
-                            {country}
-                        </div>
-                    </Column>
-                    <Column fluid={true} size="5">
-                        <div className="si-listening-button">
-                            <Icon name="listening" />
-                            Listening
-                        </div>
-                    </Column>
-                    <Column fluid={true}  size="5">
-                        <div className="si-message-button">
-                            <Icon name="message" />
-                            Message
-                        </div>
-                    </Column>
-                </Grid>
-            </div>
-           </div>
-        );
-    }
+    return (
+      <div className="profile-info-tooltip">
+        <div className="tooltip-box">
+          <Grid fluid={true} className="profile-cover">
+          </Grid>
+          <Grid fluid={true} className="profile-context">
+            <Column fluid={true} clear={true} size="13">
+              <UserImage image={image} size="64" type="rounded" className="profile-context-image"/>
+              <h3>{name}</h3>
+              <h4>{username}</h4>
+            </Column>
+            <Column fluid={true} size="2">
+              <Icon name="listen" className="profile-context-listen"/>
+            </Column>
+          </Grid>
+          <Separator />
+          <Grid fluid={true} className="profile-actions">
+            <Column fluid={true} clear={true} size="5">
+              <div className="si-country-button">
+                <CountryFlag code={ country }/>
+                {country}
+              </div>
+            </Column>
+            <Column fluid={true} size="5">
+              <div className="si-listening-button">
+                <Icon name="listening"/>
+                Listening
+              </div>
+            </Column>
+            <Column fluid={true} size="5">
+              <div className="si-message-button">
+                <Icon name="message"/>
+                Message
+              </div>
+            </Column>
+          </Grid>
+        </div>
+      </div>
+    );
+  }
 });
