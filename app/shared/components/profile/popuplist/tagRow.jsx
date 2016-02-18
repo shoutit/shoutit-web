6b2a61d6cf396027dbe0bddc88a37b3e56c5e124
p0o <pooria_az@ymail.com>
2016-02-19 02:35:53 +0330

    make TagRow as a stateless component

--- app/shared/components/profile/popuplist/tagRow.jsx
+++ app/shared/components/profile/popuplist/tagRow.jsx
@@ -4,47 +4,31 @@ import {Grid, Column} from '../../helper';
 import TagListenButton from '../../general/tagListenButton.jsx';
 import UserImage from '../../user/userImage.jsx';
 
-export default React.createClass({
-    displayName: "TagRow",
+export default function TagRow(props) {
+  const tag = JSON.parse(JSON.stringify(this.props.tag));
+  const { onListeningChange, onListenTag, onStopListenTag } = this.props;
 
-    contextTypes: {
-        flux: React.PropTypes.object
-    },
-
-    onButtonChange(ev) {
-        if(this.props.onChange) {
-            this.props.onChange(ev);
-        }
-    },
-
-    render() {
-        let tag = JSON.parse(JSON.stringify(this.props.tag));
-        let flux = this.context.flux;
-
-        if(tag.name) {
-            return (
-                <Grid fluid={true} className="popuplist-row">
-                    <Column fluid={true} clear={true} size="2" >
-                        <Link to={`/tag/${encodeURIComponent(tag.name)}`}>
-                            <UserImage size="32" image={tag.image} type="circle"/>
-                        </Link>
-                    </Column>
-                    <Column fluid={true} size="10" className="popuplist-text-row">
-                        <Link to={`/tag/${encodeURIComponent(tag.name)}`}>
-                            {tag.name}
-                        </Link>
-                    </Column>
-                    <Column fluid={true} size="3" style={{paddingTop: "5px"}}>
-                        <TagListenButton tag={tag}
-                                         hasTitle={false}
-                                         onChange={this.}
-                                         flux={flux}
-                                         />
-                    </Column>
-                </Grid>
-            );
-        } else {
-            return null;
-        }
-    }
-});
+  return (
+    <Grid fluid={true} className="popuplist-row">
+      <Column fluid={true} clear={true} size="2">
+        <Link to={`/tag/${encodeURIComponent(tag.name)}`}>
+          <UserImage size="32" image={tag.image} type="circle"/>
+        </Link>
+      </Column>
+      <Column fluid={true} size="10" className="popuplist-text-row">
+        <Link to={`/tag/${encodeURIComponent(tag.name)}`}>
+          {tag.name}
+        </Link>
+      </Column>
+      <Column fluid={true} size="3" style={{paddingTop: "5px"}}>
+        <TagListenButton
+          tag={ tag }
+          hasTitle={ false }
+          onChange={ onListeningChange }
+          onListenTag={ onListenTag }
+          onStopListenTag={ onStopListenTag }
+        />
+      </Column>
+    </Grid>
+  );
+}
