06842df47e7a7a07ee927419f6114c47f98469d9
p0o <pooria_az@ymail.com>
2016-02-19 03:03:49 +0330

    fix indents

--- app/shared/components/general/listenButton.jsx
+++ app/shared/components/general/listenButton.jsx
@@ -6,98 +6,99 @@ import statuses from '../../consts/statuses';
 const {LISTEN_BTN_LOADING} = statuses;
 
 export default React.createClass({
-    mixins: [ new StoreWatchMixin('users')],
-    displayName: "listenButton",
+  propTypes: {
+    flux: React.PropTypes.object.isRequired,
+    username: React.PropTypes.string.isRequired,
+    onChange: React.PropTypes.func,
+    hasTitle: React.PropTypes.bool
+  },
 
-    propTypes: {
-        flux: React.PropTypes.object.isRequired,
-        username: React.PropTypes.string.isRequired,
-        onChange: React.PropTypes.func,
-        hasTitle: React.PropTypes.bool
-    },
+  mixins: [
+    new StoreWatchMixin("users")
+  ],
 
-    getDefaultProps() {
-        return {
-            hasTitle: true
-        }
-    },
+  getDefaultProps() {
+    return {
+      hasTitle: true
+    };
+  },
 
-    getStateFromFlux() {
-        // cloning objects as a work around to avoid mutation of store objects
-        let user = JSON.parse(JSON.stringify(this.props.flux.store('users').getState()
-                    .users[this.props.username])),
-            loggedUser = this.props.flux.store('users').getState().user;
+  getStateFromFlux() {
+    // cloning objects as a work around to avoid mutation of store objects
+    let user = JSON.parse(JSON.stringify(this.props.flux.store('users').getState()
+        .users[this.props.username])),
+      loggedUser = this.props.flux.store('users').getState().user;
 
-        return { user: user , loggedUser: loggedUser };
-    },
+    return {user: user, loggedUser: loggedUser};
+  },
 
-    shouldComponentUpdate(nextProps, nextState) {
-        if(nextState.user.is_listening !== this.state.user.is_listening) {
-            // call to inform parent for the change
-            if(nextState.user.username === this.state.user.username && this.props.onChange) {
-                this.props.onChange({
-                    isListening: nextState.user.is_listening,
-                    username: nextState.user.username,
-                    name: nextState.user.name
-                });
-            }
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.user.is_listening !== this.state.user.is_listening) {
+      // call to inform parent for the change
+      if (nextState.user.username === this.state.user.username && this.props.onChange) {
+        this.props.onChange({
+          isListening: nextState.user.is_listening,
+          username: nextState.user.username,
+          name: nextState.user.name
+        });
+      }
 
-            return true;
-        } else if(nextState.user.fluxStatus !== this.state.fluxStatus) {
-            return true;
-        } else {
-            return false;
-        }
-    },
-
-    render() {
-        let user = this.state.user,
-            clickable = true,
-            title = "Listen",
-            iconName = "listen",
-            className = "si-shelf-button";
-
-        // disable Listening button if user is not logged in
-        if (this.state.loggedUser) {
-            let isListening = user.is_listening;
-
-            if(isListening) {
-                title = "Listening";
-                iconName = "listening_to";
-            }
-
-            if(user.fluxStatus === LISTEN_BTN_LOADING) {
-                title = "[Loading]";
-                className = className + " loading";
-            }
-        } else {
-            clickable = false;
-            className = className + " disabled";
-        }
-
-        return (
-            <div className={className} onClick={clickable? this.toggleListen: null}>
-                <div className="img-holder">
-                    <Icon name={iconName} style={{margin: '0 auto'}}/>
-                </div>
-                {this.props.hasTitle?
-                    <div className="text-holder">
-                        {title}
-                    </div>
-                    :null
-                }
-            </div>
-        );
-    },
-
-    toggleListen() {
-        const { user } = this.state;
-        const { onStopListen, onListen } = this.props;
-
-        if (user.is_listening) {
-            onStopListen(user.username);
-        } else {
-            onListen(user.username);
-        }
+      return true;
+    } else if (nextState.user.fluxStatus !== this.state.fluxStatus) {
+      return true;
+    } else {
+      return false;
     }
+  },
+
+  render() {
+    let user = this.state.user,
+      clickable = true,
+      title = "Listen",
+      iconName = "listen",
+      className = "si-shelf-button";
+
+    // disable Listening button if user is not logged in
+    if (this.state.loggedUser) {
+      const isListening = user.is_listening;
+
+      if (isListening) {
+        title = "Listening";
+        iconName = "listening_to";
+      }
+
+      if (user.fluxStatus === LISTEN_BTN_LOADING) {
+        title = "[Loading]";
+        className = className + " loading";
+      }
+    } else {
+      clickable = false;
+      className = className + " disabled";
+    }
+
+    return (
+      <div className={className} onClick={clickable? this.toggleListen: null}>
+        <div className="img-holder">
+          <Icon name={iconName} style={{margin: "0 auto"}}/>
+        </div>
+        {this.props.hasTitle ?
+          <div className="text-holder">
+            {title}
+          </div>
+          : null
+        }
+      </div>
+    );
+  },
+
+  toggleListen() {
+    const { user } = this.state;
+    const { onStopListen, onListen } = this.props;
+
+    if (user.is_listening) {
+      onStopListen(user.username);
+    } else {
+      onListen(user.username);
+    }
+  }
 });
--- app/shared/components/general/tagListenButton.jsx
+++ app/shared/components/general/tagListenButton.jsx
@@ -5,92 +5,93 @@ import statuses from '../../consts/statuses.js';
 
 const {LISTEN_BTN_LOADING} = statuses;
 
-if(process.env.BROWSER) {
-    require("styles/components/shelf_button.scss");
+if (process.env.BROWSER) {
+  require("styles/components/shelf_button.scss");
 }
 
 export default React.createClass({
-    mixins: [new StoreWatchMixin('users')],
-    displayName: "TagListenButton",
+  mixins: [
+    new StoreWatchMixin("users")
+  ],
 
-    getDefaultProps() {
-        return {
-            hasTitle: true
-        }
-    },
+  getDefaultProps() {
+    return {
+      hasTitle: true
+    };
+  },
 
-    getStateFromFlux() {
-        let loggedIn = this.props.flux.store('users').getState().user;
+  getStateFromFlux() {
+    const loggedIn = this.props.flux.store('users').getState().user;
 
-        return {
-            loggedIn
-        }
-    },
-
-    shouldComponentUpdate(nextProps) {
-        if(nextProps.tag.is_listening !== this.props.tag.is_listening) {
-            // call parent for change
-            if(nextProps.tag.name === this.props.tag.name && this.props.onChange) {
-                this.props.onChange({
-                    name: nextProps.tag.name,
-                    isListening: nextProps.tag.is_listening
-                });
-            }
-            return true;
-        } else if (nextProps.tag.fluxStatus !== this.props.tag.fluxStatus) {
-            return true;
-        } else {
-            return false;
-        }
-    },
-
-    render() {
-        let tag = this.props.tag,
-            clickable = true,
-            title = "Listen",
-            iconName = "listen",
-            className = "si-shelf-button";
-
-        // no need to load Listening button if user is not logged in
-        if (this.state.loggedIn) {
-            let isListening = tag.is_listening;
-
-            if(isListening) {
-                title = "Listening";
-                iconName = "listening_to";
-            }
-
-            if(tag.fluxStatus === LISTEN_BTN_LOADING) {
-                title = "[Loading]";
-                className = className + " loading";
-            }
-        } else {
-            clickable = false;
-            className = className + " disabled";
-        }
-
-        return (
-            <div className={className} onClick={clickable? this.toggleListen: null}>
-                <div className="img-holder">
-                    <Icon name={iconName} />
-                </div>
-                {this.props.hasTitle?
-                    <div className="text-holder">
-                        {title}
-                    </div>
-                    :null
-                }
-            </div>
-        );
-    },
-
-    toggleListen() {
-        const { tag, onStopListenTag, onListenTag } = this.props;
-
-        if (tag.is_listening) {
-            onStopListenTag(tag.name);
-        } else {
-            onListenTag(tag.name);
-        }
+    return {
+      loggedIn
     }
+  },
+
+  shouldComponentUpdate(nextProps) {
+    if (nextProps.tag.is_listening !== this.props.tag.is_listening) {
+      // call parent for change
+      if (nextProps.tag.name === this.props.tag.name && this.props.onChange) {
+        this.props.onChange({
+          name: nextProps.tag.name,
+          isListening: nextProps.tag.is_listening
+        });
+      }
+      return true;
+    } else if (nextProps.tag.fluxStatus !== this.props.tag.fluxStatus) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  render() {
+    let tag = this.props.tag,
+      clickable = true,
+      title = "Listen",
+      iconName = "listen",
+      className = "si-shelf-button";
+
+    // no need to load Listening button if user is not logged in
+    if (this.state.loggedIn) {
+      let isListening = tag.is_listening;
+
+      if (isListening) {
+        title = "Listening";
+        iconName = "listening_to";
+      }
+
+      if (tag.fluxStatus === LISTEN_BTN_LOADING) {
+        title = "[Loading]";
+        className = className + " loading";
+      }
+    } else {
+      clickable = false;
+      className = className + " disabled";
+    }
+
+    return (
+      <div className={className} onClick={clickable? this.toggleListen: null}>
+        <div className="img-holder">
+          <Icon name={iconName}/>
+        </div>
+        {this.props.hasTitle ?
+          <div className="text-holder">
+            {title}
+          </div>
+          : null
+        }
+      </div>
+    );
+  },
+
+  toggleListen() {
+    const { tag, onStopListenTag, onListenTag } = this.props;
+
+    if (tag.is_listening) {
+      onStopListenTag(tag.name);
+    } else {
+      onListenTag(tag.name);
+    }
+  }
 });
