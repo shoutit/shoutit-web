8fa66f146f6cda5e24cf3ea7f25dd91179dbfda1
p0o <pooria_az@ymail.com>
2016-02-18 15:46:51 +0330

    fix indentions

--- app/shared/components/profile/popuplist/popuplist.jsx
+++ app/shared/components/profile/popuplist/popuplist.jsx
@@ -11,146 +11,147 @@ import {Scrollbars} from 'react-custom-scrollbars';
 import SearchBar from './searchBar.jsx';
 
 export default React.createClass({
-    displayName: "PopupList",
-    mixins: [ StoreWatchMixin('users', 'tags') ],
+  displayName: "PopupList",
+  mixins: [StoreWatchMixin('users', 'tags')],
 
-    propTypes: {
-        username: React.PropTypes.string.isRequired,
-        type: React.PropTypes.oneOf(['Listeners', 'Listening', 'Tags']),
-        open: React.PropTypes.bool.isRequired
-    },
+  propTypes: {
+    username: React.PropTypes.string.isRequired,
+    type: React.PropTypes.oneOf(['Listeners', 'Listening', 'Tags']),
+    open: React.PropTypes.bool.isRequired
+  },
 
-    contextTypes: {
-        flux: React.PropTypes.object
-    },
+  contextTypes: {
+    flux: React.PropTypes.object
+  },
 
-    getStateFromFlux() {
-        const {flux} = this.context;
-        const userStore = flux.store("users").getState();
+  getStateFromFlux() {
+    const {flux} = this.context;
+    const userStore = flux.store("users").getState();
 
-        return {
-            tags: flux.store("tags").getState().tags,
-            users: userStore.users,
-            user: userStore.user,
-            listens: userStore.listens,
-            loading: userStore.loading
-        }
-    },
-
-    componentWillMount() {
-        // Initializing helper
-        this.helper = popuplistHelper(this.props.type, this.props.username);
-    },
-
-    componentDidUpdate(prevProps) {
-        const {type, username} = this.props;
-        const {flux} = this.context;
-        // Setting helper
-        this.helper = popuplistHelper(type, username);
-        // condition to send action
-        if(prevProps.type !== type || prevProps.username !== username) {
-            switch(type) {
-            case 'Listeners':
-                flux.actions.loadUserListeners(username);
-                break;
-            case 'Listening':
-                flux.actions.loadUserListening(username);
-                break;
-            case 'Tags':
-                flux.actions.loadUserListeningTags(username);
-                break;
-            }
-        }
-    },
-
-    handleClose() {
-        if(this.props.onClose) {
-            this.props.onClose();
-        }
-    },
-
-    renderViewportSensor() {
-        const list = this.helper.getList(this.state);
-
-        if(this.state.loading && list && list.length) {
-            return (
-                <Grid fluid={true}>
-                    <Progress />
-                </Grid>);
-        } else {
-            return (
-                <Grid fluid={true}>
-                    <ViewportSensor onChange={this.onLastVisibleChange}></ViewportSensor>
-                </Grid>);
-        }
-    },
-
-    onLastVisibleChange(isVisible) {
-        if (isVisible) {
-            this.loadMore();
-        }
-    },
-
-    loadMore() {
-        const {type, username} = this.props;
-        const {flux} = this.context;
-
-        switch(type) {
-        case 'Listeners':
-            flux.actions.loadMoreUserListeners(username);
-            break;
-        case 'Listening':
-            flux.actions.loadMoreUserListening(username);
-            break;
-        case 'Tags':
-            flux.actions.loadMoreUserListeningTags(username);
-            break;
-        }
-    },
-
-    render() {
-        const title = this.helper.getTitle();
-        const list = this.helper.getList(this.state);
-
-        return(
-            <Dialog
-              contentClassName="popuplist-dialog"
-              modal={false}
-              open={this.props.open}
-              onRequestClose={this.handleClose}>
-                  <Grid fluid={true} className="popuplist-content">
-                  <h3>{title}</h3>
-                  <Separator />
-                  <Scrollbars style={{ width: 380, height: 320}} className="si-scrollbar">
-                      <SearchBar type={this.props.type}/>
-                      {list && list.length?
-                        this.props.type === "Tags"?
-                            list.map((tag, idx) => {
-                                return (
-                                    <TagRow key={"tags-" + '-' + idx}
-                                            tag={tag}
-                                            onChange={this.handleChange}
-                                            />
-                                    );
-                            })
-                            :
-                            /* Could be listeners or listenings - both use ListenerRow component*/
-                            list.map((item, idx) => {
-                                return(
-                                    <ListenerRow key={"listen-" + '-' + idx}
-                                                 user={item}
-                                                 onChange={this.handleChange}
-                                                 />
-                                    );
-                            })
-                        :
-                        <Progress />
-                      }
-                      {this.renderViewportSensor()}
-                  </Scrollbars>
-                  </Grid>
-            </Dialog>
-            );
+    return {
+      tags: flux.store("tags").getState().tags,
+      users: userStore.users,
+      user: userStore.user,
+      listens: userStore.listens,
+      loading: userStore.loading
     }
+  },
 
+  componentWillMount() {
+    // Initializing helper
+    this.helper = popuplistHelper(this.props.type, this.props.username);
+  },
+
+  componentDidUpdate(prevProps) {
+    const {type, username} = this.props;
+    const {flux} = this.context;
+    // Setting helper
+    this.helper = popuplistHelper(type, username);
+    // condition to send action
+    if (prevProps.type !== type || prevProps.username !== username) {
+      switch (type) {
+        case 'Listeners':
+          flux.actions.loadUserListeners(username);
+          break;
+        case 'Listening':
+          flux.actions.loadUserListening(username);
+          break;
+        case 'Tags':
+          flux.actions.loadUserListeningTags(username);
+          break;
+      }
+    }
+  },
+
+  handleClose() {
+    if (this.props.onClose) {
+      this.props.onClose();
+    }
+  },
+
+  renderViewportSensor() {
+    const list = this.helper.getList(this.state);
+
+    if (this.state.loading && list && list.length) {
+      return (
+        <Grid fluid={true}>
+          <Progress />
+        </Grid>);
+    } else {
+      return (
+        <Grid fluid={true}>
+          <ViewportSensor onChange={this.onLastVisibleChange}></ViewportSensor>
+        </Grid>);
+    }
+  },
+
+  onLastVisibleChange(isVisible) {
+    if (isVisible) {
+      this.loadMore();
+    }
+  },
+
+  loadMore() {
+    const {type, username} = this.props;
+    const {flux} = this.context;
+
+    switch (type) {
+      case 'Listeners':
+        flux.actions.loadMoreUserListeners(username);
+        break;
+      case 'Listening':
+        flux.actions.loadMoreUserListening(username);
+        break;
+      case 'Tags':
+        flux.actions.loadMoreUserListeningTags(username);
+        break;
+    }
+  },
+
+  render() {
+    const title = this.helper.getTitle();
+    const list = this.helper.getList(this.state);
+
+    return (
+      <Dialog
+        contentClassName="popuplist-dialog"
+        modal={false}
+        open={this.props.open}
+        onRequestClose={this.handleClose}>
+        <Grid fluid={true} className="popuplist-content">
+          <h3>{title}</h3>
+          <Separator />
+          <Scrollbars style={{ width: 380, height: 320}} className="si-scrollbar">
+            <SearchBar type={this.props.type}/>
+            {list && list.length ?
+              this.props.type === "Tags" ?
+                list.map((tag, idx) => {
+                  return (
+                    <TagRow
+                      key={`tags-popuplist-${idx}`}
+                      tag={tag}
+                      onChange={this.handleChange}
+                    />
+                  );
+                })
+                :
+                /* Could be listeners or listenings - both use ListenerRow component*/
+                list.map((item, idx) => {
+                  return (
+                    <ListenerRow
+                      key={`listens-popuplist-${idx}`}
+                      user={item}
+                      onChange={this.handleChange}
+                    />
+                  );
+                })
+              :
+              <Progress />
+            }
+            {this.renderViewportSensor()}
+          </Scrollbars>
+        </Grid>
+      </Dialog>
+    );
+  }
 });
