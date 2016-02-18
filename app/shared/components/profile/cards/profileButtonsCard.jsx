4bc48d813986f91517a5a33175499730b5a135a0
p0o <pooria_az@ymail.com>
2016-02-18 20:24:50 +0330

    fix indents

--- app/shared/components/profile/cards/profileButtonsCard.jsx
+++ app/shared/components/profile/cards/profileButtonsCard.jsx
@@ -8,99 +8,99 @@ import UserListeningTagsButton from '../../general/userListeningTagsButton.jsx';
 import Popuplist from '../popuplist/popuplist.jsx';
 
 export default React.createClass({
-    displayName: "ProfileButtonsCard",
+  displayName: "ProfileButtonsCard",
 
-    propTypes: {
-        user: React.PropTypes.object.isRequired
-    },
+  propTypes: {
+    user: React.PropTypes.object.isRequired
+  },
 
-    contextTypes: {
-        flux: React.PropTypes.object
-    },
+  contextTypes: {
+    flux: React.PropTypes.object
+  },
 
-    getInitialState() {
-        return {
-            activePopuplist: null
-        }
-    },
-
-    componentDidMount() {
-        this._notificationSystem = this.refs.notificationSystem;
-    },
-
-    componentDidUpdate() {
-        this._notificationSystem = this.refs.notificationSystem;
-    },
-
-    renderPopuplists() {
-        const {username} = this.props.user;
-        const { onListeningChange } = this.props;
-        return (
-            <Popuplist
-              open={Boolean(this.state.activePopuplist)}
-              onClose={this.onPopuplistClose}
-              username={username}
-              type={this.state.activePopuplist}
-              onListeningChange={ onListeningChange }
-            />
-        );
-    },
-
-    onPopuplistClose() {
-        this.setState({activePopuplist: null})
-    },
-
-    listOnClick(type) {
-        return () => {
-            this.setState({activePopuplist: type});
-        }
-    },
-
-    renderForOwner() {
-        let user = this.props.user;
-
-        return (
-            <Grid fluid={true} className="si-card" style={{paddingTop: "15px"}}>
-                <Column fluid={true} clear={true} size="5" onClick={this.listOnClick("Listeners")}>
-                    <ListenersButton user={user} />
-                </Column>
-                <Column fluid={true} size="5" onClick={this.listOnClick("Listening")}>
-                    <ListeningButton user={user} />
-                </Column>
-                <Column fluid={true} size="5" onClick={this.listOnClick("Tags")}>
-                    <UserListeningTagsButton user={user}/>
-                </Column>
-                {this.renderPopuplists()}
-            </Grid>
-            );
-    },
-
-    renderForViewers() {
-        let {username} = this.props.user;
-        let flux = this.context.flux;
-
-        return (
-            <Grid fluid={true} className="si-card" style={{paddingTop: "15px"}}>
-                <Column fluid={true} clear={true} size="5">
-                    <ListenersButton user={this.props.user} />
-                </Column>
-                <Column fluid={true} size="5">
-                    <MessageButton />
-                </Column>
-                <Column fluid={true} size="5">
-                    <ListenButton username={username} onChange={this.onUserListenChange} flux={flux}/>
-                </Column>
-                <NotificationSystem ref="notificationSystem" />
-            </Grid>
-        );
-    },
-
-    render() {
-        if(this.props.user.is_owner) {
-            return this.renderForOwner();
-        } else {
-            return this.renderForViewers();
-        }
+  getInitialState() {
+    return {
+      activePopuplist: null
     }
+  },
+
+  componentDidMount() {
+    this._notificationSystem = this.refs.notificationSystem;
+  },
+
+  componentDidUpdate() {
+    this._notificationSystem = this.refs.notificationSystem;
+  },
+
+  renderPopuplists() {
+    const {username} = this.props.user;
+    const { onListeningChange } = this.props;
+    return (
+      <Popuplist
+        open={Boolean(this.state.activePopuplist)}
+        onClose={ this.onPopuplistClose }
+        username={ username }
+        type={ this.state.activePopuplist }
+        onListeningChange={ onListeningChange }
+      />
+    );
+  },
+
+  onPopuplistClose() {
+    this.setState({activePopuplist: null})
+  },
+
+  listOnClick(type) {
+    return () => {
+      this.setState({activePopuplist: type});
+    }
+  },
+
+  renderForOwner() {
+    let user = this.props.user;
+
+    return (
+      <Grid fluid={true} className="si-card" style={{paddingTop: "15px"}}>
+        <Column fluid={true} clear={true} size="5" onClick={this.listOnClick("Listeners")}>
+          <ListenersButton user={user}/>
+        </Column>
+        <Column fluid={true} size="5" onClick={this.listOnClick("Listening")}>
+          <ListeningButton user={user}/>
+        </Column>
+        <Column fluid={true} size="5" onClick={this.listOnClick("Tags")}>
+          <UserListeningTagsButton user={user}/>
+        </Column>
+        {this.renderPopuplists()}
+      </Grid>
+    );
+  },
+
+  renderForViewers() {
+    let {username} = this.props.user;
+    let flux = this.context.flux;
+
+    return (
+      <Grid fluid={true} className="si-card" style={{paddingTop: "15px"}}>
+        <Column fluid={true} clear={true} size="5">
+          <ListenersButton user={this.props.user}/>
+        </Column>
+        <Column fluid={true} size="5">
+          <MessageButton />
+        </Column>
+        <Column fluid={true} size="5">
+          <ListenButton username={username} onChange={this.onUserListenChange} flux={flux}/>
+        </Column>
+        <NotificationSystem ref="notificationSystem"/>
+      </Grid>
+    );
+  },
+
+  render() {
+    if (this.props.user.is_owner) {
+      return this.renderForOwner();
+    } else {
+      return this.renderForViewers();
+    }
+  }
 
 });
