3db97a28b6480b38fda4bf7571e4eb1c3b7af220
p0o <pooria_az@ymail.com>
2016-02-24 15:30:56 +0330

    add animations to ui notification component

--- app/shared/components/helper/NotificationHost.jsx
+++ app/shared/components/helper/NotificationHost.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 if (process.env.BROWSER) {
   require("styles/components/NotificationHost");
@@ -7,12 +8,14 @@ if (process.env.BROWSER) {
 export default function NotificationHost({ notifications, onDismissClick }) {
   return (
     <div className="NotificationHost">
+      <ReactCSSTransitionGroup transitionName="notification" transitionEnterTimeout={100} transitionLeaveTimeout={200}>
       { notifications.map((notification, idx) =>
         <div className="NotificationHost-notification" key={`ui-notif-${idx}`}>
           { notification.message }
           <button className="NotificationHost-closeButton" onClick={ () => onDismissClick(notification.id) }>X</button>
         </div>
       )}
+      </ReactCSSTransitionGroup>
     </div>
   );
 }
