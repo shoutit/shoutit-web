b0772ff0079e1624353b1e491aaa79906fe2341a
p0o <pooria_az@ymail.com>
2016-02-27 17:35:48 +0330

    remove return from map for code simplicity

--- app/shared/components/discover/discoverPage.jsx
+++ app/shared/components/discover/discoverPage.jsx
@@ -77,18 +77,16 @@ export default React.createClass({
             {shouts.length?
               <Grid fluid={true}>
                 <h3 className="si-center-header">Shouts</h3>
-                {shouts.map((shout, idx) => {
-                  return (
-                    <ItemScope type="Product" key={"disShout-" + idx}>
-                      <ShoutPreview
-                        index={idx}
-                        shout={shout}
-                        gridView
-                        columnsPerRow={4}
-                      />
-                    </ItemScope>
-                  );
-                })}
+                {shouts.map((shout, idx) =>
+                  <ItemScope type="Product" key={"disShout-" + idx}>
+                    <ShoutPreview
+                      index={idx}
+                      shout={shout}
+                      gridView
+                      columnsPerRow={4}
+                    />
+                  </ItemScope>
+                )}
                 {this.renderViewportSensor()}
               </Grid>
               :
