da8bfc67954579fe80ad118317f77d6b389b01a6
p0o <pooria_az@ymail.com>
2016-03-04 15:20:45 +0330

    fix lint errors

--- app/shared/components/header/search.jsx
+++ app/shared/components/header/search.jsx
@@ -1,69 +1,73 @@
-import React from 'react';
-import {StoreWatchMixin} from 'fluxxor';
-import {Input, Button} from 'react-bootstrap';
-import Icon from '../helper/icon.jsx';
+import React from "react";
+import {StoreWatchMixin} from "fluxxor";
+import {Input, Button} from "react-bootstrap";
+import Icon from "../helper/icon.jsx";
 
 
 export default React.createClass({
-    displayName: "SearchInput",
-    mixins: [new StoreWatchMixin('locations')],
+  displayName: "SearchInput",
 
-    getStateFromFlux() {
-        return {
-            locations: this.props.flux.store('locations').getState()
-        };
-    },
+  mixins: [
+    new StoreWatchMixin("locations")
+  ],
 
-    renderSubmitButton() {
-        return (
-            <Button className="search-button"
-                    onClick={this.props.onSubmit}
-                    bsStyle="link"
-                    tabIndex={2}
-                    >
-                <Icon name="search-icon"/>
-            </Button>
-        );
-    },
+  getStateFromFlux() {
+    return {
+      locations: this.props.flux.store("locations").getState()
+    };
+  },
 
-    render() {
-        let currentCity = this.state.locations.current.city,
-            btnStyle = currentCity? 
-                    {height: this.props.height + 'px', padding: '0 20px'}: {height: this.props.height + 'px'},
-            beforeButton =
-                <div className="selectpicker">
-                    <Button onClick={this.props.onFocus(2)} 
-                            key="countrySelect" 
-                            style={btnStyle}
-                            className="dropdown-toggle">
-                        {currentCity || <Icon name="location_bar"/>}
-                    </Button>
-                </div>;
-        return (
-            <div>
-                <Input
-                    placeholder="Search Shoutit"
-                    ref='searchInput'
-                    type="text"
-                    buttonBefore={beforeButton}
-                    onChange={this.props.onChangeSearch}
-                    onFocus={this.props.onFocus(1)}
-                    onKeyUp={this.onKeyUp}
-                    value={this.props.term}
-                    tabIndex={1}
-                    accessKey="s"
-                    style={{height: this.props.height + 'px'}}
-                    />
-                {this.renderSubmitButton()}
-            </div>
-        );
-    },
+  renderSubmitButton() {
+    return (
+      <Button
+        className="search-button"
+        onClick={ this.props.onSubmit }
+        bsStyle="link"
+        tabIndex={ 2 } >
+        <Icon name="search-icon"/>
+      </Button>
+    );
+  },
 
-    onKeyUp(ev) {
-        if (ev.which === 13) {
-            this.props.onSubmit();
-        } else if (ev.which === 27) {
-            this.props.onBlur();
-        }
+  onKeyUp(ev) {
+    if (ev.which === 13) {
+      this.props.onSubmit();
+    } else if (ev.which === 27) {
+      this.props.onBlur();
     }
+  },
+
+  render() {
+    const currentCity = this.state.locations.current.city;
+    const btnStyle = currentCity ?
+        {height: this.props.height + "px", padding: "0 20px"} : {height: this.props.height + "px"};
+    const beforeButton =
+        <div className="selectpicker">
+          <Button
+            onClick={this.props.onFocus(2)}
+            key="countrySelect"
+            style={btnStyle}
+            className="dropdown-toggle">
+            {currentCity || <Icon name="location_bar"/>}
+          </Button>
+        </div>;
+    return (
+      <div>
+        <Input
+          placeholder="Search Shoutit"
+          ref="searchInput"
+          type="text"
+          buttonBefore={beforeButton}
+          onChange={this.props.onChangeSearch}
+          onFocus={this.props.onFocus(1)}
+          onKeyUp={this.onKeyUp}
+          value={this.props.term}
+          tabIndex={1}
+          accessKey="s"
+          style={{height: this.props.height + "px"}}
+        />
+        {this.renderSubmitButton()}
+      </div>
+    );
+  }
 });
