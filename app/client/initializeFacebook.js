b50715b6c169df69d8284f1c9c00c3f083279fcd
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-03-04 20:31:52 +0100

    Lint file

--- app/client/initializeFacebook.js
+++ app/client/initializeFacebook.js
@@ -22,7 +22,8 @@ window.fbAsyncInit = function() {
 };
 
 (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
+  var js;
+  var fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) {return;}
   js = d.createElement(s); js.id = id;
   js.src = "//connect.facebook.net/en_US/sdk.js";
