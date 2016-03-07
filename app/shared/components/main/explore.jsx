757f6f30ea05ed98cfaef9f6d631707ca65e7888
p0o <pooria_az@ymail.com>
2016-03-08 00:36:00 +0330

    refactor explore as a stateless function

--- app/shared/components/main/explore.jsx
+++ app/shared/components/main/explore.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import { Link } from "react-router";
-import { Grid, Column, Progress } from "../helper";
+import { Grid, Column } from "../helper";
 
-export default React.createClass({
-  displayName: 'explore',
-
-  renderExplore() {
-    const { categories } = this.props;
-
-    if (categories.length) {
-      // pick 6 unique random category
-      const selectedCategories = categories.slice(0, 6);
-
-      return (
-        selectedCategories.map(function (cat, i) {
-          // clear every three item
-          if (!cat) { return null;}
+export default function Explore({ categories = [] }) {
+  const selectedCategories = categories.slice(0, 6);
+  return (
+    <Grid>
+      <Column offset="3" size="9">
+        <h2>Explore Shouts</h2>
+        { selectedCategories.map(function (cat, i) {
           return (
             <Column size="3" clear={ i % 3 === 0 } key={`main-explore-${i}`}>
               <div className="si-item-box">
@@ -26,22 +19,8 @@ export default React.createClass({
               </div>
             </Column>
           );
-        })
-      );
-    } else {
-      return  <Progress /> ;
-    }
-  },
-
-  render() {
-
-    return (
-      <Grid>
-        <Column offset="3" size="9">
-          <h2>Explore Shouts</h2>
-          {this.renderExplore()}
-        </Column>
-      </Grid>
-    );
-  }
-});
+        }) }
+      </Column>
+    </Grid>
+  );
+}
