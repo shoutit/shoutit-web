86e859535bd5577e05b71413f797137a915af8c9
p0o <pooria_az@ymail.com>
2016-03-08 00:07:10 +0330

    a fix for explore in main to load as 6 items

--- app/shared/components/main/explore.jsx
+++ app/shared/components/main/explore.jsx
@@ -5,57 +5,24 @@ import { Grid, Column, Progress } from "../helper";
 export default React.createClass({
   displayName: 'explore',
 
-  getInitialState() {
-    return {
-      cats: []
-    };
-  },
-
-  componentDidMount() {
-    this.loadCategories();
-  },
-
-  loadCategories() {
-    let cats = this.props.categories;
-
-    if (cats.length) {
-      // pick 6 unique random category
-      let codes = [], i = 0;
-      let step = Math.round(cats.length / 6);
-      while (i < cats.length - 1) {
-        let min = i;
-        let max = i + step;
-        if (i + step < cats.length - 1) {
-          i = i + step;
-        } else {
-          i = cats.length - 1;
-        }
-        codes.push(Math.floor(Math.random() * (max - min - 1) + min));
-      }
-
-      let picked = [];
-      for (let i = 0; i < 6; i++) {
-        picked.push(cats[codes[i]]);
-      }
-      this.setState({cats: picked});
-    }
-  },
-
   renderExplore() {
-    const { cats } = this.state;
+    const { categories } = this.props;
 
-    if (cats.length) {
+    if (categories.length) {
+      // pick 6 unique random category
+      const selectedCategories = categories.slice(0, 6);
+
       return (
-        cats.map(function (item, idx) {
+        selectedCategories.map(function (cat, i) {
           // clear every three item
-          if (!item) { return null;};
+          if (!cat) { return null;}
           return (
-            <Column size="3" clear={ idx % 3 === 0 } key={`main-explore-${idx}`}>
+            <Column size="3" clear={ i % 3 === 0 } key={`main-explore-${i}`}>
               <div className="si-item-box">
-                <Link to={`/tag/${item.slug}`}>
-                  <div className="img" style={{backgroundImage:`url(${item.main_tag.image})`}}></div>
+                <Link to={`/tag/${cat.slug}`}>
+                  <div className="img" style={{backgroundImage:`url(${cat.image})`}}></div>
                 </Link>
-                <Link className="subtitle" to={`/tag/${item.slug}`}>{item.name}</Link>
+                <Link className="subtitle" to={`/tag/${cat.slug}`}>{cat.name}</Link>
               </div>
             </Column>
           );
@@ -65,8 +32,6 @@ export default React.createClass({
       return  <Progress /> ;
     }
   },
-
-
 
   render() {
 
