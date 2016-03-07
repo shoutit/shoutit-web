fa6825055a586d6ecf7601b4503056ed7808bed0
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-03-07 13:45:34 +0100

    Add fetchr services to create new profiles and update req.sessions

--- app/services/index.js
+++ app/services/index.js
@@ -1,3 +1,5 @@
 export conversations from "./conversations";
+export emailVerification from "./emailVerification";
 export profile from "./profile";
+export session from "./session";
 export twilioToken from "./twilioToken";
