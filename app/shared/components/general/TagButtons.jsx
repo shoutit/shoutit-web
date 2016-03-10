a468fe6c6edf66a18ef199c03a04b0e6856f5c28
p0o <pooria_az@ymail.com>
2016-03-10 23:28:38 +0330

    add location properties to all tag urls

--- app/shared/components/general/TagButtons.jsx
+++ app/shared/components/general/TagButtons.jsx
@@ -5,10 +5,11 @@ if(process.env.BROWSER) {
   require("styles/components/TagButtons.scss");
 }
 
-export default function TagButtons({ tags = [], showWithType = false, linear = false }) {
+export default function TagButtons({ tags = [], showWithType = false, linear = false, currentLocation }) {
+  const { city, state, country } = currentLocation;
 
   const tagsList = tags.map((tag, i) =>
-    <Link to={`/tag/${encodeURIComponent(tag.value.slug)}`} key={tag + i}>
+    <Link to={`/tag/${tag.value.slug}/${country}/${state}/${city}`} key={tag + i}>
       <span className="TagButtons-button">
         {showWithType?
           `${tag.name}: ${tag.value.name}`
