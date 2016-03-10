93bcf526a69a4a419e89ca6e608017c0d0d37bd4
p0o <pooria_az@ymail.com>
2016-03-10 18:01:18 +0330

    add links to the side footer

--- app/shared/components/cards/SideFooterCard.jsx
+++ app/shared/components/cards/SideFooterCard.jsx
@@ -2,20 +2,27 @@ import React from "react";
 import { Icon, Column, Grid } from "../helper";
 import { ANDROID_LINK, IOS_LINK, FACEBOOK, TWITTER, INSTAGRAM } from "../../consts/defaults";
 
+import { imagesPath } from "../../../../config";
+
+if (process.env.BROWSER) {
+  require("styles/components/SideFooter.scss");
+}
+
 export default function SideFooterCard() {
   return (
-    <Grid fluid>
+    <Grid fluid className="SideFooter">
       <Grid fluid>
-        <a className="SideFooterLink" href={ FACEBOOK } target="_blank">Facebook</a>
-        <a className="SideFooterLink" href={ INSTAGRAM } target="_blank">Instagram</a>
-        <a className="SideFooterLink" href={ TWITTER } target="_blank">Twitter</a>
+        <p className="SideFooter-copyright">© 2016 Shoutit</p>
+        <a className="SideFooter-link" href={ FACEBOOK } target="_blank">Facebook</a>
+        <a className="SideFooter-link" href={ INSTAGRAM } target="_blank">Instagram</a>
+        <a className="SideFooter-link" href={ TWITTER } target="_blank">Twitter</a>
       </Grid>
-      <Grid fluid>
+      <Grid fluid className="SideFooterButtons">
         <a href={ ANDROID_LINK } target="_blank">
-          <Icon name="app-store-small"/>
+          <img className="SideFooterButtons-button right-spacing" src={`${imagesPath}/app_store.png`} />
         </a>
         <a href={ IOS_LINK } target="_blank">
-          <Icon name="google-play-small"/>
+          <img className="SideFooterButtons-button" src={`${imagesPath}/google_play.png`} />
         </a>
       </Grid>
     </Grid>
