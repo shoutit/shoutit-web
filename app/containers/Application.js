96f537373899d5125ec1772aa0a2de6d184e25d5
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-03-18 22:41:05 +0100

    Use ConnectToStore to fetch application data

--- app/containers/Application.js
+++ app/containers/Application.js
@@ -1,6 +1,7 @@
-import React, { PropTypes } from "react";
-import { FluxMixin, StoreWatchMixin } from "fluxxor";
-import {createSlug} from "../shared/components/helper";
+import React from "react";
+
+import { ConnectToStores } from "../utils/FluxUtils";
+
 import Header from "../shared/components/header";
 import MainPage from "../shared/components/main/mainPage.jsx";
 import UINotificationsHost from "../ui/UINotificationsHost";
@@ -12,102 +13,132 @@ if (process.env.BROWSER) {
   require("./Application.scss");
 }
 
-export default React.createClass({
+const listenToStores = [
+  "auth",
+  "ui_notifications",
+  "videocall",
+  "conversations",
+  "SuggestionsByLocationStore"
+];
 
-  displayName: "Application",
+const fetchData = (flux, params, query, done) => {
 
-  propTypes: {
-    flux: PropTypes.object.isRequired
-  },
+  flux.actions.getCurrentSession((err, user) => {
 
-  mixins: [
-    new FluxMixin(React),
-    new StoreWatchMixin(
-      "auth",
-      "chat",
-      "conversations",
-      "locations",
-      "suggestions",
-      "ui_notifications",
-      "videocall"
-    )
-  ],
+    const promises = [];
+
+    promises.push(new Promise(resolve => flux.actions.loadCurrencies(resolve)));
+    promises.push(new Promise(resolve => flux.actions.loadCategories(resolve)));
+
+    if (user) {
+      promises.push(new Promise(resolve => flux.actions.loadConversations(resolve)));
+      promises.push(new Promise(resolve => flux.actions.loadListeners(user, resolve)));
+      promises.push(new Promise(resolve => flux.actions.loadListening(user, "users", resolve)));
+    }
+
+    // Load current location and suggestions based on the current location
+    if (!user || !user.location) {
+      promises.push(new Promise(resolve => {
+        flux.actions.getCurrentLocation((err, location) => {
+          if (!location) {
+            return resolve();
+          }
+          flux.actions.loadSuggestions(location, undefined, resolve);
+        });
+      }));
+
+    } else {
+      promises.push(new Promise(resolve => flux.actions.loadSuggestions(user.location, undefined, resolve)));
+    }
+
+    Promise.all(promises).then(() => done(), err => done(err));
+  });
+
+};
+
+const mapStoresProps = stores => {
+  const categories = stores.categories.get();
+  const chat = stores.chat.getState();
+  const conversations = stores.conversations.getConversations(chat.conversationIds);
+  const currencies  = stores.currencies.get();
+  const currentLocation = stores.location.getCurrentLocation();
+  const loggedUser = stores.auth.getLoggedProfile();
+  const shuffledCategories = stores.categories.shuffle();
+  const suggestedShout = stores.SuggestionsByLocationStore.getSuggestedShout(currentLocation);
+  const suggestedShouts = stores.SuggestionsByLocationStore.getSuggestedShouts(currentLocation);
+  const suggestedTags = stores.SuggestionsByLocationStore.getSuggestedTags(currentLocation);
+  const suggestedUsers = stores.SuggestionsByLocationStore.getSuggestedUsers(currentLocation);
+  const uiNotifications = stores.ui_notifications.getNotifications();
+  const videoCallState = stores.videocall.getState();
+
+  let listening;
+  let loggedUserListeners;
+  if (loggedUser) {
+    listening = stores.ListeningStore.getListening(loggedUser.id);
+    loggedUserListeners = stores.ListenersStore.getListeners(loggedUser.id);
+  }
+
+  return {
+    categories,
+    chat,
+    conversations,
+    currencies,
+    currentLocation,
+    loggedUserListeners,
+    listening,
+    loggedUser,
+    shuffledCategories,
+    suggestedShout,
+    suggestedShouts,
+    suggestedTags,
+    suggestedUsers,
+    uiNotifications,
+    videoCallState
+  };
+};
+
+export class Application extends React.Component {
 
   componentDidMount() {
-    this.getData();
-  },
-
-  componentDidUpdate(prevProps, prevState) {
-    const { loggedUser, currentLocation } = this.state;
-    if (prevState.loggedUser !== loggedUser) {
-      this.getData();
-    }
-    // Actions triggered in location change
-    if (prevState.currentLocation.city !== currentLocation.city) {
-      this.props.flux.actions.getSuggestions(currentLocation);
-    }
-  },
-
-  getStateFromFlux() {
     const { flux } = this.props;
-    const chat = flux.store("chat").getState();
-    const conversations = flux.store("conversations").getConversations(chat.conversationIds);
-    const currentLocation = flux.store("locations").getCurrent();
-    const loggedUser = flux.store("auth").getLoggedProfile();
-    const suggestions = flux.store("suggestions").getState();
-    const uiNotifications = flux.store("ui_notifications").getNotifications();
-    const videoCallState = flux.store("videocall").getState();
-    return {
-      chat,
-      conversations,
-      currentLocation,
-      loggedUser,
-      suggestions,
-      videoCallState,
-      uiNotifications
-    };
-  },
-
-  getData() {
-    const { flux } = this.props;
-    const { loggedUser } = this.state;
-
-    flux.actions.acquireLocation();
-
-    if (loggedUser) {
-      flux.actions.loadConversations();
-      if (require("webrtcsupport").support) {
-        flux.actions.initTwilio();
-      }
+    if (this.props.loggedUser) {
+      flux.actions.initTwilio();
     }
-  },
+  }
+
+  componentDidUpdate(prevProps) {
+    const { actions } = this.props.flux;
+    const { loggedUser } = this.props;
+    const hasBeenLoggedIn = !prevProps.loggedUser && loggedUser;
+
+    if (hasBeenLoggedIn) {
+      actions.initTwilio();
+      actions.loadSuggestions(loggedUser.location || {});
+      actions.loadConversations();
+      actions.loadListeners(loggedUser);
+      actions.loadListening(loggedUser, "users");
+    }
+  }
 
   render() {
-    const { loggedUser, chat, conversations, currentLocation, suggestions, videoCallState } = this.state;
-    const { children, flux, routes, location, history } = this.props;
-
-    const suggestionsData = {
-      data: suggestions.data[createSlug(currentLocation.city)]
-    };
-
-    const hideHeader = routes.some(route =>
+    const { children, ...props } = this.props;
+    //
+    const hideHeader = this.props.routes.some(route =>
       pagesWithoutHeader.indexOf(route.component) > -1
     );
-    const props = { loggedUser, chat, conversations, currentLocation, location,
-      suggestions: suggestionsData, history, videoCallState };
 
     return (
       <div className={`App${hideHeader ? "" : " stickyHeader"}` }>
         { !hideHeader &&
           <div className="App-header">
             <Header
-              history={ history }
-              loggedUser={ loggedUser }
-              currentLocation={ currentLocation }
-              flux={ flux }
-              chat={ chat }
-              conversations={ conversations }
-              location={ location }
+              history={ props.history }
+              loggedUser={ props.loggedUser }
+              currentLocation={ props.currentLocation }
+              flux={ props.flux }
+              chat={ props.chat }
+              conversations={ props.conversations }
+              location={ props.location }
             />
           </div>
         }
@@ -115,12 +146,16 @@ export default React.createClass({
           { React.cloneElement(children, props) }
         </div>
         <UINotificationsHost
-          notifications={ this.state.uiNotifications }
-          flux={ flux }
+          notifications={ this.props.uiNotifications }
+          flux={ props.flux }
         />
-        { videoCallState.currentConversation &&
-          <VideoCallHost conversation={ videoCallState.currentConversation } /> }
+        { props.videoCallState && props.videoCallState.currentConversation &&
+          <VideoCallHost conversation={ props.videoCallState.currentConversation } /> }
       </div>
     );
   }
-});
+}
+
+Application = ConnectToStores(Application, { fetchData, mapStoresProps, listenToStores });
+
+export default Application;
