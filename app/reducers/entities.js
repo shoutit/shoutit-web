a0e0b16a398a00511048bd0132f31fbed147439b
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-03-22 12:56:32 +0100

    Do not cleanup after logout
    This will be better defined in the future

--- app/reducers/entities.js
+++ app/reducers/entities.js
@@ -1,5 +1,4 @@
 import merge from "lodash/object/merge";
-import * as actionTypes from "../actions/actionTypes";
 
 const initialState = {
   categories: {},
@@ -15,23 +14,5 @@ export default function(state=initialState, action) {
   if (action.payload && action.payload.entities) {
     return merge({}, state, action.payload.entities);
   }
-
-  switch (action.type) {
-  case actionTypes.LOGIN_SUCCESS:
-    const loggedUser = action.payload;
-    return merge({}, state, { users: { [loggedUser.id]: loggedUser }});
-
-  case actionTypes.LOGOUT:
-
-    // Cleanup state only for logged in user
-    return {
-      ...state,
-      conversations: {},
-      messages: {}
-    };
-
-  default:
-    return state;
-  }
-
+  return state;
 }
