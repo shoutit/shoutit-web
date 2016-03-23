819688cc5eef7acb314b41ee694f2d6342ab0a88
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-03-23 03:47:57 +0100

    Increase page size for messages

--- app/services/messages.js
+++ app/services/messages.js
@@ -3,10 +3,11 @@ import { parseErrorResponse } from "../utils/APIUtils";
 
 export default {
   name: "messages",
-  read: (req, resource, { conversationId, endpoint }, config, callback) => {
+  read: (req, resource, { conversationId, endpoint, pageSize=20 }, config, callback) => {
     const url = endpoint || `/conversations/${conversationId}/messages`;
     request
       .get(url)
+      .query({ page_size: pageSize })
       .setSession(req.session)
       .prefix()
       .end((err, res) => {
