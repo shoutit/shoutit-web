62ae5d8d3aa3436b3b50df715a57694ff5d2de27
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-03-30 09:50:49 +0200

    Add ScrollLink, use image variation

--- app/containers/Homepage.js
+++ app/containers/Homepage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
+import { Link as ScrollLink, Element } from 'react-scroll';
 import Button from '../ui/Button';
 import Card from '../ui/Card';
 import Footer from '../layout/Footer';
+import DocumentTitle from '../ui/DocumentTitle';
+import { getVariation } from '../utils/APIUtils';
 
 import { imagesPath, appStoreLink, playStoreLink } from '../config';
 
@@ -13,53 +16,57 @@ if (process.env.BROWSER) {
 
 export function Homepage({ categories }) {
   return (
-    <div className="Homepage">
-      <div className="Homepage-hero">
-        <div className="Homepage-hero-bg" />
+    <DocumentTitle title="Buy and sell while chatting!">
+      <div className="Homepage">
+        <div className="Homepage-hero">
+          <div className="Homepage-hero-bg" />
 
-        <div className="Homepage-hero-wrapper htmlContentWidth">
-          <div className="Homepage-hero-copy">
-            <h1>Buy and sell while chatting!</h1>
-            <h2>
-              Shoutit is the fastest way to list and share what you want to sell or
-              buy. Chat with buyers and sellers in your area or anywhere in the world!
-            </h2>
-            <Button href="#explore" primary size="huge" label="Explore" />
-            <Button href="#how-it-works" inverted size="huge" label="How it works" />
+          <div className="Homepage-hero-wrapper htmlContentWidth">
+            <div className="Homepage-hero-copy">
+              <h1>Buy and sell while chatting!</h1>
+              <h2>
+                Shoutit is the fastest way to list and share what you want to sell or
+                buy. Chat with buyers and sellers in your area or anywhere in the world!
+              </h2>
+              <ScrollLink to="explore" className="Button primary size-huge" smooth duration={ 800 } offset={ 0 }>
+                <span className="Button-label">Explore</span>
+              </ScrollLink>
+              <ScrollLink to="how-it-works" className="Button inverted size-huge" smooth duration={ 800 } offset={ 0 }>
+                <span className="Button-label">How it works</span>
+              </ScrollLink>
+            </div>
+
+            <div className="Homepage-hero-apps">
+              <a href={appStoreLink} target="_blank">
+                <img src={`${imagesPath}/app-store-badge.png`} height="60" />
+              </a>
+              <a href={playStoreLink} target="_blank">
+                <img src={`${imagesPath}/google-play-badge.png`} height="60" />
+              </a>
+            </div>
           </div>
 
-          <div className="Homepage-hero-apps">
-            <a href={appStoreLink} target="_blank">
-              <img src={`${imagesPath}/app-store-badge.png`} height="60" />
-            </a>
-            <a href={playStoreLink} target="_blank">
-              <img src={`${imagesPath}/google-play-badge.png`} height="60" />
-            </a>
+        </div>
+
+        <Element name="explore" className="Homepage-explore htmlContentWidth">
+          <h2>Explore shouts</h2>
+          <div className="Homepage-cards">
+          { categories.slice(0, 8).map(category =>
+            <Link to={`/interests/${category.slug}`}>
+              <Card size="small" title={ category.name} image={ getVariation(category.image, 'small') } />
+            </Link>
+          )}
           </div>
-        </div>
+        </Element>
 
+        <Element name="how-it-works" className="Homepage-how-it-works">
+          <h2>How it works</h2>
+          <img src={`${imagesPath}/home-how-it-works.png`} height="500" width="1000" />
+        </Element>
+
+        <Footer />
       </div>
-
-      <a name="explore" />
-      <div className="Homepage-explore htmlContentWidth">
-        <h2>Explore shouts</h2>
-        <div className="Homepage-cards">
-        { categories.slice(0, 8).map(category =>
-          <Link to={`/interests/${category.slug}`}>
-            <Card size="small" title={ category.name} image={ category.image } />
-          </Link>
-        )}
-        </div>
-      </div>
-
-      <a name="how-it-works" />
-      <div className="Homepage-how-it-works">
-        <h2>How it works</h2>
-        <img src={`${imagesPath}/home-how-it-works.png`} height="500" width="1000" />
-      </div>
-
-      <Footer />
-    </div>
+    </DocumentTitle>
   );
 }
 
