c4e9f36f34342ec44c89f9c2cbb644132154d173
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-04-01 13:44:36 +0200

    Add TimeAgo component

---
+++ app/ui/TimeAgo.js
@@ -0,0 +1,18 @@
+import React, { PropTypes, Component } from 'react';
+import moment from 'moment';
+
+export default class TimeAgo extends Component {
+
+  static propTypes = {
+    date: PropTypes.number.isRequired,
+  };
+
+  render() {
+    const m = moment.unix(this.props.date);
+    return (
+      <span title={ m.format() }>
+        { m.fromNow() }
+      </span>
+    );
+  }
+}
