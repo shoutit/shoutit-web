5fe1de864242394aec451f2910661554dc0bbc7f
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-04-04 16:58:03 +0200

    Improve tooltips

--- app/ui/Tooltip.js
+++ app/ui/Tooltip.js
@@ -13,9 +13,9 @@ export default function Tooltip({ children, white = false, getTooltipContainer, 
   } else {
     prefixCls = 'Tooltip';
   }
-  if (!getTooltipContainer) {
-    getTooltipContainer = c => c;
-  }
+  // if (!getTooltipContainer) {
+  //   getTooltipContainer = c => c;
+  // }
 
   return (
     <ReactTooltip { ...props } getTooltipContainer={ getTooltipContainer } prefixCls={ prefixCls }>
