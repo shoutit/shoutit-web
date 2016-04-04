9ecaf17942cd2c01f934b48c7b27d3eda5c07840
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-04-04 23:49:16 +0200

    Smaller pattern in background images

--- app/utils/DOMUtils.js
+++ app/utils/DOMUtils.js
@@ -5,11 +5,14 @@ export function getStyleBackgroundImage(path, variation) {
   if (!path) {
     return {
       backgroundImage: `url("${imagesPath}/pattern@2x.png")`,
-      backgroundSize: '366px 599px',
+      backgroundSize: '250px 409px',
       backgroundRepeat: 'repeat',
     };
   }
   return {
     backgroundImage: `url("${getVariation(path, variation)}")`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
   };
 }
