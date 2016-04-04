13dbd7f7d7593b3b37c6c16c9e4c42795a420aab
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-04-05 00:25:09 +0200

    Set X-Forwarded-For header

--- app/services/location.js
+++ app/services/location.js
@@ -1,13 +1,22 @@
+
 import request from '../utils/request';
 import { createLocationSlug } from '../utils/LocationUtils';
 import { parseErrorResponse } from '../utils/APIUtils';
+import { getValidIPv4Address } from '../utils/InternetUtils';
 
 export default {
   name: 'location',
   read: (req, resource, { latlng = '0,0' }, config, callback) => {
+    const remoteAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const ip = getValidIPv4Address(remoteAddress);
+    const headers = {};
+    if (ip) {
+      headers['X-Forwarded-For'] = ip;
+    }
     request
       .get('/misc/geocode')
       .query({ latlng })
+      .set(headers)
       .setSession(req.session)
       .prefix()
       .end((err, res) => {
