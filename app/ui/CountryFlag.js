75051c9249d19082bfa14399dbf30bc47c69dae1
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-04-05 16:39:18 +0200

    Add showTooltip, rounded props

--- app/ui/CountryFlag.js
+++ app/ui/CountryFlag.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 import { imagesPath } from '../config';
 import { getCountryName } from '../utils/LocationUtils';
@@ -11,17 +11,26 @@ if (process.env.BROWSER) {
   require('./CountryFlag.scss');
 }
 
-export default function CountryFlag({ code, size }) {
+export default function CountryFlag({ code, size = 'medium', rounded = 'false', showTooltip = true }) {
   code = code.toUpperCase();
   let className = 'CountryFlag';
   if (size) {
     className += ` size-${size}`;
   }
+  if (rounded) {
+    className += ' rounded';
+  }
   return (
-    <Tooltip placement="top" overlay={ getCountryName(code) }>
+    <Tooltip placement="top" overlay={ getCountryName(code) } trigger={ showTooltip ? ['hover'] : [] }>
       <span>
         <img className={ className } src={ `${imagesPath}/flags/${code}.png?v${VERSION}` } />
       </span>
     </Tooltip>
   );
 }
+
+CountryFlag.propTypes = {
+  code: PropTypes.string.isRequired,
+  size: PropTypes.string,
+  rounded: PropTypes.bool,
+};
