0799e2f3f119c91deaca82b87737ef7c151c68a9
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-04-14 11:55:20 +0200

    Show chat profile names for deleted shouts

--- app/chat/ChatUtils.js
+++ app/chat/ChatUtils.js
@@ -1,14 +1,17 @@
 export function getConversationName(conversation) {
   let name = '';
+  const profiles = conversation.profiles.filter(profile => !profile.isOwner);
+  const profileNames = profiles.map(profile => profile.name).join(', ') || '(nobody)';
   if (conversation.type === 'about_shout') {
     const shout = conversation.about;
     name += shout.title || 'Shout';
     if (shout.profile) {
-      name += ` by ${conversation.about.profile.name}`;
+      name += ` – ${conversation.about.profile.name}`;
+    } else {
+      name += ` – ${profileNames}`;
     }
   } else {
-    const profiles = conversation.profiles.filter(profile => !profile.isOwner);
-    name = profiles.map(profile => profile.name).join(', ') || '(nobody)';
+    name = profileNames;
   }
   return name;
 }
