c7ac05e344c19bd255c81c0d2730f084eacae989
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-04-14 10:43:40 +0200

    Fixes an error when loading the chat

--- app/reducers/entities-conversations.js
+++ app/reducers/entities-conversations.js
@@ -1,4 +1,3 @@
-import merge from 'lodash/object/merge';
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
@@ -14,14 +13,17 @@ const initialState = {
 export default function (state = initialState, action) {
   switch (action.type) {
     case actionTypes.SET_CURRENT_CONVERSATION:
-      if (!action.payload) {
+      const id = action.payload;
+      if (!id || !state.conversations || !state.conversations[id]) {
         return state;
       }
-      return merge({}, state, {
-        [action.payload]: {
+      return {
+        ...state,
+        [id]: {
+          ...state.conversations[id],
           unreadMessagesCount: 0,
         },
-      });
+      };
   }
   return state;
 }
