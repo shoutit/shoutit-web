4ca970a62e90e9b806cbc6b378816720f22da92f
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-04-15 05:50:25 +0200

    Lint file

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 /* eslint no-var: 0, no-console: 0 */
 /* eslint-env node */
 
-if (process.env.NODE_ENV === "production" && process.env.NEW_RELIC_APP_NAME && process.env.NEW_RELIC_LICENSE_KEY) {
-  require("newrelic");
+if (process.env.NODE_ENV === 'production' && process.env.NEW_RELIC_APP_NAME && process.env.NEW_RELIC_LICENSE_KEY) {
+  require('newrelic');
 }
 
-require("babel-register");
+require('babel-register');
 
 // Prevent issues with libraries using this var (see http://tinyurl.com/pcockwk)
 delete process.env.BROWSER;
 
-var config = require("./app/config");
+var config = require('./app/config');
 
-var express = require("express");
+var express = require('express');
 var app = express();
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
 
-  var webpack = require("webpack");
-  var webpackConfig = require("./webpack.config");
+  var webpack = require('webpack');
+  var webpackConfig = require('./webpack.config');
   var webpackCompiler = webpack(webpackConfig);
-  var webpackMiddleware = require("webpack-dev-middleware");
+  var webpackMiddleware = require('webpack-dev-middleware');
   app.use(webpackMiddleware(webpackCompiler, {
     hot: true,
     publicPath: webpackConfig.output.publicPath,
@@ -37,16 +37,16 @@ if (process.env.NODE_ENV === "development") {
     }
   }));
 
-  app.use(require("webpack-hot-middleware")(webpackCompiler));
+  app.use(require('webpack-hot-middleware')(webpackCompiler));
 }
 
 // Start the server
-require("./app/server").start(app);
+require('./app/server').start(app);
 
 console.log(config.getSummary());
 
-var port = process.env.PORT || "3000";
+var port = process.env.PORT || '3000';
 
 app.listen(port, function () {
-  console.log("\nServer is now listening to %s:%s...", process.env.HOST || "localhost", port);
+  console.log('\nServer is now listening to %s:%s...', process.env.HOST || 'localhost', port);
 });
