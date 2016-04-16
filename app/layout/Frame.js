0c4c3cf7f3ce254a12f6b5abc7e7f984a94f09d1
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-04-16 13:59:55 +0200

    Add className prop

--- app/layout/Frame.js
+++ app/layout/Frame.js
@@ -3,9 +3,9 @@ import React from 'react';
 if (process.env.BROWSER) {
   require('./Frame.scss');
 }
-export default function Frame({ title, children, transparent = false }) {
+export default function Frame({ title, children, transparent = false, style, className = ''}) {
   return (
-    <div className={`Frame${transparent ? ' transparent' : ''}`}>
+    <div className={`Frame${transparent ? ' transparent' : ''} ${className}` } style={ style }>
       <div className="Frame-header">
         <h1>{ title }</h1>
       </div>
