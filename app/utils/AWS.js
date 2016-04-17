30f855bd26f2d27068d719fb19bff1e17c8738f4
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-04-18 00:23:27 +0200

    Add function to delete files from S3

--- app/utils/AWS.js
+++ app/utils/AWS.js
@@ -12,7 +12,7 @@ AWS.config.logger = { log };
 AWS.config.accessKeyId = process.env.SHOUTIT_S3_ACCESS_KEY;
 AWS.config.secretAccessKey = process.env.SHOUTIT_S3_SECRET_KEY;
 
-export function upload({ body: Body, key: Key, bucket: Bucket }, done) {
+export function upload({ body: Body, key: Key, bucket: Bucket }, callback) {
   const s3 = new AWS.S3();
   const params = { Bucket, Body, Key };
 
@@ -21,14 +21,36 @@ export function upload({ body: Body, key: Key, bucket: Bucket }, done) {
   s3.upload(params, (err, data) => {
     if (err) {
       console.error(err);
-      if (done) {
-        done(err);
+      if (callback) {
+        callback(err);
       }
       return;
     }
     log('Upload success for %s', Key, data);
-    if (done) {
-      done(null, data);
+    if (callback) {
+      callback(null, data);
+    }
+  });
+}
+
+export function del({ keys, bucket: Bucket }, callback) {
+  const s3 = new AWS.S3();
+  const Objects = keys.map(Key => ({ Key }));
+  const params = { Bucket, Delete: { Objects } };
+
+  log('Deleting objects from %s...', Bucket, Objects);
+
+  s3.deleteObjects(params, (err, data) => {
+    if (err) {
+      console.error(err);
+      if (callback) {
+        callback(err);
+      }
+      return;
+    }
+    log('Delete success', Objects);
+    if (callback) {
+      callback(null, data);
     }
   });
 }
