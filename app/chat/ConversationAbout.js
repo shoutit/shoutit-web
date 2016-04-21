4d7486272a36d45aab3cc91159f498cacebc90ec
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-04-22 00:19:44 +0200

    Display the shout the conversation is about

---
+++ app/chat/ConversationAbout.js
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react';
+import ShoutLink from '../shouts/ShoutLink';
+
+import { getStyleBackgroundImage } from '../utils/DOMUtils';
+
+if (process.env.BROWSER) {
+  require('./ConversationAbout.scss');
+}
+
+export default function ConversationAbout({ shout }) {
+  const { thumbnail, title, description } = shout;
+  console.log(shout);
+  return (
+    <ShoutLink shout={ shout } className="ConversationAbout">
+      { thumbnail &&
+        <span className="ConversationAbout-image" style={ getStyleBackgroundImage(thumbnail, 'small') } />
+      }
+      <span className="ConversationAbout-title">
+        { title || (description && description.substring(0, 160)) || 'About a shout without content' }
+        { title || (description && description.substring(0, 160)) || 'About a shout without content' }
+        { title || (description && description.substring(0, 160)) || 'About a shout without content' }
+        { title || (description && description.substring(0, 160)) || 'About a shout without content' }
+      </span>
+    </ShoutLink>
+  );
+}
+
+ConversationAbout.propTypes = {
+  shout: PropTypes.object.isRequired,
+};
