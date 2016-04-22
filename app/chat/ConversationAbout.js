527b2d5d7921e33f4e2e4aa99550187265bf3bc5
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-04-22 11:17:58 +0200

    Fix layout for conversation about component

--- app/chat/ConversationAbout.js
+++ app/chat/ConversationAbout.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import ShoutLink from '../shouts/ShoutLink';
+import Icon from '../ui/Icon';
 
 import { getStyleBackgroundImage } from '../utils/DOMUtils';
 
@@ -11,8 +12,9 @@ export default function ConversationAbout({ shout }) {
   const { thumbnail, title, description } = shout;
   return (
     <ShoutLink shout={ shout } className="ConversationAbout">
-      { thumbnail &&
-        <span className="ConversationAbout-image" style={ getStyleBackgroundImage(thumbnail, 'small') } />
+      { thumbnail ?
+        <span className="ConversationAbout-image" style={ getStyleBackgroundImage(thumbnail, 'small') } /> :
+        <Icon name="sparkle" active />
       }
       <span className="ConversationAbout-title">
         { title || (description && description.substring(0, 160)) || 'About a shout without content' }
