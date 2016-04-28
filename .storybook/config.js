4852fe6ecd576915789eca19f2df726557e19ec2
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-04-28 10:02:20 +0200

    Move stories to root dir

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,11 @@
 import { configure } from '@kadira/storybook';
+
 import 'normalize.css/normalize.css';
 import '../app/styles/main.scss';
 
 function loadStories() {
-  require('../app/ui/stories/Button');
-  require('../app/ui/stories/Icon');
-  // require as many stories as you need.
+  require('../stories/Button');
+  require('../stories/Icon');
 }
 
 configure(loadStories, module);
