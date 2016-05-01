b52d5623f260e3c987eda6239b5fa2b1da8df1d6
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-05-02 01:03:54 +0200

    Setup sinonChai before running tests

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,8 @@
-import { jsdom } from "jsdom";
+import { jsdom } from 'jsdom';
+import chai from 'chai';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
 
-global.document = jsdom("<!doctype html><html><body></body></html>");
+global.document = jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
 global.navigator = global.window.navigator;
