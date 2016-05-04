3f2ca39f36b7e047f324252f41d10f6ba83ea09f
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-05-04 14:57:55 +0200

    Fix regression when signing up

--- app/reducers/session.js
+++ app/reducers/session.js
@@ -57,7 +57,7 @@ export default function (state = initialState, action) {
     case actionTypes.SIGNUP_SUCCESS:
       return {
         ...state,
-        user: payload.user,
+        user: payload.result.user.id,
         isSigningUp: false,
         isNewSignup: true,
       };
