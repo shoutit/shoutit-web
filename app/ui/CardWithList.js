a26a523869ff1bf0d439323b5bdc5cd989c74f89
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-05-10 12:35:27 +0200

    Accept children as node element

--- app/ui/CardWithList.js
+++ app/ui/CardWithList.js
@@ -8,17 +8,19 @@ export default function CardWithList({ children = [], title, style }) {
   return (
     <div className="CardWithList" style={ style }>
       { title && <h3>{ title }</h3> }
-      { children.length > 0 &&
-        <ul>
-          { children.map((child, i) => <li key={ i }>{ child }</li>) }
-        </ul>
+      { Array.isArray(children) ?
+        children.length > 0 &&
+          <ul>
+            { children.map((child, i) => <li key={ i }>{ child }</li>) }
+          </ul> :
+        children
       }
     </div>
   );
 }
 
 CardWithList.propTypes = {
-  children: PropTypes.array,
+  children: PropTypes.node,
   title: PropTypes.string,
   style: PropTypes.object,
 };
