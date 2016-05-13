fdd239981259e04878e5031892a868a9c8627e0f
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-05-13 22:37:54 +0200

    Close opened conversations when opening the chat

--- app/middlewares/routing.js
+++ app/middlewares/routing.js
@@ -1,11 +1,22 @@
 import { replace } from 'react-router-redux';
+
 import { closeModal } from '../actions/ui';
+import { closeConversation } from '../actions/chat';
 import * as actionTypes from '../actions/actionTypes';
 
 export default store => next => action => { // eslint-disable-line no-unused-vars
   const state = store.getState();
 
   switch (action.type) {
+    case '@@router/LOCATION_CHANGE':
+      const { pathname } = action.payload;
+      if (state.modals.show) {
+        store.dispatch(closeModal());
+      }
+      if (pathname.match(/^\/messages/)) {
+        state.chat.openedConversations.map(id => store.dispatch(closeConversation(id)));
+      }
+      break;
     case actionTypes.LEAVE_CONVERSATION_START:
       const { id } = action.payload;
       if (state.routing.currentUrl === `/messages/${id}`) {
