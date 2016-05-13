72f5618d212f5f442c935ebc9f11bc597d901954
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-05-13 22:37:11 +0200

    Redirect to /messages when a conversation is left

--- app/middlewares/routing.js
+++ app/middlewares/routing.js
@@ -1,8 +1,19 @@
+import { replace } from 'react-router-redux';
 import { closeModal } from '../actions/ui';
+import * as actionTypes from '../actions/actionTypes';
 
 export default store => next => action => { // eslint-disable-line no-unused-vars
-  if (action.type === '@@router/LOCATION_CHANGE') {
-    store.dispatch(closeModal());
+  const state = store.getState();
+
+  switch (action.type) {
+    case actionTypes.LEAVE_CONVERSATION_START:
+      const { id } = action.payload;
+      if (state.routing.currentUrl === `/messages/${id}`) {
+        store.dispatch(replace('/messages'));
+      }
+      break;
+    default:
+
   }
   return next(action);
 
