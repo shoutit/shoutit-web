8935df7dd4a061fd2fd581967d2d6e45a5568f40
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-05-15 08:32:15 +0200

    Cleanup code

--- app/actions/messages.js
+++ app/actions/messages.js
@@ -1,45 +1,41 @@
 import * as actionTypes from './actionTypes';
 import { MESSAGES } from '../schemas';
 
-export function loadMessages(conversation, endpoint) {
-  return {
-    types: [
-      actionTypes.LOAD_MESSAGES_START,
-      actionTypes.LOAD_MESSAGES_SUCCESS,
-      actionTypes.LOAD_MESSAGES_FAILURE,
-    ],
-    payload: { conversation },
-    service: {
-      name: 'messages',
-      params: { id: conversation.id, endpoint },
-      schema: MESSAGES,
-    },
-  };
-}
+export const loadMessages = (conversation, endpoint) => ({
+  types: [
+    actionTypes.LOAD_MESSAGES_START,
+    actionTypes.LOAD_MESSAGES_SUCCESS,
+    actionTypes.LOAD_MESSAGES_FAILURE,
+  ],
+  payload: { conversation },
+  service: {
+    name: 'messages',
+    params: { id: conversation.id, endpoint },
+    schema: MESSAGES,
+  },
+});
 
-export function readMessage(message) {
-  return {
-    types: [
-      actionTypes.READ_MESSAGE_START,
-      actionTypes.READ_MESSAGE_SUCCESS,
-      actionTypes.READ_MESSAGE_FAILURE,
-    ],
-    payload: { message },
-    service: {
-      name: 'messagesRead',
-      method: 'create',
-      params: { id: message.id },
-      schema: MESSAGES,
-    },
-  };
-}
+export const readMessage = message => ({
+  types: [
+    actionTypes.READ_MESSAGE_START,
+    actionTypes.READ_MESSAGE_SUCCESS,
+    actionTypes.READ_MESSAGE_FAILURE,
+  ],
+  payload: { message },
+  service: {
+    name: 'messagesRead',
+    method: 'create',
+    params: { id: message.id },
+    schema: MESSAGES,
+  },
+});
 
 export const setMessageReadBy = readBy => ({
   type: actionTypes.SET_MESSAGE_READ_BY,
   payload: readBy,
 });
 
-export function addNewMessage(normalizedPayload) {
+export const addNewMessage = normalizedPayload => {
   const { entities, result: id } = normalizedPayload;
   return {
     type: actionTypes.ADD_NEW_MESSAGE,
@@ -51,4 +47,4 @@ export function addNewMessage(normalizedPayload) {
       ...normalizedPayload,
     },
   };
-}
+};
--- app/actions/search.js
+++ app/actions/search.js
@@ -3,58 +3,49 @@ import omit from 'lodash/omit';
 import * as actionTypes from './actionTypes';
 import { TAGS, PROFILES, SHOUTS } from '../schemas';
 
-export function searchTags(searchParams) {
-  return {
-    types: [
-      actionTypes.SEARCH_TAGS_START,
-      actionTypes.SEARCH_TAGS_SUCCESS,
-      actionTypes.SEARCH_TAGS_FAILURE,
-    ],
-    service: {
-      name: 'tags',
-      schema: TAGS,
-      params: searchParams,
-    },
-    payload: { searchParams },
-  };
-}
+export const searchTags = searchParams => ({
+  types: [
+    actionTypes.SEARCH_TAGS_START,
+    actionTypes.SEARCH_TAGS_SUCCESS,
+    actionTypes.SEARCH_TAGS_FAILURE,
+  ],
+  service: {
+    name: 'tags',
+    schema: TAGS,
+    params: searchParams,
+  },
+  payload: { searchParams },
+});
 
-export function searchProfiles(searchParams) {
-  return {
-    types: [
-      actionTypes.SEARCH_PROFILES_START,
-      actionTypes.SEARCH_PROFILES_SUCCESS,
-      actionTypes.SEARCH_PROFILES_FAILURE,
-    ],
-    service: {
-      name: 'profiles',
-      schema: PROFILES,
-      params: searchParams,
-    },
-    payload: { searchParams },
-  };
-}
+export const searchProfiles = searchParams => ({
+  types: [
+    actionTypes.SEARCH_PROFILES_START,
+    actionTypes.SEARCH_PROFILES_SUCCESS,
+    actionTypes.SEARCH_PROFILES_FAILURE,
+  ],
+  service: {
+    name: 'profiles',
+    schema: PROFILES,
+    params: searchParams,
+  },
+  payload: { searchParams },
+});
 
-export function searchShouts(location, searchParams, endpoint) {
-  location = omit(location, ['slug', 'name']);
-  return {
-    types: [
-      actionTypes.SEARCH_SHOUTS_START,
-      actionTypes.SEARCH_SHOUTS_SUCCESS,
-      actionTypes.SEARCH_SHOUTS_FAILURE,
-    ],
-    service: {
-      name: 'shouts',
-      schema: SHOUTS,
-      params: { searchParams, location, endpoint },
-    },
-    payload: { searchParams, endpoint },
-  };
-}
+export const searchShouts = (location, searchParams, endpoint) => ({
+  types: [
+    actionTypes.SEARCH_SHOUTS_START,
+    actionTypes.SEARCH_SHOUTS_SUCCESS,
+    actionTypes.SEARCH_SHOUTS_FAILURE,
+  ],
+  service: {
+    name: 'shouts',
+    schema: SHOUTS,
+    params: { searchParams, location: omit(location, ['slug', 'name']), endpoint },
+  },
+  payload: { searchParams, endpoint },
+});
 
-export function invalidateShoutsSearch(searchParams) {
-  return {
-    type: actionTypes.INVALIDATE_SHOUTS_SEARCH,
-    payload: { searchParams },
-  };
-}
+export const invalidateShoutsSearch = (searchParams) => ({
+  type: actionTypes.INVALIDATE_SHOUTS_SEARCH,
+  payload: { searchParams },
+});
