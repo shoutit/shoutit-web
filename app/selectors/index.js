163023dcbfba03da710b0d1d9867fadf1d6c746e
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-05-25 21:52:19 +0200

    Add redux selectors to dashboard

--- app/selectors/index.js
+++ app/selectors/index.js
@@ -1,8 +1,19 @@
+import omit from 'lodash/omit';
+import get from 'lodash/get';
+
 import { denormalize } from '../schemas';
 
 export function getLoggedUser(state) {
   return denormalize(state.entities.users[state.session.user], state.entities, 'PROFILE');
 }
+
+// Misc selector
+
+export function getPaginationStatus(state, selector) {
+  return omit(get(state.paginated, selector), 'ids');
+}
+
+// Shouts selectors
 
 export function getShoutsByUsername(state, username) {
   const paginated = state.paginated.shoutsByUsername[username];
@@ -11,5 +22,11 @@ export function getShoutsByUsername(state, username) {
   }
   return paginated.ids.map(id =>
     denormalize(state.entities.shouts[id], state.entities, 'SHOUT')
+  );
+}
+
+export function getHomepageShouts(state) {
+  return state.paginated.shoutsByHome.ids.map(
+    id => denormalize(state.entities.shouts[id], state.entities, 'SHOUT')
   );
 }
