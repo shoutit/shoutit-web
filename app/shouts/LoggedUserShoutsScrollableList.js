0ec9c0d6cef3488285a57abdbfbcd1ab19e78cc8
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-05-25 16:41:51 +0200

    Add toolbar to reply form

---
+++ app/shouts/LoggedUserShoutsScrollableList.js
@@ -0,0 +1,17 @@
+import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
+import { getLoggedUser } from '../selectors';
+
+import ShoutsScrollableList from './ShoutsScrollableList';
+
+export function LoggedUserShoutsScrollableList({ loggedUser, ...props }) {
+  return <ShoutsScrollableList profile={ loggedUser } {...props} />;
+}
+
+LoggedUserShoutsScrollableList.propTypes = {
+  loggedUser: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({ loggedUser: getLoggedUser(state) });
+
+export default connect(mapStateToProps)(LoggedUserShoutsScrollableList);
