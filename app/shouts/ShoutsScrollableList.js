e8dec1e41cfeb39955ac57a3b151303ef44ee8f6
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-05-26 20:52:39 +0200

    Complete the ShoutsScrollableList component

--- app/shouts/ShoutsScrollableList.js
+++ app/shouts/ShoutsScrollableList.js
@@ -2,12 +2,12 @@ import React, { PropTypes, Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import { connect } from 'react-redux';
-import { getShoutsByUsername } from '../selectors';
+import { getShoutsByUsername, getPaginationState } from '../selectors';
 import { loadShoutsByUsername } from '../actions/users';
 
 import Progress from '../ui/Progress';
 import Scrollable from '../ui/Scrollable';
-// import ShoutListItem from '../shouts/ShoutListItem';
+import ShoutListItem from '../shouts/ShoutListItem';
 
 if (process.env.BROWSER) {
   require('./ShoutsScrollableList.scss');
@@ -21,6 +21,7 @@ export class ShoutsScrollableList extends Component {
     isFetching: PropTypes.bool,
     nextUrl: PropTypes.string,
     shouts: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onShoutClick: PropTypes.func,
   }
 
   constructor(props) {
@@ -39,7 +40,6 @@ export class ShoutsScrollableList extends Component {
   loadMoreIfNeeded() {
     const { loadData, nextUrl, isFetching } = this.props;
     const { scrollable } = this.refs;
-
     if (!isFetching && nextUrl && !scrollable.canScroll()) {
       loadData(nextUrl);
     }
@@ -50,6 +50,14 @@ export class ShoutsScrollableList extends Component {
     if (nextUrl) {
       loadData(nextUrl);
     }
+  }
+
+  handleShoutClick(shout, e) {
+    if (!this.props.onShoutClick) {
+      return;
+    }
+    e.preventDefault();
+    this.props.onShoutClick(shout);
   }
 
   render() {
@@ -64,8 +72,7 @@ export class ShoutsScrollableList extends Component {
         <div className="ShoutsScrollableList-items">
           { shouts.map(shout =>
             <span key={ shout.id }>
-              { shout.title }
-              { /* <ShoutListItem size="large" shout={ shout } />*/ }
+              <ShoutListItem shout={ shout } onClick={ this.handleShoutClick.bind(this, shout) } />
             </span>
           ) }
         </div>
@@ -77,6 +84,7 @@ export class ShoutsScrollableList extends Component {
 
 const mapStateToProps = (state, ownProps) => ({
   shouts: getShoutsByUsername(state, ownProps.profile.username),
+  ...getPaginationState(state, `shoutsByUsername.${ownProps.profile.username}`),
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
