6fc9119659de834f4a0f7747bf17236bac7aacff
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-05-26 20:51:38 +0200

    Complete the ShoutListItem component

--- app/shouts/ShoutListItem.js
+++ app/shouts/ShoutListItem.js
@@ -1,11 +1,11 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
-import ProfileAvatar from '../users/ProfileAvatar';
 import ListItem from '../ui/ListItem';
-import { formatLocation } from '../utils/LocationUtils';
+import { getStyleBackgroundImage } from '../utils/DOMUtils';
+import { formatPrice } from '../utils/CurrencyUtils';
 
 if (process.env.BROWSER) {
-  // require('./ShoutListItem.scss');
+  require('./ShoutListItem.scss');
 }
 
 export default function ShoutListItem({
@@ -15,16 +15,14 @@ export default function ShoutListItem({
   onClick,
 }) {
 
-  const avatar = <ProfileAvatar size={ size } shout={ shout } />;
+  const image = <span className="ShoutListItem-thumbnail" style={ getStyleBackgroundImage(shout.thumbnail, 'small') } />;
   let content = (
-    <ListItem className="ShoutListItem" size={ size } nowrap start={ avatar } onClick={ onClick }>
+    <ListItem className="ShoutListItem" size={ size } nowrap start={ image } onClick={ onClick }>
       <div className="ShoutListItem-child">
-        <div className="ShoutListItem-name">{ shout.name }</div>
-        { size === 'large' && shout.title &&
-          <div className="ShoutListItem-ancillary">
-            { formatLocation(shout.title) }
-          </div>
-        }
+        <div className="ShoutListItem-title">{ shout.title }</div>
+        <div className="ShoutListItem-details">
+          { formatPrice(shout.price, shout.currency) }
+        </div>
       </div>
     </ListItem>
   );
