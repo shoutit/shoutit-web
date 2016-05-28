81c54dd51e618fc7932ec25b11db3c7110fe4945
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-05-28 17:38:11 +0200

    Use modal for ProfileAvatarEditor

--- app/users/ProfileAvatarEditable.js
+++ app/users/ProfileAvatarEditable.js
@@ -1,9 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
-import { openModal, closeModal } from '../actions/ui';
+import { openModal } from '../actions/ui';
 
-import AvatarEditor from '../users/AvatarEditor';
+import AvatarEditorModal from '../users/AvatarEditorModal';
 import ProfileAvatar from '../users/ProfileAvatar';
 import FileInput from '../ui/FileInput';
 
@@ -15,7 +15,7 @@ export class ProfileAvatarEditable extends Component {
 
   static propTypes = {
     ...ProfileAvatar.PropTypes,
-    openAvatarEditor: PropTypes.func.isRequired,
+    openModal: PropTypes.func.isRequired,
     profile: PropTypes.object.isRequired,
   }
 
@@ -30,20 +30,19 @@ export class ProfileAvatarEditable extends Component {
   handleFileInputChange(e) {
     const { files } = e.target;
     if (files && files.length > 0) {
-      const { openAvatarEditor, profile } = this.props;
+      const { openModal, profile } = this.props;
       const fileReader = new FileReader();
-      fileReader.onload = event => openAvatarEditor(event.target.result, profile);
+      fileReader.onload = event => openModal(event.target.result, profile);
       fileReader.readAsDataURL(files[0]);
     }
   }
 
   render() {
-    const { profile, openAvatarEditor } = this.props;
+    const { profile, openModal } = this.props;
     return (
       <span
         className="ProfileAvatarEditable"
-        onClick={ profile.image ? () => openAvatarEditor(profile.image, profile) : null }
-      >
+        onClick={ profile.image ? () => openModal(profile.image, profile) : null }>
         <FileInput
           name="avatar"
           disabled={ !!profile.image }
@@ -60,13 +59,8 @@ export class ProfileAvatarEditable extends Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-  openAvatarEditor: (image, profile) => dispatch(openModal(
-    <AvatarEditor
-      image={ image }
-      profile={ profile }
-      onCancel={ () => dispatch(closeModal()) }
-      onSuccess={ () => dispatch(closeModal()) }
-    />, { title: 'Update Your Picture' }
+  openModal: (image, profile) => dispatch(openModal(
+    <AvatarEditorModal initialImage={ image } profile={ profile } />
   )),
 });
 export default connect(null, mapDispatchToProps)(ProfileAvatarEditable);
