2e30274f9b95b9be6ca9753ebaf6b5f77b0dffae
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-05-29 17:16:59 +0200

    Fix an issue where onChange event may not fire

--- app/ui/FileInput.js
+++ app/ui/FileInput.js
@@ -13,18 +13,34 @@ export default class FileInput extends Component {
     className: PropTypes.string,
     name: PropTypes.string.isRequired,
     onChange: PropTypes.func,
+    onClick: PropTypes.func,
+  }
+  handleClick(e) {
+    // prevent missing onchange event, see http://stackoverflow.com/questions/2133807
+    this.refs.input.value = null;
+    if (this.props.onClick) {
+      this.props.onClick(e);
+    }
   }
   render() {
-    const { accept, children, name, onChange, className, disabled, multiple } = this.props;
     let cssClass = 'FileInput';
-    if (className) {
-      cssClass += ` ${className}`;
+    if (this.props.className) {
+      cssClass += ` ${this.props.className}`;
     }
     return (
       <span className={ cssClass }>
-        <input id={ name } type="file" accept={ accept } onChange={ onChange } disabled={ disabled } multiple={ multiple } />
+        <input
+          ref="input"
+          id={ name }
+          type="file"
+          accept={ this.props.accept }
+          onClick={ this.handleClick.bind(this) }
+          onChange={ this.props.onChange }
+          disabled={ this.props.disabled }
+          multiple={ this.props.multiple }
+        />
         <label htmlFor={ name } zIndex={ 0 }>
-          { children }
+          { this.props.children }
         </label>
       </span>
     );
