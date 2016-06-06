3ebac75690c0412cd60559efa5b60532aa439eee
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-06-06 08:51:28 +0200

    Set locale from querystring

--- app/server/localeMiddleware.js
+++ app/server/localeMiddleware.js
@@ -3,7 +3,12 @@ import { locales } from '../config';
 const log = debug('shoutit:server:localeMiddleware');
 
 export default function localeMiddleware(req, res, next) {
-  req.locale = req.acceptsLanguages(locales) || 'en';
-  log('Detected locale: %s', req.locale);
+  if (req.query.hl && locales.indexOf(req.query.hl) > -1) {
+    req.locale = req.query.hl;
+    log('Detected locale from querystring: %s', req.locale);
+  } else {
+    req.locale = req.acceptsLanguages(locales) || 'en';
+    log('Detected locale from accept-language: %s', req.locale);
+  }
   next();
 }
