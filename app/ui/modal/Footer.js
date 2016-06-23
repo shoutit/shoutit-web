bcfd795eb58e4be10dfb5792d05cff2347b61644
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-06-23 18:20:33 +0200

    Add start prop to modal's footer

--- app/ui/modal/Footer.js
+++ app/ui/modal/Footer.js
@@ -3,11 +3,19 @@ import React, { Component, PropTypes } from 'react';
 export default class Footer extends Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
+    start: PropTypes.node,
   };
   render() {
     return (
       <div className="ModalFooter">
-        { this.props.children }
+        { this.props.start &&
+          <div className="ModalFooter-start">
+            { this.props.start }
+          </div>
+        }
+        <div className="ModalFooter-end">
+          { this.props.children }
+        </div>
       </div>
     );
   }
