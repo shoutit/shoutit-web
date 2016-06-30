aea243dbdbafacb78eb1134ed9947d8fa47f9c13
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-06-30 11:46:27 +0200

    Just return children if scrollElement is defined

--- app/ui/Scrollable.js
+++ app/ui/Scrollable.js
@@ -165,6 +165,9 @@ export default class Scrollable extends Component {
 
   render() {
     const { children, className, style } = this.props;
+    if (this.props.scrollElement) {
+      return children;
+    }
     return (
       <div ref="node" style={ style } className={ className }>
         { children }
