00e606cd6330f9b8fa510266ae89b7bc7015b29c
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-07-07 10:30:53 +0200

    Add sortTypes service

---
+++ app/services/sortTypes.js
@@ -0,0 +1,33 @@
+import request from '../utils/request';
+import { parseApiError } from '../utils/APIUtils';
+let cache;
+
+export function setCache(body) {
+  cache = body;
+}
+
+export function getCache() {
+  return cache;
+}
+
+export default {
+  name: 'sortTypes',
+  read: (req, resource, params, config, callback) => {
+    const cache = getCache();
+    if (cache) {
+      callback(null, cache);
+      return;
+    }
+    request
+      .get('/shouts/sort_types')
+      .prefix()
+      .use(req)
+      .end((err, res) => {
+        if (err) {
+          return callback(parseApiError(err));
+        }
+        setCache(res.body);
+        return callback(null, res.body);
+      });
+  },
+};
