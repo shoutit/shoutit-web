42a104053f913abd6f5bd37bbab1257e1da20fe2
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-07-18 11:54:42 +0200

    Update searchutils tests to last changes

--- app/utils/SearchUtils.test.js
+++ app/utils/SearchUtils.test.js
@@ -57,31 +57,159 @@ describe('utils/SearchUtils', () => {
 
   });
 
-  describe('getSearchParamsFromQuery', () => {
+  describe('getSearchQuery', () => {
 
-    it('should create a valid search params object', () => {
-      const searchParams = SearchUtils.getSearchParamsFromQuery({
+    it('should append a page param if not provided', () => {
+      expect(SearchUtils.getSearchQuery({})).to.eql({ page: 1 });
+      expect(SearchUtils.getSearchQuery({ page: '2' })).to.eql({ page: 2 });
+    });
+    it('should decode an encoded character in the search query', () => {
+      const query = SearchUtils.getSearchQuery({
+        search: 'with%20space',
+      });
+      expect(query).to.eql({
+        search: 'with space',
+        page: 1,
+      });
+    });
+    it('should create a valid query with shout type, search, sort and category', () => {
+      const query = SearchUtils.getSearchQuery({
+        shout_type: 'request',
+        category: 'foo-category',
+        search: 'hello',
+        page: '1',
+        sort: 'foo',
+      });
+      expect(query).to.eql({
+        shout_type: 'request',
+        category: 'foo-category',
+        search: 'hello',
+        page: 1,
+        sort: 'foo',
+      });
+    });
+
+    it('should parse prices to integer', () => {
+      const query = SearchUtils.getSearchQuery({
+        min_price: '1',
+        max_price: '2',
+      });
+      expect(query).to.eql({
+        min_price: 1,
+        max_price: 2,
+        page: 1,
+      });
+    });
+
+    it('should create a valid query with prices having the `free` param set', () => {
+      const query = SearchUtils.getSearchQuery({
         shout_type: 'request',
         category: 'foo-category',
         min_price: '1',
         max_price: '2',
-        search: 'with%20space',
-        filters: 'foo:bar;abc:xyz',
-        page: '1',
-        sort: 'foo',
+        free: true,
       });
-      expect(searchParams).to.eql({
+      expect(query).to.eql({
         shout_type: 'request',
         category: 'foo-category',
-        min_price: 1,
-        max_price: 2,
-        search: 'with space',
-        foo: 'bar',
-        abc: 'xyz',
+        max_price: 0,
         page: 1,
-        sort: 'foo',
-        free: false,
       });
+    });
+
+    it('should use the location argument when passed', () => {
+      const location = {
+        city: 'a city',
+        state: 'a state',
+        country: 'a country',
+        latitude: 123,
+        longitude: 245,
+      };
+      const query = SearchUtils.getSearchQuery({}, location);
+      const expectedValue = {
+        location: {
+          city: 'a city',
+          state: 'a state',
+          country: 'a country',
+          latitude: 123,
+          longitude: 245,
+        },
+        page: 1,
+      };
+      expect(query).to.eql(expectedValue);
+    });
+
+    it('should use the within argument in km', () => {
+      const location = {
+        city: 'a city',
+        state: 'a state',
+        country: 'a country',
+      };
+      const query = SearchUtils.getSearchQuery({ within: '200' }, location);
+      const expectedValue = {
+        location: {
+          city: 'a city',
+          state: 'a state',
+          country: 'a country',
+        },
+        within: 200,
+        page: 1,
+      };
+      expect(query).to.eql(expectedValue);
+    });
+    it('should use the within argument set as city', () => {
+      const location = {
+        city: 'a city',
+        state: 'a state',
+        country: 'a country',
+        latitude: 123,
+        longitude: 245,
+      };
+      const query = SearchUtils.getSearchQuery({ within: 'city' }, location);
+      const expectedValue = {
+        location: {
+          city: 'a city',
+          state: 'a state',
+          country: 'a country',
+        },
+        page: 1,
+      };
+      expect(query).to.eql(expectedValue);
+    });
+    it('should use the within argument set as state', () => {
+      const location = {
+        city: 'a city',
+        state: 'a state',
+        country: 'a country',
+        latitude: 123,
+        longitude: 245,
+      };
+      const query = SearchUtils.getSearchQuery({ within: 'state' }, location);
+      const expectedValue = {
+        location: {
+          state: 'a state',
+          country: 'a country',
+        },
+        page: 1,
+      };
+      expect(query).to.eql(expectedValue);
+    });
+    it('should use the within argument set as country', () => {
+      const location = {
+        city: 'a city',
+        state: 'a state',
+        country: 'a country',
+        latitude: 123,
+        longitude: 245,
+      };
+      const query = SearchUtils.getSearchQuery({ within: 'country' }, location);
+      const expectedValue = {
+        location: {
+          country: 'a country',
+        },
+        page: 1,
+      };
+      expect(query).to.eql(expectedValue);
     });
 
   });
