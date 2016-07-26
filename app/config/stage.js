f7cb413316ee50912f8187827540ce281ed0e21e
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-07-26 22:18:30 +0200

    Add app settings per environment

--- app/config/stage.js
+++ app/config/stage.js
@@ -5,3 +5,9 @@ export const publicUrl = 'https://stage.web.static.shoutit.com';
 export const pusherAppKey = '7bee1e468fabb6287fc5';
 export const siteUrl = 'https://stage.www.shoutit.com';
 export const mixpanelToken = 'd2de0109a8de7237dede66874c7b8951';
+
+export const androidPackage = 'com.shoutit.app.android.staging';
+export const androidAppName = 'Shoutit - Buy & Sell on Chat';
+export const iosAppId = '947017118';
+export const iosAppName = 'Shoutit - Buy & Sell while Chatting';
+
