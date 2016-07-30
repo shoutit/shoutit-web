ebca0c0b40b638aefa115df94ba2c9fb0ab76895
Goran Udosic <udosic.goran@gmail.com>
2016-07-30 15:31:56 +0200

    Add caching and cache invalidation in staticResource service Cache is invalidated when 'invalidate' is specified as query param

--- app/services/staticResource.js
+++ app/services/staticResource.js
@@ -1,3 +1,5 @@
+import has from 'lodash/has';
+import omit from 'lodash/omit';
 import fs from 'fs';
 import { parseApiError } from '../utils/APIUtils';
 // import * as AWS from '../utils/AWS';
@@ -5,23 +7,53 @@ import { parseApiError } from '../utils/APIUtils';
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'development' || !process.env.SHOUTIT_ENV;
 const STATIC_RESOURCES_DIR = `${__dirname}/../../assets/static`;
 
+let staticCache = {};
+
+const cachePage = (pageKey, data) => {
+  staticCache = {
+    ...staticCache,
+    [pageKey]: data,
+  };
+};
+
+const invalidateCache = pageKey => {
+  staticCache = { ...omit(staticCache, pageKey) };
+};
+
 export default {
   name: 'staticResource',
   read: (req, resource, { id }, config, callback) => {
 
-    const fileName = `${id}.${req.locale}.html`;
+    let cachedContent = undefined;
+    const filePrefix = `${id}.${req.locale}`;
+    const fileSuffix = '.html';
+    const fileName = `${filePrefix}${fileSuffix}`;
+    const invalidate = has(req.query, 'invalidate');
+
+    invalidate && invalidateCache(filePrefix);
+
+    cachedContent = staticCache[filePrefix];
+
+    if (cachedContent) {
+      callback(null, {
+        content: cachedContent,
+      });
+      return;
+    }
 
     if (IS_DEVELOPMENT) {
       fs.readFile(`${STATIC_RESOURCES_DIR}/${fileName}`, 'utf8', (err, data) => {
         if (err) {
           return callback(parseApiError(err));
         }
+
+        cachePage(filePrefix, data);
+
         return callback(null, {
           content: data,
         });
       });
     }
-
 
     // TODO Define a bucket in config/index uploadResources
     // AWS.getObject({
