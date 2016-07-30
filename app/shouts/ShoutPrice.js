819e35ba528abdd8c43e5bfc4a419a1199f7eeaf
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-07-30 18:36:49 +0200

    Display when a price is negotiable

--- app/shouts/ShoutPrice.js
+++ app/shouts/ShoutPrice.js
@@ -1,16 +1,20 @@
+/* eslint no-nested-ternary: 0 */
 import React, { PropTypes } from 'react';
 import { FormattedNumber, FormattedMessage } from 'react-intl';
 
 import './ShoutPrice.scss';
 export default function ShoutPrice({ shout, layout = 'plain' }) {
-  const isFree = shout.price === 0;
-  let { price } = shout;
+  let { price, currency } = shout;
+  const isFree = price === 0;
+  const isNegotiable = price === null;
   let className = `ShoutPrice ${layout}`;
+
   if (isFree) {
     className += ' free';
   } else {
     price = price / 100;
   }
+
   return (
     <span className={ className }>
       { isFree ?
@@ -18,14 +22,19 @@ export default function ShoutPrice({ shout, layout = 'plain' }) {
           id="shoutPrice.free"
           defaultMessage="Free"
         /> :
-        <FormattedNumber
-          style="currency"
-          currencyDisplay="symbol"
-          currency={ shout.currency }
-          value={ price }
-          maximumFractionDigits={ Number.isInteger(price) ? 0 : undefined }
-          minimumFractionDigits={ Number.isInteger(price) ? 0 : undefined }
-        />
+        isNegotiable ?
+          <FormattedMessage
+            id="shoutPrice.negotiable"
+            defaultMessage="Negotiable price"
+          /> :
+          <FormattedNumber
+            style="currency"
+            currencyDisplay="symbol"
+            currency={ currency }
+            value={ price }
+            maximumFractionDigits={ Number.isInteger(price) ? 0 : undefined }
+            minimumFractionDigits={ Number.isInteger(price) ? 0 : undefined }
+          />
       }
     </span>
   );
