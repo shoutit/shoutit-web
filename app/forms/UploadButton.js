eda42f850670dfb6dc9b937fb0156f550f39a188
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-08-09 17:55:51 +0200

    Fix style for upload button
    Use <button> instad of <span>

--- app/forms/UploadButton.js
+++ app/forms/UploadButton.js
@@ -15,7 +15,7 @@ export default class UploadButton extends Component {
     const { accept, children, name, onChange, ...props } = this.props;
     return (
       <FileInput className="UploadButton" accept={ accept } name={ name } onChange={ onChange }>
-        <Button { ...props } block={ false } element="span">
+        <Button { ...props } block={ false }>
           { children }
         </Button>
       </FileInput>
