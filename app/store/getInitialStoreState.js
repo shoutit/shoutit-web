8dc5d0c96f7e93f887a6d971649465cded822b2b
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-08-10 10:23:48 +0200

    Extract code to get initial store state

---
+++ app/store/getInitialStoreState.js
@@ -0,0 +1,44 @@
+import path from 'path';
+import { locales as availableLocales } from '../config';
+
+const translationsPath = path.resolve(__dirname, '../../assets/intl/translations');
+
+/**
+ * Return an initial store state according to the express request.
+ * Use this function only on the server to get the state to rehydrate on the client.
+ *
+ * @export
+ * @param {any} req
+ * @return {Object}
+ */
+export default function getInitialStoreState(req) {
+  const { locale = 'en' } = req;
+  const messages = require(`${translationsPath}/${locale}.json`);
+  const state = {
+    routing: {
+      currentUrl: req.url,
+      query: req.query,
+      path: req.path,
+    },
+    browser: req.browser,
+    currentLocation: req.geolocation,
+    i18n: {
+      availableLocales,
+      messages,
+      locale,
+      rtl: locale === 'ar',
+    },
+  };
+  if (req.session && req.session.user) {
+    const { user } = req.session;
+    state.session = {
+      user: user.id,
+    };
+    state.entities = {
+      users: {
+        [user.id]: user,
+      },
+    };
+  }
+  return state;
+}
---
+++ app/store/getInitialStoreState.test.js
@@ -0,0 +1,77 @@
+import { expect } from 'chai';
+import { locales as availableLocales } from '../config';
+
+import getInitialStoreState from './getInitialStoreState';
+
+describe('store/getInitialStoreState', () => {
+
+  it('should set the routing store', () => {
+    const req = {
+      url: 'url',
+      query: { foo: 'bar' },
+      path: 'path',
+    };
+    const state = getInitialStoreState(req);
+    expect(state).to.have.property('routing');
+    expect(state.routing).to.eql({
+      currentUrl: 'url',
+      query: { foo: 'bar' },
+      path: 'path',
+    });
+  });
+
+  it('should set the browser store', () => {
+    const req = {
+      browser: 'a_browser',
+    };
+    const state = getInitialStoreState(req);
+    expect(state).to.have.property('browser');
+    expect(getInitialStoreState(req).browser).to.eql('a_browser');
+  });
+
+  it('should set the i18n store', () => {
+    const req = {
+      locale: 'en',
+    };
+    const state = getInitialStoreState(req);
+    expect(state).to.have.property('i18n');
+    expect(state.i18n).to.have.property('messages');
+    expect(state.i18n).to.have.property('locale', 'en');
+    expect(state.i18n).to.have.property('rtl', false);
+    expect(state.i18n).to.have.property('availableLocales', availableLocales);
+  });
+
+  it('should set the i18n store (rtl)', () => {
+    const req = {
+      locale: 'ar',
+    };
+    const state = getInitialStoreState(req);
+    expect(state).to.have.property('i18n');
+    expect(state.i18n).to.have.property('messages');
+    expect(state.i18n).to.have.property('locale', 'ar');
+    expect(state.i18n).to.have.property('rtl', true);
+    expect(state.i18n).to.have.property('availableLocales', availableLocales);
+  });
+
+  it('should set the user in the session store and in the entities', () => {
+    const req = {
+      session: {
+        user: {
+          id: 'user_id',
+          name: 'user_name',
+        },
+      },
+    };
+    const state = getInitialStoreState(req);
+    expect(state).to.have.property('session');
+    expect(state.session).to.have.property('user');
+    expect(state.session.user).to.equal('user_id');
+    expect(state.entities.users).to.eql({
+      user_id: {
+        id: 'user_id',
+        name: 'user_name',
+      },
+    });
+  });
+
+});
