43df6622bfc120ec9823741390c151cffad3f6b8
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-08-19 17:22:40 +0200

    Add removeParamsFromUrl string util

--- app/utils/StringUtils.js
+++ app/utils/StringUtils.js
@@ -1,9 +1,45 @@
 import last from 'lodash/last';
+import omit from 'lodash/omit';
+import queryString from 'query-string';
 
 export function getFilename(str) {
   return last(str.split('/'));
 }
 
+const titleCaseRegExp = /[\u00BF-\u1FFF\u2C00-\uD7FF\w]*/g;
+/**
+ * Transform a string to title case
+ *
+ * @export
+ * @param {String} str
+ * @returns
+ */
 export function toTitleCase(str) {
-  return str.replace(/[\u00BF-\u1FFF\u2C00-\uD7FF\w]*/g, txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
+  return str.replace(titleCaseRegExp, txt =>
+    txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+  );
+}
+
+/**
+ * Removes one or more params from an url
+ *
+ * @export
+ * @param {String} url
+ * @param {String|Array} params
+ * @returns
+ */
+export function removeParamsFromUrl(url, params) {
+  const path = url.split('?')[0];
+  let qs = url.split('?')[1] || '';
+  if (qs) {
+    qs = queryString.stringify(
+      omit(
+        queryString.parse(qs),
+        params
+      ));
+    if (qs) {
+      qs = `?${qs}`;
+    }
+  }
+  return `${path}${qs}`;
 }
--- app/utils/StringUtils.test.js
+++ app/utils/StringUtils.test.js
@@ -1,6 +1,6 @@
 /* eslint-env mocha */
 import { expect } from 'chai';
-import { getFilename, toTitleCase } from "./StringUtils"; // eslint-disable-line
+import { getFilename, toTitleCase, removeParamsFromUrl } from "./StringUtils"; // eslint-disable-line
 
 describe('utils/StringUtils', () => {
   describe('getFilename', () => {
@@ -17,6 +17,28 @@ describe('utils/StringUtils', () => {
     });
     it('should change the case when string has unicode letters', () => {
       expect(toTitleCase('müllheim nrw')).to.eql('Müllheim Nrw');
+    });
+  });
+  describe('removeParamsFromUrl', () => {
+    it('should return the url without param', () => {
+      const url = 'http://example.com/test/?foo=bar';
+      const newUrl = 'http://example.com/test/';
+      expect(removeParamsFromUrl(url, 'foo')).to.equal(newUrl);
+    });
+    it('should return the url for params without a value', () => {
+      const url = 'http://example.com/test/?foo';
+      const newUrl = 'http://example.com/test/';
+      expect(removeParamsFromUrl(url, 'foo')).to.equal(newUrl);
+    });
+    it('should return an url with other params stripping the ?', () => {
+      const url = 'http://example.com/test/?bar=foo&foo=bar';
+      const newUrl = 'http://example.com/test/?bar=foo';
+      expect(removeParamsFromUrl(url, 'foo')).to.equal(newUrl);
+    });
+    it('should return an url with other params stripping the &', () => {
+      const url = 'http://example.com/test/?foo=bar&bar=foo';
+      const newUrl = 'http://example.com/test/?bar=foo';
+      expect(removeParamsFromUrl(url, 'foo')).to.equal(newUrl);
     });
   });
 });
