90a3205708c9f68a41583740a11e55619051b832
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-08-22 21:45:51 +0200

    Add enter animations, loading prop to Modal

--- app/modals/BodyFixed.js
+++ app/modals/BodyFixed.js
@@ -4,9 +4,17 @@ export default class BodyFixed extends Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
   };
+
+
+  getHeight() {
+    return this.node.offsetHeight;
+  }
+
+  node = null;
+
   render() {
     return (
-      <div className="ModalBodyFixed">
+      <div className="ModalBodyFixed" ref={ el => { this.node = el; } }>
         { this.props.children }
       </div>
     );
