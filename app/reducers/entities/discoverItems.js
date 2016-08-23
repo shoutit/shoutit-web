9494218b3d5a836ff8192885242360b40306b0eb
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-08-23 12:58:22 +0200

    Fix selectors in Discover page

--- app/reducers/entities/discoverItems.js
+++ app/reducers/entities/discoverItems.js
@@ -1,3 +1,7 @@
 import createEntityReducer from './createEntityReducer';
 
 export default createEntityReducer({ name: 'discoverItems' });
+
+export function getDiscoverItem(state, id) {
+  return state.entities.discoverItems[id];
+}
