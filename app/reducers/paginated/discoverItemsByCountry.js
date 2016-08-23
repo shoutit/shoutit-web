97346238e91d467a10b3678b6b9add0ce66986bb
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-08-23 12:58:22 +0200

    Fix selectors in Discover page

--- app/reducers/paginated/discoverItemsByCountry.js
+++ app/reducers/paginated/discoverItemsByCountry.js
@@ -1,5 +1,7 @@
 import * as actionTypes from '../../actions/actionTypes';
 import createPaginatedReducer from './createPaginatedReducer';
+import { denormalize } from '../../schemas';
+import { getPagination } from '../paginated';
 
 export default createPaginatedReducer({
   mapActionToKey: action => action.payload.country,
@@ -9,3 +11,15 @@ export default createPaginatedReducer({
     actionTypes.LOAD_DISCOVER_ITEMS_FAILURE,
   ],
 });
+
+export function getDiscoverItemsByCountry(state, country) {
+  const paginated = state.paginated.discoverItemsByCountry[country];
+  if (!paginated) {
+    return undefined;
+  }
+  return denormalize(paginated.ids, state.entities, 'DISCOVERITEMS');
+}
+
+export function getPaginationState(state, id) {
+  return getPagination(state, ['shoutsByDiscoverItem', id]);
+}
