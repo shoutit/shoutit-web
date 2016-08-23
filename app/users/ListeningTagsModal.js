2c8960c39d402cbb612feb8a18775fa7f5f26351
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-08-23 12:27:14 +0200

    Use ScrollablePagination for ListeningTagsModal

--- app/users/ListeningTagsModal.js
+++ app/users/ListeningTagsModal.js
@@ -1,45 +1,83 @@
-import React, { PropTypes, Component } from 'react';
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
-import Modal, { Header, Body, Footer } from '../modals';
+
+import PropTypes, { PaginationPropTypes } from '../utils/PropTypes';
+
+import { getListeningTags, getPaginationState } from '../reducers/paginated/tagListeningByUser';
+
+import { loadListeningTags } from '../actions/users';
+
+import Modal, { Header, Footer, Body } from '../modals';
 import Button from '../forms/Button';
-import ListeningTagsScrollableList from '../users/ListeningTagsScrollableList';
+import Progress from '../widgets/Progress';
+import ScrollablePagination from '../widgets/ScrollablePagination';
+import TagListItem from '../tags/TagListItem';
 
-export default class ListeningTagsModal extends Component {
+export class ListeningTagsModal extends Component {
+
   static propTypes = {
+    tags: PropTypes.array.isRequired,
     profile: PropTypes.object.isRequired,
+    loadTags: PropTypes.func.isRequired,
+    pagination: PropTypes.shape(PaginationPropTypes),
   }
 
-  componentDidMount() {
-    this.refs.close.focus();
-  }
+  modal = null
+  body = null
 
   render() {
-    const { profile, ...modalProps } = this.props;
+    const { tags, pagination, loadTags, ...modalProps } = this.props;
     return (
-      <Modal { ...modalProps } ref="modal" size="small">
-        <Header closeButton>
-          <FormattedMessage
-            id="users.ListeningTagsModal.title"
-            defaultMessage="{firstName}’s interests"
-            values={ { firstName: profile.firstName } }
-          />
-        </Header>
-        <Body>
-          <ListeningTagsScrollableList profile={ profile } />
-        </Body>
-        <Footer>
-          <Button ref="close"
-            size="small"
-            kind="primary"
-            onClick={ () => this.refs.modal.hide() }>
+      <ScrollablePagination
+        { ...pagination }
+        scrollElement={ () => this.modal.getBodyNode() }
+        loadData={ loadTags }>
+        <Modal
+          { ...modalProps }
+          ref={ el => { this.modal = el; } }
+          loading={ !tags }
+          size="small" >
+          <Header closeButton>
             <FormattedMessage
-              id="users.ListeningTagsModal.close"
-              defaultMessage="Close"
+              id="users.ListeningTagsModal.title"
+              defaultMessage="{firstName}’s interests"
+              values={ this.props.profile }
             />
-          </Button>
-        </Footer>
-      </Modal>
+          </Header>
+          <Body ref={ el => { this.body = el; } }>
+            { tags && tags.map(tag =>
+              <TagListItem
+                key={ tag.id }
+                popover={ false }
+                tag={ tag } />
+            )}
+            <Progress animate={ pagination.isFetching } />
+          </Body>
+          <Footer>
+            <Button
+              kind="primary"
+              onClick={ () => this.modal.hide() }>
+              <FormattedMessage
+                id="users.ListeningTagsModal.close"
+                defaultMessage="Close"
+              />
+            </Button>
+          </Footer>
+        </Modal>
+      </ScrollablePagination>
     );
   }
-
 }
+
+const mapStateToProps = (state, ownProps) => ({
+  tags: getListeningTags(state, ownProps.profile),
+  pagination: getPaginationState(state, ownProps.profile),
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  loadTags: params => dispatch(loadListeningTags(ownProps.profile, params)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListeningTagsModal);
