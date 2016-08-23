4f85771d8395990627f289cc3cbb7e1379709dd5
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-08-23 12:22:36 +0200

    As default state, ids are undefined, count is optional

--- app/utils/PropTypes.js
+++ app/utils/PropTypes.js
@@ -1,10 +1,11 @@
-/* eslint import/prefer-default-export: 0 */
-
 import { PropTypes } from 'react';
 
 export const PaginationPropTypes = {
-  count: PropTypes.number.isRequired,
+  count: PropTypes.number,
+  ids: PropTypes.array.isRequired,
   isFetching: PropTypes.bool,
   nextUrl: PropTypes.string,
   previousUrl: PropTypes.string,
 };
+
+export default PropTypes;
