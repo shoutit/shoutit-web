6a67448ece6b8cde494ce4631e6997cfa151b923
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-08-24 21:23:06 +0200

    Remove devic prop from ModalHost

--- app/modals/ModalHost.js
+++ app/modals/ModalHost.js
@@ -2,14 +2,12 @@ import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import { closeModal } from '../actions/ui';
 import { getModal, isAlreadyVisible } from '../reducers/modals';
-import { getDevice } from '../reducers/browser';
 
 export class ModalHost extends Component {
 
   static propTypes = {
     modal: PropTypes.element,
     isAlreadyVisible: PropTypes.bool,
-    device: PropTypes.oneOf(['smartphone', 'tablet', 'desktop']),
     onHide: PropTypes.func.isRequired,
   };
 
@@ -19,7 +17,6 @@ export class ModalHost extends Component {
         { this.props.modal &&
           React.cloneElement(this.props.modal, {
             onHide: this.props.onHide,
-            autoSize: this.props.device === 'desktop',
             enterAnimation: !this.props.isAlreadyVisible,
           })
         }
@@ -30,7 +27,6 @@ export class ModalHost extends Component {
 
 const mapStateToProps = state => ({
   modal: getModal(state),
-  device: getDevice(state),
   isAlreadyVisible: isAlreadyVisible(state),
 });
 
