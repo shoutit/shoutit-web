bcbe684616570ec8d5e5b92fa657f9947336810a
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-08-24 21:22:05 +0200

    Refactor ScrollablePagination → ScrollablePaginated

--- app/layout/ScrollablePaginated.js
+++ app/layout/ScrollablePaginated.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import debug from 'debug';
+import classNames from 'classnames';
+
 import PropTypes, { PaginationPropTypes } from '../utils/PropTypes';
 
+import Progress from '../widgets/Progress';
 import Scrollable from '../layout/Scrollable';
 
-const log = debug('shoutit:widgets:ScrollablePagination');
+const log = debug('shoutit:widgets:ScrollablePaginated');
 
-export default class ScrollablePagination extends Component {
+/**
+ * Render a Scrollable that can load paginated data when it's scrolled
+ * to the bottom.
+ *
+ * @export
+ * @class ScrollablePaginated
+ * @extends {Component}
+ */
+export default class ScrollablePaginated extends Component {
 
   static propTypes = {
     loadData: PropTypes.func.isRequired,
-    scrollElement: PropTypes.func,
-    children: PropTypes.node.isRequired,
+    children: PropTypes.element.isRequired,
+    className: PropTypes.string,
+    showProgress: PropTypes.bool,
     ...PaginationPropTypes,
   }
 
@@ -24,8 +36,8 @@ export default class ScrollablePagination extends Component {
     const params = this.getLoadParams();
     log('Loading data after component is mounted...');
     if (this.props.ids) {
+      log('Will refresh using the current page size');
       delete params.endpoint;
-      log('Will refresh the current page size');
       params.query = {
         page_size: this.props.ids.length,
       };
@@ -45,6 +57,8 @@ export default class ScrollablePagination extends Component {
     };
   }
 
+  scrollable = null
+
   loadData(params) {
     log('Loading data with params', params);
     this.props.loadData(params);
@@ -53,23 +67,23 @@ export default class ScrollablePagination extends Component {
   loadDataIfNeeded() {
     log('Loading data if needed...');
     if (this.props.isFetching) {
-      log('Will not load since already fetching');
+      log('Will not load data since already fetching');
       return;
     }
     if (!this.props.nextUrl) {
-      log('Will not load since there\'s no next url');
+      log('Will not load data since there\'s no next url');
       return;
     }
     if (this.scrollable.canScroll()) {
-      log('Will not load since there\'s space to scroll');
+      log('Will not load data since there\'s still space to scroll');
       return;
     }
     this.loadData(this.getLoadParams());
   }
 
   handleScrollBottom() {
-    log('Scrolled to bottom');
     if (this.props.nextUrl) {
+      log('Scrolled to bottom, will load new data');
       this.loadData(this.getLoadParams());
     }
   }
@@ -77,11 +91,14 @@ export default class ScrollablePagination extends Component {
   render() {
     return (
       <Scrollable
-        ref={ el => { this.scrollable = el; } }
-        className="ScrollablePagination"
-        scrollElement={ this.props.scrollElement }
+        ref={ el => this.scrollable = el }
+        style={ this.props.style }
+        className={ classNames('ScrollablePaginated', this.props.className) }
         onScrollBottom={ this.handleScrollBottom }>
+        <div className="ScrollablePaginated-Content">
           { this.props.children }
+        </div>
+        <Progress animate={ this.props.showProgress } />
       </Scrollable>
     );
   }
