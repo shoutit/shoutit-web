95a532dc97e0a856fbcdad974bf3dccd149d9264
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-08-24 23:52:53 +0200

    Use a ProfilesListModal
    For listeners/listening/tag listeners

--- app/tags/TagListenersModal.js
+++ app/tags/TagListenersModal.js
@@ -1,40 +1,46 @@
-import React, { PropTypes, Component } from 'react';
+import React, { PropTypes } from 'react';
+
 import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
 
-import Modal, { Header, Body, Footer } from '../modals';
-import Button from '../forms/Button';
-import TagListenersScrollableList from '../tags/TagListenersScrollableList';
+import { loadTagListeners } from '../actions/tags';
+import { getListenersByTag, getPaginationState } from '../reducers/paginated/listenersByTag';
 
-export default class ListenersModal extends Component {
-  static propTypes = {
-    tag: PropTypes.object.isRequired,
-    category: PropTypes.object,
-  };
+import ProfilesListModal from '../users/ProfilesListModal';
 
-  componentDidMount() {
-    this.refs.close.focus();
-  }
-
-  render() {
-    const { tag, category, ...modalProps } = this.props;
-
-    return (
-      <Modal { ...modalProps } ref="modal" size="small">
-        <Header closeButton>
-          <FormattedMessage
-            id="tagListenersModal"
-            defaultMessage="Listening to {name}"
-            values={ { name: category ? category.name : tag.name } }
-          />
-        </Header>
-        <Body>
-          <TagListenersScrollableList tag={ tag } />
-        </Body>
-        <Footer>
-          <Button ref="close" kind="primary" onClick={ () => this.refs.modal.hide() }>Close</Button>
-        </Footer>
-      </Modal>
-    );
-  }
-
+export function TagListenersModal({ tag, category, profiles, loadProfiles, pagination, ...props }) {
+  const title = (
+    <FormattedMessage
+      id="tagListenersModal"
+      defaultMessage="Listening to {name}"
+      values={ { name: category ? category.name : tag.name } }
+    />
+  );
+  return (
+    <ProfilesListModal
+      title={ title }
+      profiles={ profiles }
+      loadProfiles={ loadProfiles }
+      pagination={ pagination }
+      { ...props }
+    />
+  );
 }
+
+TagListenersModal.propTypes = {
+  category: PropTypes.object,
+  tag: PropTypes.object.isRequired,
+  ...ProfilesListModal.propTypes,
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  profiles: getListenersByTag(state, ownProps.tag.id),
+  pagination: getPaginationState(state, ownProps.tag.id),
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  loadProfiles: params => dispatch(loadTagListeners(ownProps.tag, params)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TagListenersModal);
+
--- app/users/ListenersModal.js
+++ app/users/ListenersModal.js
@@ -1,53 +1,87 @@
-import React, { PropTypes, Component } from 'react';
+import React from 'react';
+import PropTypes, { PaginationPropTypes } from '../utils/PropTypes';
+
 import { FormattedMessage } from 'react-intl';
-import Modal, { Header, Body, Footer } from '../modals';
-import Button from '../forms/Button';
-import ListenersScrollableList from '../users/ListenersScrollableList';
+import { connect } from 'react-redux';
 
-export default class ListenersModal extends Component {
-  static propTypes = {
-    type: PropTypes.oneOf(['listeners', 'listening']),
-    profile: PropTypes.object.isRequired,
-  }
+import { loadListeners, loadListeningProfiles } from '../actions/users';
+import {
+  getListenersByProfile,
+  getPaginationState as getListenersPaginationState,
+} from '../reducers/paginated/listenersByUser';
 
-  static defaultProps = {
-    type: 'listeners',
-  }
+import {
+  getListeningByProfile,
+  getPaginationState as getListeningPaginationState,
+} from '../reducers/paginated/listeningByUser';
 
-  componentDidMount() {
-    this.refs.close.focus();
-  }
+import ProfilesListModal from '../users/ProfilesListModal';
 
-  render() {
-    const { type, profile, ...modalProps } = this.props;
-    return (
-      <Modal { ...modalProps } ref="modal" size="small">
-        <Header closeButton>
-          <FormattedMessage
-            id="listenersModal.title"
-            defaultMessage="{type, select,
-              listeners {Listening to {name}}
-              listening {{name} is listening to}
-            }"
-            values={ { type, ...profile } }
-          />
-        </Header>
-        <Body>
-          <ListenersScrollableList type={ type } profile={ profile } />
-        </Body>
-        <Footer>
-          <Button
-            ref="close"
-            kind="primary"
-            onClick={ () => this.refs.modal.hide() }>
-            <FormattedMessage
-              id="listenersModal.closeButton"
-              defaultMessage="Close"
-            />
-          </Button>
-        </Footer>
-      </Modal>
-    );
-  }
-
+function ListenersModal({ profile, type, profiles, loadProfiles, pagination, ...props }) {
+  const title = (
+    <FormattedMessage
+      id="listenersModal.title"
+      defaultMessage="{type, select,
+        listeners {Listening to {name}}
+        listening {{name} is listening to}
+      }"
+      values={ {
+        type,
+        name: profile.name,
+      } }
+      />
+  );
+  return (
+    <ProfilesListModal
+      title={ title }
+      profiles={ profiles }
+      loadProfiles={ loadProfiles }
+      pagination={ pagination }
+      { ...props }
+    />
+  );
 }
+
+ListenersModal.propTypes = {
+  type: PropTypes.oneOf(['listeners', 'listening']),
+  profile: PropTypes.object.isRequired,
+
+  profiles: PropTypes.array.isRequired,
+  title: PropTypes.node.isRequired,
+  loadProfiles: PropTypes.func.isRequired,
+  pagination: PropTypes.shape(PaginationPropTypes),
+};
+
+function mapStateToProps(state, ownProps) {
+  let profiles;
+  let pagination;
+  const { id } = ownProps.profile;
+  if (ownProps.type === 'listening') {
+    profiles = getListeningByProfile(state, id);
+    pagination = getListeningPaginationState(state, id);
+  } else {
+    profiles = getListenersByProfile(state, id);
+    pagination = getListenersPaginationState(state, id);
+  }
+
+  return {
+    profiles,
+    pagination,
+  };
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  loadProfiles:
+    params => {
+      let action;
+      if (ownProps.type === 'listening') {
+        action = loadListeningProfiles;
+      } else {
+        action = loadListeners;
+      }
+      return dispatch(action(ownProps.profile, params));
+    },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListenersModal);
+
