af197fda70eab83e7420c0195003c38c73de29d4
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-08-27 12:20:48 +0200

    Newrelic enable debug

--- newrelic.js
+++ newrelic.js
@@ -9,7 +9,7 @@ exports.config = {
   app_name: [process.env.NEW_RELIC_APP_NAME],
   license_key: process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
-    level: 'info',
+    level: 'debug',
   },
   rules: {
     ignore: [
