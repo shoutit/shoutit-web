4319e169426f23dd46aab5ab00599383cecd1770
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-08-28 22:51:01 +0200

    Add delete method

--- app/services/authenticateAs.js
+++ app/services/authenticateAs.js
@@ -5,11 +5,6 @@ const log = debug('shoutit:services:authenticateAs');
 export default {
   name: 'authenticateAs',
   create: (req, resource, params, body, config, callback) => {
-    if (!req.session || !req.session.profile) {
-      callback(new Error('Session does not exists'));
-      return;
-    }
-
     req.fetchr.read('profile')
       .params({ username: body.username })
       .end((err, page) => {
@@ -24,6 +19,12 @@ export default {
         req.session.page = page;
         log('Authenticated with page %s', req.session.page.username);
         callback(null, page);
-    });
+      });
+  },
+
+  delete: (req, resource, params, config, callback) => {
+    delete req.session.page;
+    log('Logged out as page');
+    callback();
   },
 };
