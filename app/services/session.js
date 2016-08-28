a8b785ad79f11c53b1438b370418d5403f2724df
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-08-28 13:02:22 +0200

    Refactor profile/session services for accounts

--- app/services/session.js
+++ app/services/session.js
@@ -9,73 +9,114 @@ import {
   AUTH_CLIENT_SECRET as client_secret,
 } from './constants';
 
-const log = debug('shoutit:service:session');
+const log = debug('shoutit:services:session');
 
-export function setRequestSession(req, sessionData) {
-  const { profile, accessToken, refreshToken, scope, expiresIn } = sessionData;
-  req.session.user = profile;
-  req.session.accessToken = accessToken;
-  req.session.refreshToken = refreshToken;
-  req.session.cookie.expires = new Date(Date.now() + (expiresIn * 1000));
-  req.session.scope = scope ? scope.split[' '] : [];
-  log('Request session has been created and will expires on %s', req.session.cookie.expires);
-}
-
-export function createRequestSession(req, data, callback) {
-  if (!req.session) {
-    const error = new Error('Cannot initialize session');
-    callback(error);
-    return;
-  }
-  log('Creating request session...');
-  request
-    .post('/oauth2/access_token')
-    .send({ ...data, client_id, client_secret })
-    .use(req)
-    .prefix()
-    .end((err, res) => {
-      if (err) {
-        callback(parseApiError(err));
-        return;
-      }
-      setRequestSession(req, camelizeKeys(res.body));
-      callback(null, req.session.user);
-    });
-}
-
-export function readSessionProfile(req, callback) {
+export function readSessionProfile(req) {
   log('Reading the current session\'s profile...');
-  request
-    .get('/profiles/me')
-    .use(req)
-    .prefix()
-    .end((err, res) => {
-      if (err) {
-        console.warn('Trying to get user %s but got an error with status code %s: destroying current session...', req.session.user.username, res && res.status); //eslint-disable-line
-        console.error(err); //eslint-disable-line
-        req.session.destroy();
-        return callback();
-      }
-      log('Current profile\'s username is %s', res.body.username);
-      const user = camelizeKeys(res.body);
-      return callback(null, user);
-    });
+  return new Promise(resolve => {
+    request
+      .get('/profiles/me')
+      .use(req)
+      .prefix()
+      .end((err, res) => {
+        if (err) {
+          console.warn('Trying to get user %s but got an error with status code %s: destroying current session...', req.session.user.username, res && res.status); //eslint-disable-line
+          console.error(err); //eslint-disable-line
+          req.session.destroy();
+          resolve();
+        }
+        const profile = camelizeKeys(res.body);
+        log('Current profile\'s username is %s', profile.username);
+        resolve(profile);
+      });
+  });
 }
+
+export function readAuthenticatedPage(req) {
+  if (!req.session || !req.session.authorizationPage) {
+    return Promise.resolve();
+  }
+  log('Reading the current authenticated page...');
+  const { authorizationPage } = req.session;
+  return new Promise(resolve => {
+    request
+      .get(`/profiles/${authorizationPage.username}`)
+      .use(req)
+      .prefix()
+      .end((err, res) => {
+        if (err) {
+          console.error(err);
+          resolve();
+        }
+        const page = camelizeKeys(res.body);
+        resolve(page);
+      });
+  });
+}
+
 
 export default {
   name: 'session',
+
   create: (req, resource, params, body, config, callback) => {
-    createRequestSession(req, body, callback);
+    log('Creating request session...');
+    request
+      .post('/oauth2/access_token')
+      .send({ ...body, client_id, client_secret })
+      .use(req)
+      .prefix()
+      .end((err, res) => {
+        if (err) {
+          callback(parseApiError(err));
+          return;
+        }
+
+        const response = camelizeKeys(res.body);
+        const { accessToken, refreshToken, newSignup, expiresIn, scope, profile } = response;
+
+        req.session.user = profile;
+        req.session.newSignup = newSignup;
+        req.session.accessToken = accessToken;
+        req.session.refreshToken = refreshToken;
+        req.session.cookie.expires = new Date(Date.now() + (expiresIn * 1000));
+        req.session.scope = scope ? scope.split[' '] : [];
+
+        log('Request session has been created and will expires on %s', req.session.cookie.expires);
+
+        callback(null, profile);
+      });
   },
+
   read: (req, resource, params, config, callback) => {
-    log('Reading current session...');
+    log('Reading current session...', req.session);
     if (!req.session || !req.session.user) {
       callback(new Error('Session does not exists'));
       return;
     }
-    readSessionProfile(req, callback);
+
+    readSessionProfile(req)
+      .then(profile =>
+        req.session.user = profile
+      )
+      .then(() =>
+        readAuthenticatedPage(req)
+      )
+      .then(page => {
+        if (!page.isOwner) {
+          log('Not owner of %s, removing authorizationPage from session', page.username);
+          delete req.session.authorizationPage;
+          return;
+        }
+        log('Authenticated page\'s username is %s', page.username);
+        req.session.page = page;
+      })
+      .then(() =>
+        callback(req.session.profile)
+      );
   },
+
   delete: (req, resource, params, config, callback) => {
     req.session.destroy(callback);
   },
+
 };
