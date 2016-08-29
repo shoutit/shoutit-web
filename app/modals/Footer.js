00c03c17585bf61676b34f7aae5ef2f04589665b
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-08-29 21:28:31 +0200

    Add props to manage loading state

--- app/modals/Footer.js
+++ app/modals/Footer.js
@@ -1,9 +1,15 @@
 import React, { PureComponent, PropTypes } from 'react';
+import Progress from '../widgets/Progress';
 
 export default class Footer extends PureComponent {
   static propTypes = {
     children: PropTypes.node.isRequired,
+    showProgress: PropTypes.bool,
   };
+
+  static defaultProps = {
+    showProgress: false,
+  }
 
   getHeight() {
     return this.node.offsetHeight;
@@ -14,7 +20,10 @@ export default class Footer extends PureComponent {
   render() {
     return (
       <div className="ModalFooter" ref={ el => { this.node = el; } }>
-        { this.props.children }
+        { this.props.showProgress ?
+          <Progress animate /> :
+          this.props.children
+        }
       </div>
     );
   }
