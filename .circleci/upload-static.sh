1e286c8757d4caf3c30fc81e09aeef5d476adfce
Mo Chawich <mohamad.chawich@rocket-internet.com>
2017-08-26 18:07:02 +0200

    Upload static to s3

---
+++ .circleci/upload-static.sh
@@ -0,0 +1,17 @@
+#! /bin/bash -e
+#/ Usage: upload-static.sh S3_BUCKET_NAME PATH
+#/ This script
+#/   - Uploads built static files to S3
+
+if [ -z "$1" ]; then
+    echo "Please provide name for S3 bucket"
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "Please provide path under $1"
+    exit 1
+fi
+
+# Built Public
+aws s3 cp --recursive "../built/public/" "s3://$1/$2/"
