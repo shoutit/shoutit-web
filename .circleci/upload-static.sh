4a80a727638fe5c891b05ed540eea4548697707e
Mo Chawich <mohamad.chawich@rocket-internet.com>
2017-08-26 18:27:51 +0200

    Fix upload static

--- .circleci/upload-static.sh
+++ .circleci/upload-static.sh
@@ -1,17 +1,22 @@
 #! /bin/bash -e
-#/ Usage: upload-static.sh S3_BUCKET_NAME PATH
+#/ Usage: upload-static.sh LOCAL_PATH S3_BUCKET_NAME PATH
 #/ This script
 #/   - Uploads built static files to S3
 
 if [ -z "$1" ]; then
-    echo "Please provide name for S3 bucket"
+    echo "Please provide local path"
     exit 1
 fi
 
 if [ -z "$2" ]; then
+    echo "Please provide name for S3 bucket"
+    exit 1
+fi
+
+if [ -z "$3" ]; then
     echo "Please provide path under $1"
     exit 1
 fi
 
 # Built Public
-aws s3 cp --recursive "$(dirname "$(pwd)")/built/public/" "s3://$1/$2/"
+aws s3 cp --recursive "$1" "s3://$2/$3/"
