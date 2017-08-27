c9efc67d1ab7b1216da4d9189bbc3e3eec828af3
Mo Chawich <mohamad.chawich@rocket-internet.com>
2017-08-27 22:29:10 +0200

    Use CircleCI workflows

--- .circleci/set-build-tag.sh
+++ .circleci/set-build-tag.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
-BUILD_TAG=`echo "${CIRCLE_TAG:-$CIRCLE_BRANCH}" | sed -e "s/\//-/g"`
-echo ${BUILD_TAG}
-echo "export BUILD_TAG=${BUILD_TAG}" >> $BASH_ENV
+# Set BUILD_TAG to $CIRCLE_BUILD_NUM
+echo "BUILD_TAG=${CIRCLE_BUILD_NUM}"
+echo "export BUILD_TAG=${CIRCLE_BUILD_NUM}" >> $BASH_ENV
+
+# Set BUILD_BRANCH
+case "${CIRCLE_TAG:-$CIRCLE_BRANCH}" in
+    release-*)
+      BUILD_BRANCH=live
+    ;;
+    stage-*)
+      BUILD_BRANCH=${CIRCLE_TAG}
+    ;;
+    *)
+      BUILD_BRANCH=${CIRCLE_BRANCH}
+    ;;
+esac
+
+echo "BUILD_BRANCH=${BUILD_BRANCH}"
+echo "export BUILD_BRANCH=${BUILD_BRANCH}" >> $BASH_ENV
